456ba8b9b58e1ebcd3fe2091baa76e2d2dbfb22f
Alexander Dean <alexander.dean@keplarllp.com>
2012-05-21 15:50:50 +0100

    Stubbed a test harness for the Deserializers

--- src/main/java/com/snowplowanalytics/snowplow/hive/serde/SnowPlowEventDeserializer.java
+++ src/main/java/com/snowplowanalytics/snowplow/hive/serde/SnowPlowEventDeserializer.java
@@ -69,14 +69,13 @@ public class SnowPlowEventDeserializer implements Deserializer {
   /**
    * @param args
    */
-  public static void runTest() {
+  public static void runTest(String textLine) {
     System.err.println("This is only a test run");
     try {
       SnowPlowEventDeserializer serDe = new SnowPlowEventDeserializer();
       Configuration conf = new Configuration();
       Properties tbl = new Properties();
-      Text sample = new Text("2012-03-16	11:45:01	ARN1	3422	195.78.71.32	GET	detlpfvsg0d9v.cloudfront.net	/ice.png	200	http://delivery.ads-creativesyndicator.com/adserver/www/delivery/afr.php?zoneid=103&cb=INSERT_RANDOM_NUMBER_HERE&ct0=INSERT_CLICKURL_HERE	Mozilla/5.0%20(Windows%20NT%206.0)%20AppleWebKit/535.11%20(KHTML,%20like%20Gecko)%20Chrome/17.0.963.79%20Safari/535.11	&ad_ba=1884&ad_ca=547&ad_us=a1088f76c6931b0a26228dc3bde321d7&r=481413&urlref=http%253A%252F%252Fwww.fantasyfootballscout.co.uk%252F&_id=b41cf6859dccd8ce&_ref=http%253A%252F%252Fwww.fantasyfootballscout.co.uk%252F&pdf=1&qt=0&realp=0&wma=0&dir=1&fla=1&java=1&gears=0&ag=1&res=1920x1200&cookie=1");
-      // Text sample = new Text("02/01/2011 01:13:12 LAX1 2390282 192.0.2.202 GET www.singalong.com /soundtrack/happy.mp3 304 www.unknownsingers.com Mozilla/4.0%20(compatible;%20MSIE%207.0;%20Windows%20NT%205.1) a=b&c=d");
+      Text sample = new Text(textLine);
       serDe.initialize(conf, tbl);
       Object row = serDe.deserialize(sample);
       System.err.println(serDe.getObjectInspector().getClass().toString());
