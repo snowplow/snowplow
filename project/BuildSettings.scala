142ecb53154f968e2085593797cd98f6c9d7d57d
Alex Dean <alexander.dean@keplarllp.com>
2012-05-24 20:31:35 +0100

    Version bump to keep in sync with parent project (snowplow/snowplow)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "Orderly Ltd",
-    version       := "0.1",
+    version       := "0.4.0",
     description   := "Hive deserializers for the SnowPlow log data",
     scalaVersion  := "2.9.1",
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
