673fcacec6f54b7dc46187427365c0e3b6ec90e7
Alex Dean <alexander.dean@keplarllp.com>
2012-06-09 18:04:28 +0300

    Added underscore to var name

--- hive/etl/hiveql/1-extract.sql
+++ hive/etl/hiveql/1-extract.sql
@@ -2,4 +2,4 @@ ADD JAR s3://${JARBUCKET}/snowplow-log-deserializers-0.4.4.jar ;
 
 CREATE EXTERNAL TABLE `extracted_logs_${RUN_DATE}`
 ROW FORMAT SERDE 'com.snowplowanalytics.snowplow.hadoop.hive.SnowPlowEventDeserializer'
-LOCATION '${CLOUDFRONTLOGS}' ;
+LOCATION '${CLOUDFRONT_LOGS}' ;
