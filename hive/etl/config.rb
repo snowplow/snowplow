8787a2347e41029ecd615f058fc0fda2307331d9
Alex Dean <alexander.dean@keplarllp.com>
2012-06-14 01:11:25 +0300

    Added trailing slash support

--- hive/etl/config.rb
+++ hive/etl/config.rb
@@ -32,6 +32,10 @@ module Config
     options = Config.parse_args()
     config = YAML.load_file(options[:config])
 
+    # Add trailing slashes if needed
+    trail = lamba {|str| return str[-1].chr != '/' ? str << '/' : str}
+    config[:buckets].update(config[:buckets]){|k,v| trail.call(v)}
+
     config[:date] = (Date.today - 1).strftime('%Y-%m-%d') # Yesterday's date
     config[:query_file][:local] = File.join(File.dirname(__FILE__), "hiveql", QUERY_FILE)
     config[:query_file][:remote] = File.join(config[:buckets][:query], QUERY_FILE)
