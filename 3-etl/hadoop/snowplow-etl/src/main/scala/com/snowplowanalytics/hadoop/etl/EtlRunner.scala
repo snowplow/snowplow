005e51d93dc636a273edc7280f7446fbf6ff029f
Alex Dean <alexander.dean@keplarllp.com>
2012-09-04 01:57:59 +0300

    Made a start on the Geo code

--- 3-etl/hadoop/snowplow-etl/src/main/scala/com/snowplowanalytics/hadoop/etl/EtlRunner.scala
+++ 3-etl/hadoop/snowplow-etl/src/main/scala/com/snowplowanalytics/hadoop/etl/EtlRunner.scala
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package com.snowplowanalytics.hadoop.scalding
+package com.snowplowanalytics.hadoop.etl
 
 // Hadoop
 import org.apache.hadoop
@@ -19,11 +19,11 @@ import org.apache.hadoop
 import com.twitter.scalding.Tool
 
 /**
- * Entrypoint for Hadoop to kick off the job.
+ * Entrypoint for Hadoop to kick off the ETL job.
  *
  * Borrowed from com.twitter.scalding.Tool
  */
-object JobRunner {
+object EtlRunner {
   def main(args : Array[String]) {
     hadoop.util.ToolRunner.run(new hadoop.conf.Configuration, new Tool, args);
   }
