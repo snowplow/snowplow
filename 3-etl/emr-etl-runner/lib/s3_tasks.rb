c3292d67a849237736adba83bf4e2ae0571c7f5b
Alex Dean <alexander.dean@keplarllp.com>
2012-09-22 19:25:21 +0300

    Added files for log archiving

--- 3-etl/emr-etl-runner/lib/s3_tasks.rb
+++ 3-etl/emr-etl-runner/lib/s3_tasks.rb
@@ -21,23 +21,18 @@ require 'aws/s3'
 # 2. Archiving the CloudFront log files by moving them into a separate bucket
 module S3Tasks
 
+  class NoBucketError < StandardError; end
+
   # Uploads the Hive query to S3 ready to be executed as part of the Hive job.
   # Ensures we are executing the most recent version of the Hive query.
   #
   # Parameters:
   # +config+:: the hash of configuration options
   #
-  # TODO: update this when https://github.com/rslifka/elasticity/issues/34 fixed
-  def S3Tasks.upload_etl_tools(config)
-
-  # Specify the bucket and AWS credentials
-  s3 = Elasticity::SyncToS3('my-bucket', 'access', 'secret')
-
-  # Use the standard environment variables (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY)
-  # s3 = Elasticity::SyncToS3('my-bucket')
-
-  # Recursively sync the contents of '/some/parent/dir' under the remote location 'remote-dir/this-job/assets'
-  s3.sync('/some/parent/dir', 'remote-dir/this-job/assets')
+  # TODO: replace with Elasticity code when added:
+  # - https://github.com/rslifka/elasticity/issues/34
+  # - https://github.com/rslifka/elasticity/pull/35
+  def sync_assets(config)
 
    # Connect to S3
     AWS::S3::Base.establish_connection!(
@@ -47,21 +42,45 @@ module S3Tasks
 
     # Upload the two query files and the serde
     # Array of files to upload: "tuple" format is [Filename, Local filepath, S3 bucket path, Content type]
-    [[config[:daily_query_file], config[:daily_query_path], config[:buckets][:query], 'text/plain'],
-     [config[:datespan_query_file], config[:datespan_query_path], config[:buckets][:query], 'text/plain'],
+    [[config[:daily_query_file], config[:daily_query_path], config[:buckets][:assets], 'text/plain'],
+     [config[:datespan_query_file], config[:datespan_query_path], config[:buckets][:assets], 'text/plain'],
      [config[:serde_file], config[:serde_path], config[:buckets][:serde], 'application/java-archive']
     ].each do |f|
       AWS::S3::S3Object.store(f[0], open(f[1]), f[2], :content_type => f[3])
     end
-
   end
+  module_function :sync_assets
 
   # Moves (archives) the processed CloudFront logs to an archive bucket.
-  # Prevents the same log files from being processed again the next day.
+  # Prevents the same log files from being processed again.
   # Parameters:
   # +config+:: the hash of configuration options
-  def S3Tasks.archive_logs(config)
+  def archive_logs(config)
+
+    s3 = Fog::Storage.new({
+      :provider => 'AWS',
+      :aws_access_key_id => config[:aws][:access_key_id],
+      :aws_secret_access_key => config[:aws][:secret_access_key]
+    })
+
+    # Bucket containing logs to archive...
+    in = bucket(config[:buckets][:in])
+    in.files.each do |file|
+      puts file.etag
+    end
+
     # TODO: implement
     puts "Archiving CloudFront logs... (TODO)"
   end
+  module_function :archive_logs
+
+  def bucket(bucket_name)
+    index = s3.directories.index { |d| d.key == bucket_name }
+    bucket ||= index ? s3.directories[index] : nil
+    if bucket.nil?
+      raise NoBucketError, "Bucket '%s' does not exist" % bucket_name
+    end
+    bucket
+  end
+
 end
--- 3-etl/emr-etl-runner/snowplow-emr-etl-runner.gemspec
+++ 3-etl/emr-etl-runner/snowplow-emr-etl-runner.gemspec
@@ -2,7 +2,7 @@
 require File.expand_path('../lib/snowplow-emr-etl-runner/version', __FILE__)
 
 Gem::Specification.new do |gem|
-  gem.authors       = ["Alex Dean"]
+  gem.authors       = ["Alex Dean <support@snowplowanalytics.com>"]
   gem.email         = ["support@snowplowanalytics.com"]
   gem.summary       = %q{Runs the SnowPlow Hive ETL on EMR}
   gem.description   = %q{A Bundler RubyGem to run SnowPlow's Hive ETL job on Amazon Elastic MapReduce. Uses Elasticity and Fog}
@@ -12,10 +12,12 @@ Gem::Specification.new do |gem|
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = SnowPlow::EmrEtlRunner::SCRIPT_NAME
-  gem.require_paths = ["lib"]
   gem.version       = SnowPlow::EmrEtlRunner::VERSION
   gem.platform      = Gem::Platform::RUBY
+  gem.require_paths = ["lib"]
 
   gem.add_dependency 'elasticity', '~> 2.4'
   gem.add_dependency 'fog', '~> 1.6.0'
+  gem.add_dependency 's3', '0.3.11'
+  # TODO: remove s3 when no longer used
 end
