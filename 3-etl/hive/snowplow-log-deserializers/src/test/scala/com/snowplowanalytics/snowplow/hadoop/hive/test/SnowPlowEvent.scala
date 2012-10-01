9d469183205875d69aaee9d3e454b323a7dbdb72
Alex Dean <alexander.dean@keplarllp.com>
2012-10-02 00:31:18 +0100

    Removed @BeanProperty's

--- 3-etl/hive/snowplow-log-deserializers/src/test/scala/com/snowplowanalytics/snowplow/hadoop/hive/test/SnowPlowEvent.scala
+++ 3-etl/hive/snowplow-log-deserializers/src/test/scala/com/snowplowanalytics/snowplow/hadoop/hive/test/SnowPlowEvent.scala
@@ -13,9 +13,6 @@
 package com.snowplowanalytics.snowplow.hadoop.hive
 package test
 
-// Scala
-import scala.reflect.BeanProperty
-
 /**
  * Scala-friendly equivalent of SnowPlowEventStruct.
  * Can't use a case class because we have too many
@@ -25,127 +22,79 @@ import scala.reflect.BeanProperty
 class SnowPlowEvent {
 
   // Date/time
-  @BeanProperty
   var dt: String = _
-  @BeanProperty
   var tm: String = _
 
   // Transaction (i.e. this logging event)
-  @BeanProperty
   var txn_id: String = _
 
   // The application (site, game, app etc) this event belongs to
   var app_id: String = _
 
   // User and visit
-  @BeanProperty
   var user_id: String = _
-  @BeanProperty
   var user_ipaddress: String = _
-  @BeanProperty
   var visit_id: Int = _
 
   // Page
-  @BeanProperty
   var page_url: String = _
-  @BeanProperty
   var page_title: String = _
-  @BeanProperty
   var page_referrer: String = _
 
   // Marketing
-  @BeanProperty
   var mkt_medium: String = _
-  @BeanProperty
   var mkt_source: String = _
-  @BeanProperty
   var mkt_term: String = _
-  @BeanProperty
   var mkt_content: String = _
-  @BeanProperty
   var mkt_campaign: String = _
 
   // Event
-  @BeanProperty
   var ev_category: String = _
-  @BeanProperty
   var ev_action: String = _
-  @BeanProperty
   var ev_label: String = _
-  @BeanProperty
   var ev_property: String = _
-  @BeanProperty
   var ev_value: String = _
 
   // Browser (from user-agent)
-  @BeanProperty
   var br_name: String = _
-  @BeanProperty
   var br_family: String = _
-  @BeanProperty
   var br_version: String = _
-  @BeanProperty
   var br_type: String = _
-  @BeanProperty
   var br_renderengine: String = _
 
   // Browser (from querystring)
-  @BeanProperty
   var br_lang: String = _
-  @BeanProperty
   var br_features: List[String] = List[String]()
-  @BeanProperty
   var br_cookies: Boolean = _
 
   // OS (from user-agent)
-  @BeanProperty
   var os_name: String = _
-  @BeanProperty
   var os_family: String = _
-  @BeanProperty
   var os_manufacturer: String = _
 
   // Device/Hardware (from user-agent)
-  @BeanProperty
   var dvce_type: String = _
-  @BeanProperty
   var dvce_ismobile: Boolean = _
 
   // Device (from querystring)
-  @BeanProperty
   var dvce_screenwidth: Int = _
-  @BeanProperty
   var dvce_screenheight: Int = _
 
   // Ecommerce transaction (from querystring)
-  @BeanProperty
   var tr_orderid: String = _
-  @BeanProperty
   var tr_affiliation: String = _
-  @BeanProperty
   var tr_total: String = _
-  @BeanProperty
   var tr_tax: String = _
-  @BeanProperty
   var tr_shipping: String = _
-  @BeanProperty
   var tr_city: String = _
-  @BeanProperty
   var tr_state: String = _
-  @BeanProperty
   var tr_country: String = _
 
   // Ecommerce transaction item (from querystring)
-  @BeanProperty
   var ti_orderid: String = _
-  @BeanProperty
   var ti_sku: String = _
-  @BeanProperty
   var ti_name: String = _
-  @BeanProperty
   var ti_category: String = _
-  @BeanProperty
   var ti_price: String = _
-  @BeanProperty
   var ti_quantity: String = _
 }
