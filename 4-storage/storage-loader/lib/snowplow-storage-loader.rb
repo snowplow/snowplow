fa62bb63e9ee63732816feddbe2c14d1450355e1
Alex Dean <alexander.dean@keplarllp.com>
2012-11-03 01:41:26 +0200

    Stubbed the files

---
+++ 4-storage/storage-loader/lib/snowplow-storage-loader.rb
@@ -0,0 +1,7 @@
+require "storage-loader/version"
+
+module Storage
+  module Loader
+    # Your code goes here...
+  end
+end
---
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/version.rb
@@ -0,0 +1,5 @@
+module Storage
+  module Loader
+    VERSION = "0.0.1"
+  end
+end
