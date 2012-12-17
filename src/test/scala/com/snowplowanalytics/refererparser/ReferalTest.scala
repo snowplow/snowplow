78205d1a2df77f27289f474f8173f20e1d83d385
Alex Dean <alexander.dean@keplarllp.com>
2012-12-17 15:54:57 +0200

    Stubbed the test

---
+++ src/test/scala/com/snowplowanalytics/refererparser/ReferalTest.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012 SnowPlow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.refererparser.scala
+
+// Specs2
+import org.specs2.mutable.Specification
+
+class BadRowTest extends Specification {
+
+  // Toggle if tests are failing and you want to inspect the struct contents
+  implicit val _DEBUG = false
+
+  val malformedRows = Seq(
+    "",
+    "NOT VALID",
+    "2012-05-21\t07:14:47\tFRA2\t3343\t83.4.209.35\tGET\td3t05xllj8hhgj.cloudfront.net"
+  )
+
+  "An invalid or corrupted CloudFront row should throw an exception" >> {
+     malformedRows foreach { row =>
+      "invalid row \"%s\" throws a SerDeException".format(row) >> {
+        SnowPlowDeserializer.deserializeUntyped(row) must throwA[SerDeException](message = "Row does not match expected CloudFront regexp pattern")
+      }
+    }
+  }
+}
