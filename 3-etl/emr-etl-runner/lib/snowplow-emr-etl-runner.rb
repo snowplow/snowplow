6f09f98dc2cdb155dba630abed4fb8dd58a1c7d2
Alex Dean <alexander.dean@keplarllp.com>
2012-12-29 00:49:55 +0200

    Version bump

--- 3-etl/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-etl/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -21,6 +21,6 @@ require 'snowplow-emr-etl-runner/errors'
 module SnowPlow
   module EmrEtlRunner
     NAME          = "snowplow-emr-etl-runner"
-    VERSION       = "0.0.6"
+    VERSION       = "0.0.7"
   end
 end
