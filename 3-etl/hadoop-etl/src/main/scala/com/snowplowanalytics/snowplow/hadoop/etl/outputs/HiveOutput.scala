1a49e637e7cf2029c873423f834738252a9b4997
Alex Dean <alexander.dean@keplarllp.com>
2013-01-13 00:24:59 +0000

    Stubbed HiveOutput file for general Hive file generation

---
+++ 3-etl/hadoop-etl/src/main/scala/com/snowplowanalytics/snowplow/hadoop/etl/outputs/HiveOutput.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012 SnowPlow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.hadoop.etl
+package outputs
+
+object HiveOutput {
+
+  /**
+   * TODO: we need to port the below:
+
+	1. (byte)(this.dvce_ismobile ? 1 : 0);
+
+  2. Turning a List of items into a Hive
+
+  See:
+
+  http://www.nearinfinity.com/blogs/stephen_mouring_jr/2013/01/04/writing-hive-tables-from-mapreduce.html
+
+  */
+}
