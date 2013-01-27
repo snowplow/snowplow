146182a6998c6d4583674985fb278af5f6f2e504
Yali Sassoon <yali.sassoon@keplarllp.com>
2013-01-27 16:04:02 +0000

    updated tracker ui with jquery to give it more polish

--- 1-trackers/no-js-tracker/js/no-js-tracker.js
+++ 1-trackers/no-js-tracker/js/no-js-tracker.js
@@ -1,34 +1,61 @@
 $(function() {
+	var trackerVersion = 'no-js-0.0.1';
+
+	/**
+	 * On page load... 
+	 */
+	// 1. Display the title including tracker version
+	$('#title').append($('<h1>No-Javascript Tracker for SnowPlow</h1><h2>No-JS tracking tag generator version ' + trackerVersion + '</h2>'));
+	
+	// 2. Set the radio buttons to default options i.e. scheme = HTTP and collectorType = cloudfront...
+	$('input:radio[name=pageScheme]:nth(0)').attr('checked',true);
+	$('input:radio[name=collectorType]:nth(0)').attr('checked',true);
+	
+	// 3.  Hide the div with the other collector input field
+	$('#other-collector-div').hide();
+
+	/**
+	 * When a user selects 'other collector type' then hide the input fields that are only relevant for the 
+	 * Cloudfront collector and display the fields that are relevant for the self-hosted collector type
+	 */
+	$("#otherCollectorType").click(function(){
+		$('#other-collector-div').show();
+		$('#cloudfront-collector-div').hide();
+	});
+
+	/**
+	 * Conversley, when a user selects Cloudfront collector type then show the relevant fields and hide the irrelevant fields
+	 */
+	$("#cloudfrontCollectorType").click(function(){
+		$('#other-collector-div').hide();
+		$('#cloudfront-collector-div').show();
+	});
+
 	/**
 	 * The following function generates the embed code when the visitor clicks on the "Generate embed code" button
 	 */
 	$(".button").click(function() {
 		// First clear the output div of any contents (e.g. from tags that were inserted last time the form was submitted)
 		$('#output').html("");
-		alert('starting function!');
-
+		
 		// Now pass the variables in each of the fields into the Javascript, so we can use them to generate the tracking tag
 		var applicationId = $("input#applicationId").val();
-		var pageScheme = $("input#pageScheme").val();
+		// var pageScheme = $("input#pageScheme").val();
+		var pageScheme = $('input:radio[name=pageScheme]:checked').val();
 		
 		var pageTitle = $("input#pageTitle").val();
 		var pageUrl = $("input#pageUrl").val();
 
-		var isCloudfrontCollector
-		if ($("input#is-cloudfront-collector").is(':checked')) {
-			isCloudfrontCollector = true;
-		} else {
-			isCloudfrontCollector = false
-		}
-
+		// var collectorType = $("input#collectorType").val();
+		var collectorType = $('input:radio[name=collectorType]:checked').val();
+		
 		var cloudfrontSubdomain = $("input#cloudfrontSubdomain").val();
 		var selfHostedCollectorUrl = $("input#selfHostedCollectorUrl").val();
 	
-		alert('variables grabbed');
-
-		var embedCode = generateNoJsTag(applicationId, pageScheme, pageTitle, pageUrl, isCloudfrontCollector, cloudfrontSubdomain, selfHostedCollectorUrl);
+		
+		var embedCode = generateNoJsTag(applicationId, pageScheme, pageTitle, pageUrl, collectorType, cloudfrontSubdomain, selfHostedCollectorUrl);
 		alert('embed code generated');
-		$('#output').append($('<h3>The No-JS tracking tag for this page is:</h3><br /><br /><h2>' + embedCode + '</h2>'));
+		$('#output').append($('<h3>The No-JS tracking tag for this page is:</h3><h2>' + embedCode + '</h2>'));
 
 		// Return false because we do NOT want the page to reload. 
 		// (Which would cause the values entered in the fields to be lost, and the embed code to disappear)
@@ -38,11 +65,11 @@ $(function() {
 	/**
 	 * Generates the tag, based on the values inputted on the form above
 	 */
-	function generateNoJsTag(appId, pageScheme, pageTitle, pageUrl, isCloudfrontCollector, cloudFrontSubDomain, collectorDomain ){
+	function generateNoJsTag(appId, pageScheme, pageTitle, pageUrl, collectorType, cloudFrontSubDomain, collectorDomain ){
 		// 1st, let's set the endpoint
 		var configCollectorUrl;
 
-		if (isCloudfrontCollector) {
+		if (collectorType == 'cloudfront') {
 			configCollectorUrl = collectorUrlFromCfDist(cloudFrontSubDomain, pageScheme);
 		} else {
 			configCollectorUrl = asCollectorUrl(collectorDomain, pageScheme);
