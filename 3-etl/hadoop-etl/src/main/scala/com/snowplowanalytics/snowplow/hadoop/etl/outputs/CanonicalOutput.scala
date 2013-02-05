906a5f4d9c3210a16242ee41dc946c5e311f033d
Alex Dean <alexander.dean@keplarllp.com>
2013-02-05 14:09:25 +0000

    Updated so each field has a bean setter

--- 3-etl/hadoop-etl/src/main/scala/com/snowplowanalytics/snowplow/hadoop/etl/outputs/CanonicalOutput.scala
+++ 3-etl/hadoop-etl/src/main/scala/com/snowplowanalytics/snowplow/hadoop/etl/outputs/CanonicalOutput.scala
@@ -35,102 +35,102 @@ import scala.reflect.BeanProperty
 class CanonicalOutput {
 
   // The application (site, game, app etc) this event belongs to, and the tracker platform
-  var app_id: String = _
-  var platform: String = _
+  @BeanProperty var app_id: String = _
+  @BeanProperty var platform: String = _
 
   // Date/time
-  var dt: String = _
-  var tm: String = _
+  @BeanProperty var dt: String = _
+  @BeanProperty var tm: String = _
 
   // Transaction (i.e. this logging event)
-  var event: String = _
-  var event_id: String = _
-  var txn_id: String = _
+  @BeanProperty var event: String = _
+  @BeanProperty var event_id: String = _
+  @BeanProperty var txn_id: String = _
 
   // Versioning
-  var v_tracker: String = _
-  var v_collector: String = _
-  var v_etl: String = _
+  @BeanProperty var v_tracker: String = _
+  @BeanProperty var v_collector: String = _
+  @BeanProperty var v_etl: String = _
 
   // User and visit
-  var user_id: String = _
-  var user_ipaddress: String = _
-  var user_fingerprint: String = _
-  var visit_id: Int = _
+  @BeanProperty var user_id: String = _
+  @BeanProperty var user_ipaddress: String = _
+  @BeanProperty var user_fingerprint: String = _
+  @BeanProperty var visit_id: Int = _
 
   // Page
-  var page_url: String = _
-  var page_title: String = _
-  var page_referrer: String = _
+  @BeanProperty var page_url: String = _
+  @BeanProperty var page_title: String = _
+  @BeanProperty var page_referrer: String = _
 
   // Marketing
-  var mkt_medium: String = _
-  var mkt_source: String = _
-  var mkt_term: String = _
-  var mkt_content: String = _
-  var mkt_campaign: String = _
+  @BeanProperty var mkt_medium: String = _
+  @BeanProperty var mkt_source: String = _
+  @BeanProperty var mkt_term: String = _
+  @BeanProperty var mkt_content: String = _
+  @BeanProperty var mkt_campaign: String = _
 
   // Event
-  var ev_category: String = _
-  var ev_action: String = _
-  var ev_label: String = _
-  var ev_property: String = _
-  var ev_value: String = _
+  @BeanProperty var ev_category: String = _
+  @BeanProperty var ev_action: String = _
+  @BeanProperty var ev_label: String = _
+  @BeanProperty var ev_property: String = _
+  @BeanProperty var ev_value: String = _
 
   // User Agent
-  var useragent: String = _
+  @BeanProperty var useragent: String = _
 
   // Browser (from user-agent)
-  var br_name: String = _
-  var br_family: String = _
-  var br_version: String = _
-  var br_type: String = _
-  var br_renderengine: String = _
+  @BeanProperty var br_name: String = _
+  @BeanProperty var br_family: String = _
+  @BeanProperty var br_version: String = _
+  @BeanProperty var br_type: String = _
+  @BeanProperty var br_renderengine: String = _
 
   // Browser (from querystring)
-  var br_lang: String = _
+  @BeanProperty var br_lang: String = _
   // Individual feature fields for non-Hive targets (e.g. Infobright)
-  var br_features_pdf: Byte = _
-  var br_features_flash: Byte = _
-  var br_features_java: Byte = _
-  var br_features_director: Byte = _
-  var br_features_quicktime: Byte = _
-  var br_features_realplayer: Byte = _
-  var br_features_windowsmedia: Byte = _
-  var br_features_gears: Byte = _
-  var br_features_silverlight: Byte = _
-  var br_cookies: Byte = _
-  var br_colordepth: String = _
+  @BeanProperty var br_features_pdf: Byte = _
+  @BeanProperty var br_features_flash: Byte = _
+  @BeanProperty var br_features_java: Byte = _
+  @BeanProperty var br_features_director: Byte = _
+  @BeanProperty var br_features_quicktime: Byte = _
+  @BeanProperty var br_features_realplayer: Byte = _
+  @BeanProperty var br_features_windowsmedia: Byte = _
+  @BeanProperty var br_features_gears: Byte = _
+  @BeanProperty var br_features_silverlight: Byte = _
+  @BeanProperty var br_cookies: Byte = _
+  @BeanProperty var br_colordepth: String = _
 
   // OS (from user-agent)
-  var os_name: String = _
-  var os_family: String = _
-  var os_manufacturer: String = _
-  var os_timezone: String = _
+  @BeanProperty var os_name: String = _
+  @BeanProperty var os_family: String = _
+  @BeanProperty var os_manufacturer: String = _
+  @BeanProperty var os_timezone: String = _
 
   // Device/Hardware (from user-agent)
-  var dvce_type: String = _
-  var dvce_ismobile: Byte = _
+  @BeanProperty var dvce_type: String = _
+  @BeanProperty var dvce_ismobile: Byte = _
 
   // Device (from querystring)
-  var dvce_screenwidth: Int = _
-  var dvce_screenheight: Int = _
+  @BeanProperty var dvce_screenwidth: Int = _
+  @BeanProperty var dvce_screenheight: Int = _
 
   // Ecommerce transaction (from querystring)
-  var tr_orderid: String = _
-  var tr_affiliation: String = _
-  var tr_total: String = _
-  var tr_tax: String = _
-  var tr_shipping: String = _
-  var tr_city: String = _
-  var tr_state: String = _
-  var tr_country: String = _
+  @BeanProperty var tr_orderid: String = _
+  @BeanProperty var tr_affiliation: String = _
+  @BeanProperty var tr_total: String = _
+  @BeanProperty var tr_tax: String = _
+  @BeanProperty var tr_shipping: String = _
+  @BeanProperty var tr_city: String = _
+  @BeanProperty var tr_state: String = _
+  @BeanProperty var tr_country: String = _
 
   // Ecommerce transaction item (from querystring)
-  var ti_orderid: String = _
-  var ti_sku: String = _
-  var ti_name: String = _
-  var ti_category: String = _
-  var ti_price: String = _
-  var ti_quantity: String = _
+  @BeanProperty var ti_orderid: String = _
+  @BeanProperty var ti_sku: String = _
+  @BeanProperty var ti_name: String = _
+  @BeanProperty var ti_category: String = _
+  @BeanProperty var ti_price: String = _
+  @BeanProperty var ti_quantity: String = _
 }
