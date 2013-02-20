a0229724ba0feb841743f1eaa5a22a69f606828c
Alex Dean <alexander.dean@keplarllp.com>
2013-02-20 15:29:55 +0000

    Updated table def

--- 4-storage/infobright-storage/sql/table-def.sql
+++ 4-storage/infobright-storage/sql/table-def.sql
@@ -17,5 +17,108 @@
 -- License:     Apache License Version 2.0
 
 CREATE TABLE IF NOT EXISTS events_007 (
-	-- TODO: update with final events_007 defn
+	-- App
+	`app_id` varchar(255) comment 'lookup', -- 'lookup' is a varchar optimisation for Infobright
+	`platform` varchar(50) comment 'lookup',
+	-- Date/time
+	`collector_dt` date, -- Renamed in 0.0.7
+	`collector_tm` time, -- Renamed in 0.0.7
+	`dvce_dt` date, -- Added in 0.0.7
+	`dvce_tm` time, -- Added in 0.0.7
+	`dvce_epoch` bigint, -- Added in 0.0.7
+	-- Event
+	`event` varchar(128) comment 'lookup', -- Size reduced in 0.0.7
+	`event_vendor` varchar(128) comment 'lookup',
+	`event_id` varchar(38),
+	`txn_id` int,
+	-- Versioning
+	`v_tracker` varchar(100) comment 'lookup',
+	`v_collector` varchar(100) comment 'lookup',
+	`v_etl` varchar(100) comment 'lookup',
+	-- User and visit
+	`user_id` varchar(255), -- Size increased in 0.0.7
+	`user_ipaddress` varchar(19),
+	`user_fingerprint` varchar(50),
+	`domain_userid` varchar(16),  -- Added in 0.0.7
+	`domain_sessionidx` smallint, -- Renamed in 0.0.7
+	`network_userid` varchar(38), -- Added in 0.0.7
+	-- Page
+	`page_url` varchar(3000),
+	`page_title` varchar(2000),
+	`page_referrer` varchar(3000),
+	-- Page URL components
+	`page_urlscheme` varchar(16),
+	`page_urlhost` varchar(255),
+	`page_urlport` smallint,
+	`page_urlpath` varchar(1000),
+	`page_urlquery` varchar(3000),
+	`page_urlfragment` varchar(255),
+	-- Marketing
+	`mkt_source` varchar(255),
+	`mkt_medium` varchar(255),
+	`mkt_term` varchar(255),
+	`mkt_content` varchar(500),
+	`mkt_campaign` varchar(255),
+	-- Custom Event
+	`ev_category` varchar(255),
+	`ev_action` varchar(255),
+	`ev_label` varchar(255),
+	`ev_property` varchar(255),
+	`ev_value` float,
+	-- Ecommerce
+	`tr_orderid` varchar(255),
+	`tr_affiliation` varchar(255),
+	`tr_total` dec(18,2),
+	`tr_tax` dec(18,2),
+	`tr_shipping` dec(18,2),
+	`tr_city` varchar(255),
+	`tr_state` varchar(255),
+	`tr_country` varchar(255),
+	`ti_orderid` varchar(255),
+	`ti_sku` varchar(255),
+	`ti_name` varchar(255),
+	`ti_category` varchar(255),
+	`ti_price` dec(18,2),
+	`ti_quantity` int,
+	-- Page ping
+	`pp_xoffset_min` mediumint,
+	`pp_xoffset_max` mediumint,
+	`pp_yoffset_min` mediumint,
+	`pp_yoffset_max` mediumint,
+	-- User Agent
+	`useragent` varchar(500),
+	-- Browser
+	`br_name` varchar(50) comment 'lookup',
+	`br_family` varchar(50) comment 'lookup',
+	`br_version` varchar(50) comment 'lookup',
+	`br_type` varchar(50) comment 'lookup',
+	`br_renderengine` varchar(50) comment 'lookup',
+	`br_lang` varchar(255) comment 'lookup',
+	`br_features_pdf` tinyint(1),
+	`br_features_flash` tinyint(1),
+	`br_features_java` tinyint(1),
+	`br_features_director` tinyint(1),
+	`br_features_quicktime` tinyint(1),
+	`br_features_realplayer` tinyint(1),
+	`br_features_windowsmedia` tinyint(1),
+	`br_features_gears` tinyint(1) ,
+	`br_features_silverlight` tinyint(1),
+	`br_cookies` tinyint(1),
+	`br_colordepth` varchar(12) comment 'lookup',
+	`br_viewwidth` mediumint,
+	`br_viewheight` mediumint,
+	-- Operating System
+	`os_name` varchar(50) comment 'lookup',
+	`os_family` varchar(50) comment 'lookup',
+	`os_manufacturer` varchar(50) comment 'lookup',
+	`os_timezone` varchar(255) comment 'lookup',
+	-- Device/Hardware
+	`dvce_type` varchar(50) comment 'lookup',
+	`dvce_ismobile` tinyint(1),
+	`dvce_screenwidth` mediumint,
+	`dvce_screenheight` mediumint,
+	-- Document
+	`doc_charset` varchar(128),
+	`doc_width` mediumint,
+	`doc_height` mediumint
 ) ENGINE=BRIGHTHOUSE DEFAULT CHARSET=utf8 ;
