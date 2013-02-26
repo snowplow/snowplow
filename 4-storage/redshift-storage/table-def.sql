31e56dfdbe4e6f5e3b0f0c781b1715851e32f214
Alex Dean <alexander.dean@keplarllp.com>
2013-02-26 15:30:52 +0000

    Added in DISTSTYLE etc

--- 4-storage/redshift-storage/table-def.sql
+++ 4-storage/redshift-storage/table-def.sql
@@ -1,3 +1,21 @@
+-- Copyright (c) 2013 SnowPlow Analytics Ltd. All rights reserved.
+--
+-- This program is licensed to you under the Apache License Version 2.0,
+-- and you may not use this file except in compliance with the Apache License Version 2.0.
+-- You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the Apache License Version 2.0 is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+--
+-- Version:     0.0.1
+-- URL:         -
+--
+-- Authors:     Yali Sassoon, Alex Dean
+-- Copyright:   Copyright (c) 2013 SnowPlow Analytics Ltd
+-- License:     Apache License Version 2.0
+
 CREATE TABLE events (
 	-- App
 	app_id varchar(255) encode text255, 
@@ -99,4 +117,7 @@ CREATE TABLE events (
 	doc_charset varchar(128) encode text255,
 	doc_width integer,
 	doc_height integer
-);
+)
+DISTSTYLE KEY
+DISTKEY domain_userid
+SORTKEY collector_tstamp;
