795556b41a5c01bdab93ff5f029a98731531e1e2
Alex Dean <alexander.dean@keplarllp.com>
2013-02-27 15:46:59 +0000

    Added analyze and vacuum in

--- 4-storage/storage-loader/lib/snowplow-storage-loader/redshift_loader.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/redshift_loader.rb
@@ -30,29 +30,53 @@ module SnowPlow
         puts "Loading SnowPlow events into Redshift..."
 
         # Assemble the relevant parameters for the bulk load query
-        jdbc_url = "jdbc:postgresql://#{config[:storage][:endpoint]}:#{config[:storage][:port]}/#{config[:storage][:database]}"
         credentials = "aws_access_key_id=#{config[:aws][:access_key_id]};aws_secret_access_key=#{config[:aws][:secret_access_key]}"
-        query = "copy '#{config[:storage][:table]}' from '#{config[:s3][:buckets][:in]}' credentials '#{credentials}' delimiter '#{EVENT_FIELD_SEPARATOR}';"
+        queries = ["copy #{config[:storage][:table]} from '#{config[:s3][:buckets][:in]}' credentials '#{credentials}' delimiter '#{EVENT_FIELD_SEPARATOR}'",
+                   "analyze",
+                   "vacuum"]
+
+        status = execute_queries(config, queries)
+        unless status == []
+          raise DatabaseLoadError, "Error code #{status[1]} executing #{status[0]}: #{status[2]}"
+        end 
+      end
+      module_function :load_events
+
+      private
+
+      # Execute a chain of SQL commands, stopping as soon as
+      # an error is encountered. At that point, it returns a
+      # 'tuple' of the error code, stdout/err and command
+      # that caused the error
+      #
+      # Parameters:
+      # +config+:: the hash of configuration options
+      # +queries+:: the Redshift queries to execute sequentially
+      #
+      # Returns either an empty list on success, or on failure
+      # a list of the form [query, ret_val, stdout_err]
+      def execute_queries(config, queries)
+
+        jdbc_url = "jdbc:postgresql://#{config[:storage][:endpoint]}:#{config[:storage][:port]}/#{config[:storage][:database]}"
         classpath = "%s/jisql-2.0.11.jar:%s/jopt-simple-3.2.jar:%s/postgresql-8.4-703.jdbc4.jar" % ([JISQL_PATH] * 3)
         username = config[:storage][:username]
         password = config[:storage][:password]
-           
-        # Execute the following request at the command line:
-        jisql_cmd = %Q!java -cp #{classpath} com.xigole.util.sql.Jisql -driver postgresql -cstring #{jdbc_url} -user #{username} -password #{password} -c \\; -query "#{query}"!
 
-        puts ">>>>>>>>>>"
-        puts jisql_cmd
-        puts ">>>>>>>>>>"
+        status = []
+        queries.each do |q|
+          jisql_cmd = %Q!java -cp #{classpath} com.xigole.util.sql.Jisql -driver postgresql -cstring #{jdbc_url} -user #{username} -password #{password} -c \\; -query "#{q};"!
+          stdout_err = `#{jisql_cmd} 2>&1` # Execute
+          ret_val = $?.to_i
 
-        stdout_err = `#{jisql_cmd} 2>&1` # Execute
-        ret_val = $?.to_i
-
-        # Error handling
-        unless ret_val == 0
-          raise DatabaseLoadError, "Error code #{ret_val}: #{stdout_err}"
+          unless ret_val == 0
+            status = [q, ret_val, stdout_err]
+            break
+          end
         end
+
+        return status
       end
-      module_function :load_events
+      module_function
 
     end
   end
