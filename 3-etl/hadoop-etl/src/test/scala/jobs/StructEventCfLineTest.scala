9f4f3ed76042b69696e700a75ae995280ad32cdc
Alex Dean <alexander.dean@keplarllp.com>
2013-03-25 17:18:31 +0000

    Added a test for a structured event

--- 3-etl/hadoop-etl/src/test/scala/jobs/StructEventCfLineTest.scala
+++ 3-etl/hadoop-etl/src/test/scala/jobs/StructEventCfLineTest.scala
@@ -31,35 +31,35 @@ import JobTestHelpers._
 /**
  * Integration test for the EtlJob:
  *
- * Check that all tuples in a raw page view event
+ * Check that all tuples in a custom structured event
  * (CloudFront format) are successfully extracted.
  */
-class PageViewCfLineTest extends Specification with TupleConversions {
+class StructEventCfLineTest extends Specification with TupleConversions {
 
   val input = Lines(
-    "2012-05-24  00:06:42  LHR5  3402  90.194.12.51  GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla/5.0%20(iPhone;%20CPU%20iPhone%20OS%205_1_1%20like%20Mac%20OS%20X)%20AppleWebKit/534.46%20(KHTML,%20like%20Gecko)%20Version/5.1%20Mobile/9B206%20Safari/7534.48.3  &e=pv&page=Psychic%20Bazaar&dtm=1364219529188&tid=637309&vp=2560x935&ds=2543x1273&vid=41&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2Freaders%2F"
+    "2012-05-27  11:35:53  DFW3  3343  99.116.172.58 GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla/5.0%20(Windows%20NT%206.1;%20WOW64;%20rv:12.0)%20Gecko/20100101%20Firefox/12.0  ?&e=se&ev_ca=ecomm&ev_ac=add-to-basket&ev_la=PBZ00110&ev_pr=1&ev_va=13.99&dtm=1364230969450&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards"
     )
 
   val expected = List(
     "pbzsite",
     "web",
-    "2012-05-24 00:06:42.000",
-    "2013-03-25 13:52:09.188",
-    "page_view",
+    "2012-05-27 11:35:53.000",
+    "2013-03-25 17:02:49.450",
+    "struct",
     "com.snowplowanalytics",
     null, // We can't predict the event_id
-    "637309",
+    "598951",
     "js-0.11.1",
     "cloudfront",
     "hadoop-0.1.0",
     null, // No user_id set
-    "90.194.12.51",
+    "99.116.172.58",
     "2876815413",
     "9795bd0203804cd1",
-    "41",
+    "43",
     null, // No network_userid set
     // Raw page URL is discarded 
-    "Psychic Bazaar",
+    null,
     "http://www.psychicbazaar.com/",
     "http",
     "www.psychicbazaar.com",
@@ -72,11 +72,11 @@ class PageViewCfLineTest extends Specification with TupleConversions {
     null, //
     null, //
     null, //
-    null, // Event fields empty
-    null, //
-    null, //
-    null, //
-    null, //
+    "ecomm",
+    "add-to-basket",
+    "PBZ00110",
+    "1",
+    "13.99",
     null, // Transaction fields empty 
     null, //
     null, //
@@ -91,12 +91,12 @@ class PageViewCfLineTest extends Specification with TupleConversions {
     null, //
     null, //
     null, //
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3",
-    "Mobile Safari",
-    "Safari",
-    "5.1",
-    "Browser (mobile)",
-    "WEBKIT",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0",
+    "Firefox 12",
+    "Firefox",
+    "12.0",
+    "Browser",
+    "GECKO",
     null, // Page ping fields are empty
     null, //
     null, //
@@ -114,10 +114,10 @@ class PageViewCfLineTest extends Specification with TupleConversions {
     "1",
     "32",
     "2560",
-    "935",
-    "Mac OS",
-    "Mac OS",
-    "Apple Inc.",
+    "934",
+    "Windows",
+    "Windows",
+    "Microsoft Corporation",
     "Europe/London",
     "Computer",
     "0",
@@ -125,14 +125,14 @@ class PageViewCfLineTest extends Specification with TupleConversions {
     "1440",
     "UTF-8",
     "2543",
-    "1273"
+    "1420"
     )
 
-  "A job which processes a CloudFront file containing 1 valid page view event" should {
+  "A job which processes a CloudFront file containing 1 valid custom structured event" should {
     EtlJobTest.
       source(MultipleTextLineFiles("inputFolder"), input).
       sink[TupleEntry](Tsv("outputFolder")){ buf : Buffer[TupleEntry] =>
-        "correctly output a page_view event" in {
+        "correctly output a struct event" in {
           buf.size must_== 1
           val actual = buf.head
           for (idx <- expected.indices) {
