55b9c26b3a12b972300f0386c32782327359ff96
Yali Sassoon <yali.sassoon@keplarllp.com>
2013-04-04 11:32:30 +0100

    Updated properties so can now handle booleans Making very slow progress on the testing harness

--- 4-storage/avro-storage/src/main/java/TestUser.java
+++ 4-storage/avro-storage/src/main/java/TestUser.java
@@ -1,16 +1,22 @@
 package com.snowplowanalytics.snowplow.storage.avro;
 
-import com.snowplowanalytics.snowplow.storage.avro.schema.user;
+import com.snowplowanalytics.snowplow.storage.avro.schema.User;
+import org.apache.avro.io.*;
+import org.apache.avro.specific.*;
+import org.apache.avro.file.*;
+import java.io.File;
 
 
 public class TestUser {
 	int i;
 
-	public static void run() {
-		User user1 = new user();
+	void run() {
+		User user1 = new User();
 		user1.setName("Alyssa");
 		user1.setFavoriteNumber(256);
 		// Leave favorite color null
+
+		System.out.println("Hello World!");
 
 		// alternate constructor
 		User user2 = new User("Ben", 7, "red");
@@ -21,5 +27,15 @@ public class TestUser {
              .setFavoriteColor("blue")
              .setFavoriteNumber(null)
              .build();
+
+        // Serialize user1 and user2 to disk
+		File file = new File("users.avro");
+		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+		dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+		dataFileWriter.append(user1);
+		dataFileWriter.append(user2);
+		dataFileWriter.append(user3);
+		dataFileWriter.close();
 	}
 }
