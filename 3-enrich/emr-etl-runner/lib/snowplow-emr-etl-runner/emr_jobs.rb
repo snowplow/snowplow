41172a31e694de16a6dbcb53c181195307672496
Alex Dean <alexander.dean@keplarllp.com>
2013-05-15 11:08:57 +0100

    Tidied up message

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/emr_jobs.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/emr_jobs.rb
@@ -123,7 +123,7 @@ module SnowPlow
         status = wait_for(jobflow_id)
 
         if !status
-          raise EmrExecutionError, "EMR jobflow #{jobflow_id} failed, check Amazon EMR console and Hadoop logs for details (or try: https://github.com/snowplow/snowplow/wiki/Troubleshooting#wiki-etl-failure). Data files not archived."
+          raise EmrExecutionError, "EMR jobflow #{jobflow_id} failed, check Amazon EMR console and Hadoop logs for details (help: https://github.com/snowplow/snowplow/wiki/Troubleshooting#wiki-etl-failure). Data files not archived."
         end
 
         puts "EMR jobflow #{jobflow_id} completed successfully."
