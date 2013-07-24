d5d675c970917469a54a25d1fdda9e4e5107d523
Alex Dean <alexander.dean@keplarllp.com>
2013-07-24 20:33:22 +0100

    Fixed capitalization of "Snowplow Analytics"

--- 4-storage/storage-loader/lib/snowplow-storage-loader/redshift_loader.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/redshift_loader.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2013 SnowPlow Analytics Ltd. All rights reserved.
+# Copyright (c) 2012-2013 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0,
 # and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,7 +10,7 @@
 # See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
 # Author::    Alex Dean (mailto:support@snowplowanalytics.com)
-# Copyright:: Copyright (c) 2012-2013 SnowPlow Analytics Ltd
+# Copyright:: Copyright (c) 2012-2013 Snowplow Analytics Ltd
 # License::   Apache License Version 2.0
 
 require 'pg'
