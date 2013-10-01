6e12e035ae89a360b3ee64bb455ed109aa3a53b3
Yali Sassoon <yali.sassoon@keplarllp.com>
2013-10-01 13:20:04 +0100

    added additional customer recipes

--- 4-storage/redshift-storage/sql/recipes/recipes-customers.sql
+++ 4-storage/redshift-storage/sql/recipes/recipes-customers.sql
@@ -15,6 +15,7 @@ GROUP BY 1,2,3,4,5
 ORDER BY 1,2,3,4,5;
 
 
+-- CUSTOMER LIFETIME VALUE
 -- Total transaction value by domain_userid over time
 CREATE VIEW total_transaction_value_by_user AS
 SELECT
@@ -29,3 +30,59 @@ FROM (
 	WHERE event='transaction'
 	GROUP BY 1,2,3 ) AS t -- deduped transaction table
 GROUP BY 1
+
+
+-- MEASURING USER ENGAGEMENT
+-- Users by number of days per month visit website
+CREATE VIEW engagement_users_by_days_per_month_visit_website AS
+SELECT
+"Month",
+"Days_visited_website",
+COUNT(domain_userid) AS "Frequency"
+FROM (
+	SELECT
+	DATE_TRUNC('month', collector_tstamp) AS "Month",
+	domain_userid,
+	COUNT(DISTINCT(DATE_TRUNC('day', collector_tstamp))) AS "Days_visited_website"
+	FROM "atomic".events
+	GROUP BY 1,2 ) t
+GROUP BY 1,2
+ORDER BY 1,2;
+
+
+-- Users by number of days per week visit website
+CREATE VIEW engagement_users_by_days_per_week_visit_website AS
+SELECT
+"Week",
+"Days_visited_website",
+COUNT(domain_userid) AS "Frequency"
+FROM (
+	SELECT
+	DATE_TRUNC('week', collector_tstamp) AS "Week",
+	domain_userid,
+	COUNT(DISTINCT(DATE_TRUNC('day', collector_tstamp))) AS "Days_visited_website"
+	FROM "atomic".events
+	GROUP BY 1,2 ) t
+GROUP BY 1,2
+ORDER BY 1,2;
+
+
+-- Users by number of visits per month
+CREATE VIEW engagement_users_by_visits_per_month
+SELECT
+"Month",
+"Visits_per_month",
+COUNT(*) AS "Frequency"
+FROM (
+	SELECT
+	DATE_TRUNC('month', collector_tstamp) AS "Month",
+	domain_userid,
+	COUNT(DISTINCT(domain_sessionidx)) AS "Visits_per_month"
+	FROM "atomic".events
+	GROUP BY 1,2 
+) t
+GROUP BY 1,2
+ORDER BY 1,2;
+
+-- Users by number of visits per week
+
