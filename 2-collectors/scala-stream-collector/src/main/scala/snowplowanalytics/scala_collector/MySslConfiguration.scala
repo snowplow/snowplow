fdb842fb25b3b4f781e3bf8118a69bc1d3d804b6
Brandon Amos <bdamos@vt.edu>
2013-12-17 06:11:58 -0500

    Scala Collector: Add spray 'bootstrap' code.

---
+++ 2-collectors/scala-stream-collector/src/main/scala/snowplowanalytics/scala_collector/MySslConfiguration.scala
@@ -0,0 +1,37 @@
+package spray.examples
+
+import java.security.{SecureRandom, KeyStore}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+import spray.io._
+
+// for SSL support (if enabled in application.conf)
+trait MySslConfiguration {
+
+  // if there is no SSLContext in scope implicitly the HttpServer uses the default SSLContext,
+  // since we want non-default settings in this example we make a custom SSLContext available here
+  implicit def sslContext: SSLContext = {
+    val keyStoreResource = "/ssl-test-keystore.jks"
+    val password = ""
+
+    val keyStore = KeyStore.getInstance("jks")
+    keyStore.load(getClass.getResourceAsStream(keyStoreResource), password.toCharArray)
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(keyStore, password.toCharArray)
+    val trustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    trustManagerFactory.init(keyStore)
+    val context = SSLContext.getInstance("TLS")
+    context.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom)
+    context
+  }
+
+  // if there is no ServerSSLEngineProvider in scope implicitly the HttpServer uses the default one,
+  // since we want to explicitly enable cipher suites and protocols we make a custom ServerSSLEngineProvider
+  // available here
+  implicit def sslEngineProvider: ServerSSLEngineProvider = {
+    ServerSSLEngineProvider { engine =>
+      engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA"))
+      engine.setEnabledProtocols(Array("SSLv3", "TLSv1"))
+      engine
+    }
+  }
+}
