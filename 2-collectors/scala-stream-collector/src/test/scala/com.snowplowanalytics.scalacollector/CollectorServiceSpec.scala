1605984e0439ed64ad7206b1f820d0600b9b333d
Brandon Amos <bdamos@vt.edu>
2013-12-27 07:16:49 -0500

    Scala Collector: Add test to verify requests are being stored in Kinesis.

--- 2-collectors/scala-stream-collector/src/test/scala/com.snowplowanalytics.scalacollector/CollectorServiceSpec.scala
+++ 2-collectors/scala-stream-collector/src/test/scala/com.snowplowanalytics.scalacollector/CollectorServiceSpec.scala
@@ -18,7 +18,7 @@ package com.snowplowanalytics.scalacollector
 // specs2 and spray testing libraries.
 import org.specs2.matcher.AnyMatchers
 import org.specs2.mutable.Specification
-import org.specs2.specification.Scope
+import org.specs2.specification.{Scope,Fragments}
 import spray.testkit.Specs2RouteTest
 
 // Spray classes.
@@ -27,9 +27,23 @@ import spray.http.HttpHeaders.{Cookie,`Set-Cookie`,`Remote-Address`}
 
 import scala.collection.mutable.MutableList
 
+import org.specs2.specification.Step
+
+// Trait allowing an entire Specification to be wrapped
+// with code executed before and after all tests have run.
+// http://stackoverflow.com/questions/16936811
+trait BeforeAllAfterAll extends Specification {
+  // see http://bit.ly/11I9kFM (specs2 User Guide)
+  override def map(fragments: =>Fragments) = 
+    Step(beforeAll) ^ fragments ^ Step(afterAll)
+
+  protected def beforeAll()
+  protected def afterAll()
+}
+
 // http://spray.io/documentation/1.2.0/spray-testkit/
 class CollectorServiceSpec extends Specification with Specs2RouteTest with
-     CollectorService with AnyMatchers {
+     CollectorService with AnyMatchers with BeforeAllAfterAll {
   def actorRefFactory = system
 
   // By default, spray will always add Remote-Address to every request
@@ -44,13 +58,27 @@ class CollectorServiceSpec extends Specification with Specs2RouteTest with
     Get(uri).withHeaders(headers.toList)
   }
 
-  "Snowplow's Scala collector" should {
-    "return an invisible pixel." in new InitKinesis {
-      CollectorGet("/i") ~> collectorRoute ~> check {
-          responseAs[Array[Byte]] === Responses.pixel
-        }
+  def beforeAll() {
+    if (!KinesisInterface.createAndLoadStream()) {
+      throw new RuntimeException("Unable to initialize Kinesis stream.")
     }
-    "return a cookie expiring at the correct time." in new InitKinesis {
+  }
+
+  def afterAll() {
+    KinesisInterface.deleteStream()
+  }
+
+  // Don't run tests in parallel because KinesisInterface
+  // is not thread safe (currently).
+  sequential
+
+  "Snowplow's Scala collector" should {
+    "return an invisible pixel." in {
+      CollectorGet("/i") ~> collectorRoute ~> check {
+        responseAs[Array[Byte]] === Responses.pixel
+      }
+    }
+    "return a cookie expiring at the correct time." in {
       CollectorGet("/i") ~> collectorRoute ~> check {
         headers must not be empty
 
@@ -72,7 +100,7 @@ class CollectorServiceSpec extends Specification with Specs2RouteTest with
         offset.asInstanceOf[Int] must beCloseTo(0, 2000) // 1000 ms window.
       }
     }
-    "return the same cookie as passed in." in new InitKinesis {
+    "return the same cookie as passed in." in {
       CollectorGet("/i", Some(HttpCookie("sp", "UUID_Test"))) ~>
           collectorRoute ~> check {
         val httpCookies: List[HttpCookie] = headers.collect {
@@ -86,15 +114,23 @@ class CollectorServiceSpec extends Specification with Specs2RouteTest with
         httpCookie.content must beEqualTo("UUID_Test")
       }
     }
-    "store multiple requests in Kinesis." in new InitKinesis {
-      // TODO
-    }
-  }
-}
+    "store a request in Kinesis." in {
+      CollectorGet("/i?payload=true") ~> collectorRoute ~> check {
+        // Sleep to let Kinesis store the data.
+        Thread.sleep(10000)
 
-trait InitKinesis extends Scope {
-  // TODO: Clear the Kinesis stream.
-  if (!KinesisInterface.createStream()) {
-    throw new RuntimeException("Unable to initialize Kinesis stream.")
+        // TODO: Might not correctly retrieve data due to a bug.
+        // https://github.com/cloudify/scalazon/issues/5
+        val kinesisRecords = KinesisInterface.getRecords()
+        println("Store: " + kinesisRecords.toString)
+        val recordWithPayload = kinesisRecords.filter (
+          record =>
+            (record._1.payload != null &&
+             record._1.payload.data.equals("payload=true"))
+        )
+
+        recordWithPayload must not be empty
+      }
+    }
   }
 }
