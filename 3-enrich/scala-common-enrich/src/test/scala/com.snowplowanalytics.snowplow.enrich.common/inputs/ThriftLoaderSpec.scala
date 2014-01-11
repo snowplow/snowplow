fd5595660f763546434be778dba8a4ede3076118
Alex Dean <alex@snowplowanalytics.com>
2014-01-11 15:49:49 +0000

    ThriftLoader is now an object

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/inputs/ThriftLoaderSpec.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/inputs/ThriftLoaderSpec.scala
@@ -13,7 +13,19 @@
 package com.snowplowanalytics.snowplow.enrich.common
 package inputs
 
-// Snowplow.
+// Commons Codec
+import org.apache.commons.codec.binary.Base64
+
+// Joda-Time
+import org.joda.time.DateTime
+
+// Thrift
+import org.apache.thrift.TDeserializer
+
+// Specs2
+import org.specs2.mutable._
+
+// Snowplow
 import com.snowplowanalytics.snowplow.collectors.thrift.{
   SnowplowRawEvent,
   TrackerPayload => ThriftTrackerPayload,
@@ -21,28 +33,14 @@ import com.snowplowanalytics.snowplow.collectors.thrift.{
   PayloadFormat
 }
 
-// Commons Codec.
-import org.apache.commons.codec.binary.Base64
-
-// Joda-Time
-import org.joda.time.DateTime
-
-// Thrift.
-import org.apache.thrift.TDeserializer
-
-// Specs2.
-import org.specs2.mutable._
-
 class ThriftLoaderSpec extends Specification {
   private val thriftDeserializer = new TDeserializer
   private val snowplowRawEventBytes = Base64.decodeBase64("CgABAAABQ3KVZkgMAAoIAAEAAAABCAACAAAAAQsAAwAAABh0ZXN0UGFyYW09MyZ0ZXN0UGFyYW0yPTQACwAUAAAAEHNzYy0wLjAuMS1zdGRvdXQLAB4AAAAFVVRGLTgLACgAAAAJMTI3LjAuMC4xCwApAAAACTEyNy4wLjAuMQsAMgAAAGhNb3ppbGxhLzUuMCAoWDExOyBMaW51eCB4ODZfNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8zMS4wLjE2NTAuNjMgU2FmYXJpLzUzNy4zNg8ARgsAAAAHAAAAL0Nvb2tpZTogc3A9YzVmM2EwOWYtNzVmOC00MzA5LWJlYzUtZmVhNTYwZjc4NDU1AAAAHkFjY2VwdC1MYW5ndWFnZTogZW4tVVMsIGVuLCBldAAAACRBY2NlcHQtRW5jb2Rpbmc6IGd6aXAsIGRlZmxhdGUsIHNkY2gAAAB0VXNlci1BZ2VudDogTW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMzEuMC4xNjUwLjYzIFNhZmFyaS81MzcuMzYAAABWQWNjZXB0OiB0ZXh0L2h0bWwsIGFwcGxpY2F0aW9uL3hodG1sK3htbCwgYXBwbGljYXRpb24veG1sO3E9MC45LCBpbWFnZS93ZWJwLCAqLyo7cT0wLjgAAAAWQ29ubmVjdGlvbjoga2VlcC1hbGl2ZQAAABRIb3N0OiAxMjcuMC4wLjE6ODA4MAsAUAAAACRjNWYzYTA5Zi03NWY4LTQzMDktYmVjNS1mZWE1NjBmNzg0NTUA")
   private var snowplowRawEvent = new SnowplowRawEvent
   thriftDeserializer.deserialize(snowplowRawEvent, snowplowRawEventBytes)
 
-  private val thriftLoader = new ThriftLoader
-
   "Thrift SnowplowRawEvent canonical objects" should {
-    val canonicalEvent = thriftLoader.toCanonicalInput(
+    val canonicalEvent = ThriftLoader.toCanonicalInput(
       new String(snowplowRawEventBytes.map(_.toChar))
     ).toOption.get.get
     "correctly load original parameters" in {
