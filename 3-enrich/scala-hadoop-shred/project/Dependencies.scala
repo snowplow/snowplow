fb4183a8ab8cf70d5858b8e29e89960e13033619
Alex Dean <alex@snowplowanalytics.com>
2014-04-27 01:20:36 +0100

    Stubbed code for Hadoop shred

---
+++ 3-enrich/scala-hadoop-shred/project/Dependencies.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2012 SnowPlow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+import sbt._
+
+object Dependencies {
+  val resolutionRepos = Seq(
+    "ScalaTools snapshots at Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/",
+    "Concurrent Maven Repo" at "http://conjars.org/repo", // For Scalding, Cascading etc
+    // For Snowplow libs
+    "Snowplow Analytics Maven repo" at "http://maven.snplow.com/releases/",
+    "Snowplow Analytics Maven snapshot repo" at "http://maven.snplow.com/snapshots/"
+  )
+
+  object V {
+    // Java
+    val hadoop       = "1.1.2"
+    // Scala
+    val commonEnrich = "0.3.0"
+    val scalding     = "0.9.1"
+    val scalaz7      = "7.0.0"
+    val argonaut     = "6.0.4"
+    // Scala (test only)
+    val specs2       = "2.3.11"
+  }
+
+  object Libraries {
+    // Java
+    val hadoopCore       = "org.apache.hadoop"          %  "hadoop-core"             % V.hadoop        % "provided"
+    // Scala
+    val commonEnrich     = "com.snowplowanalytics"      %  "snowplow-common-enrich"  % V.commonEnrich
+    val scaldingCore     = "com.twitter"                %% "scalding-core"           % V.scalding
+    val scaldingArgs     = "com.twitter"                %% "scalding-args"           % V.scalding
+    val scalaz7          = "org.scalaz"                 %% "scalaz-core"             % V.scalaz7
+    val argonaut         = "io.argonaut"                %% "argonaut"                % V.argonaut
+    // Scala (test only)
+    val specs2           = "org.specs2"                 %% "specs2"                  % V.specs2        % "test"
+  }
+}
---
+++ 3-enrich/scala-hadoop-shred/src/test/scala/com/snowplowanalytics/hadoop/scalding/WordCountTest.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 Twitter, Inc.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.hadoop.scalding
+
+// Specs2
+import org.specs2.mutable.Specification
+
+// Scalding
+import com.twitter.scalding._
+
+class WordCountTest extends Specification {
+  "A WordCount job" should {
+    JobTest("com.snowplowanalytics.hadoop.scalding.WordCountJob").
+      arg("input", "inputFile").
+      arg("output", "outputFile").
+      source(TextLine("inputFile"), List("0" -> "hack hack hack and hack")).
+      sink[(String,Int)](Tsv("outputFile")){ outputBuffer =>
+        val outMap = outputBuffer.toMap
+        "count words correctly" in {
+          outMap("hack") must be_==(4)
+          outMap("and") must be_==(1)
+        }
+      }.
+      run.
+      finish
+  }
+}
