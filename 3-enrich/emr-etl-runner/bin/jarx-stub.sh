09d2023cb6ad6f73ae4f8ee5d1839ea4c56d8604
Alex Dean <alex@snowplowanalytics.com>
2014-05-18 14:41:59 +0100

    Additional helpers for making executable jars

---
+++ 3-enrich/emr-etl-runner/bin/jarx-stub.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+MYSELF=`which "$0" 2>/dev/null`
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+exec "$java" $java_args -jar $MYSELF "$@"
+exit 1
