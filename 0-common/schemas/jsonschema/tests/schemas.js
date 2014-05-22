65b06cbc9c036f856d17f7e341574caa1a3c4ace
Fred Blundun <fred@snowplowanalytics.com>
2014-05-22 09:30:53 +0100

    Updated file paths and removed schema ambiguity

--- 0-common/schemas/jsonschema/tests/schemas.js
+++ 0-common/schemas/jsonschema/tests/schemas.js
@@ -3,16 +3,16 @@ define([
 	'intern!object',
 	'intern/chai!expect',
 	'intern/dojo/node!z-schema',
-	'intern/dojo/node!../com.snowplowanalytics/self-desc/schema',
-	'intern/dojo/node!../com.snowplowanalytics/self-desc/instance',	
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/payload_data',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/contexts',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/unstruct_event',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/ad_impression',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/ad_click',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/ad_conversion',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/link_click',
-	'intern/dojo/node!../com.snowplowanalytics/snowplow/screen_view'
+	'intern/dojo/node!../com.snowplowanalytics.self-desc/schema',
+	'intern/dojo/node!../com.snowplowanalytics.self-desc/instance',	
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/payload_data',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/contexts',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/unstruct_event',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/ad_impression',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/ad_click',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/ad_conversion',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/link_click',
+	'intern/dojo/node!../com.snowplowanalytics.snowplow/screen_view'
 ], function(registerSuite, expect, ZSchema, schema, instance, trackerProtocol, customContexts, unstructEvent, adImpression, adClick, adConversion, linkClick, screenView) {
 
 	var schemas = [trackerProtocol, customContexts, unstructEvent, schema, instance, trackerProtocol, customContexts, unstructEvent, adImpression, adClick, adConversion, linkClick, screenView],
