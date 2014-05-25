f2344f18b78eb51190badd318e659419f09f90fb
Alex Dean <alex@snowplowanalytics.com>
2014-05-25 16:21:27 +0100

    Added JsonUtils to work with Jackson

---
+++ 3-enrich/scala-hadoop-shred/src/main/scala/com.snowplowanalytics.snowplow.enrich/hadoop/utils/JsonUtils.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.enrich
+package hadoop
+package utils
+
+// Jackson
+import com.fasterxml.jackson.databind.{
+  ObjectMapper,
+  JsonNode
+}
+
+// Scalaz
+import scalaz._
+import Scalaz._
+
+/**
+ * Contains general purpose extractors and other
+ * utilities for JSONs. Jackson-based.
+ */
+object JsonUtils {
+
+  private lazy val Mapper = new ObjectMapper
+
+  /**
+   * Converts a JSON string into a Validation[String, Json]
+   *
+   * @param json The JSON string to parse
+   * @return a Scalaz Validation, wrapping either an error
+   *         String or the extracted JsonNode
+   */
+  def extractJson(field: String, str: String): Validation[String, JsonNode] =
+    try {
+      Mapper.readTree(str).success
+    } catch {
+      case e: Throwable => "Field [$field]: invalid JSON [$str] with parsing error: $e.getMessage".fail
+    }
+
+}
