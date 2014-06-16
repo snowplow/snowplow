9f72a06bb605416aacdbdf068a86fe31b11a062f
Fred Blundun <fred@snowplowanalytics.com>
2014-06-16 12:21:40 +0100

    Postgres: bumped table-def to 0.3.0

--- 4-storage/postgres-storage/sql/atomic-def.sql
+++ 4-storage/postgres-storage/sql/atomic-def.sql
@@ -9,7 +9,7 @@
 -- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 --
--- Version:     0.2.0
+-- Version:     0.3.0
 -- URL:         -
 --
 -- Authors:     Yali Sassoon, Alex Dean
