cb6722f91f4d357ef2a6904c62563db3216ec5e3
Fred Blundun <fred@snowplowanalytics.com>
2014-06-18 13:03:43 +0100

    Moved ScalazJson4sUtils and enrichmentConfigs into Scala Common Enrich

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/enrichmentConfigs.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/enrichmentConfigs.scala
@@ -10,8 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package com.snowplowanalytics.snowplow.enrich
-package hadoop
+package com.snowplowanalytics.snowplow.enrich.common
 package utils
 
 // Scalaz
