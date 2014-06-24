98499807f8674793fb69ee905c765500ab2b51ec
Fred Blundun <fred@snowplowanalytics.com>
2014-06-24 15:57:55 +0100

    Got ScalazArgsSpec working again

--- 3-enrich/scala-hadoop-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/utils/ScalazArgsSpec.scala
+++ 3-enrich/scala-hadoop-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/utils/ScalazArgsSpec.scala
@@ -10,7 +10,9 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package com.snowplowanalytics.snowplow.enrich
+package com.snowplowanalytics
+package snowplow
+package enrich
 package hadoop
 package utils
 
@@ -20,6 +22,9 @@ import com.twitter.scalding.Args
 // Scalaz
 import scalaz._
 import Scalaz._
+
+// Iglu Scala Client
+import iglu.client.validation.ProcessingMessageMethods._
 
 // Specs2
 import org.specs2.Specification
@@ -43,7 +48,7 @@ class ScalazArgsSpec extends Specification with DataTables with ValidationMatche
   val scaldingArgs = Args(Array("--alpha", "123", "--beta", "456", "--delta", "789", "abc", "--hive", "run%3D2013-07-07"))
 
   import ScalazArgs._
-  def e1 = scaldingArgs.requiredz("alpha") must beSuccessful("123")
+  def e1 = scaldingArgs.requiredz("alpha").leftMap(_.toString).map(_.toString) must beSuccessful("123")
 
   val scalazArgs = new ScalazArgs(scaldingArgs)
   def err: (String) => String = key => "".format(key)
@@ -51,22 +56,22 @@ class ScalazArgsSpec extends Specification with DataTables with ValidationMatche
   def e2 =
     "SPEC NAME"                       || "KEY"       | "EXPECTED"                                                      |
     "Required and is present"         !! "alpha"     ! "123".success                                                   |
-    "Error, required but not present" !! "epsilon"   ! "Required argument [epsilon] not found".fail                    |
-    "Error, values are a list"        !! "delta"     ! "List of values found for argument [delta], should be one".fail |> {
+    "Error, required but not present" !! "epsilon"   ! "Required argument [epsilon] not found".toProcessingMessage.toString.fail                    |
+    "Error, values are a list"        !! "delta"     ! "List of values found for argument [delta], should be one".toProcessingMessage.toString.fail |> {
       (_, key, expected) =>
-        scalazArgs.requiredz(key) must_== expected
+        scalazArgs.requiredz(key).leftMap(_.toString) must_== expected
     }
 
   def e3 =
     "SPEC NAME"                       || "KEY"       | "EXPECTED"                                                              |
     "Optional and is present"         !! "beta"      ! Some("456").success                                                     |
     "Optional and is not present"     !! "gamma"     ! None.success                                                            |
-    "Error, values are a list"        !! "delta"     ! "List of values found for argument [delta], should be at most one".fail |> {
+    "Error, values are a list"        !! "delta"     ! "List of values found for argument [delta], should be at most one".toProcessingMessage.toString.fail |> {
       (_, key, expected) =>
-        scalazArgs.optionalz(key) must_== expected
+        scalazArgs.optionalz(key).leftMap(_.toString) must_== expected
     }
 
   // TODO: can remove these when future Scalding Args drops support for name=value arguments
-  def e4 = scalazArgs.requiredz("hive") must_== "run=2013-07-07".success
-  def e5 = scalazArgs.optionalz("hive") must_== Some("run=2013-07-07").success
+  def e4 = scalazArgs.requiredz("hive").leftMap(_.toString) must_== "run=2013-07-07".success
+  def e5 = scalazArgs.optionalz("hive").leftMap(_.toString) must_== Some("run=2013-07-07").success
 }
