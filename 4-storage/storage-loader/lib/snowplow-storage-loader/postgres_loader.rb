3bd167416fcb9b2eac8e9048cba291bc855304b1
Alex Dean <alex@snowplowanalytics.com>
2014-07-07 23:47:45 +0100

    Removed debug message

--- 4-storage/storage-loader/lib/snowplow-storage-loader/postgres_loader.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/postgres_loader.rb
@@ -84,8 +84,6 @@ module Snowplow
           [ "COMMIT;" ]
         ).join("\n")
 
-        puts transaction
-
         execute_queries(target, [ transaction ])
       end
       module_function :execute_transaction
