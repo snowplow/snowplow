43c50b9e5903737f7a13ad619f8d6967aa27bc6c
Fred Blundun <fred@snowplowanalytics.com>
2014-07-08 11:52:27 +0100

    Redshift: added new MaxMind fields (#871)

--- 4-storage/redshift-storage/sql/atomic-def.sql
+++ 4-storage/redshift-storage/sql/atomic-def.sql
@@ -52,6 +52,11 @@ CREATE TABLE atomic.events (
 	geo_zipcode varchar(15),
 	geo_latitude double precision,
 	geo_longitude double precision,
+	geo_region_name varchar(100),                      -- Added in 0.4.0
+	-- IP lookups
+	ip_isp varchar(100),                               -- Added in 0.4.0
+	ip_org varchar(100),                               -- Added in 0.4.0
+	ip_domain varchar(100),                            -- Added in 0.4.0
 	-- Page
 	page_url varchar(4096),                            -- Added in 0.3.0
 	page_title varchar(2000),
--- 4-storage/redshift-storage/sql/migrate_0.3.0_to_0.4.0.sql
+++ 4-storage/redshift-storage/sql/migrate_0.3.0_to_0.4.0.sql
@@ -52,6 +52,11 @@ CREATE TABLE atomic.events (
 	geo_zipcode varchar(15),
 	geo_latitude double precision,
 	geo_longitude double precision,
+	geo_region_name varchar(100),                      -- Added in 0.3.0
+	-- IP lookups
+	ip_isp varchar(100)                                -- Added in 0.3.0
+	ip_org varchar(100)                                -- Added in 0.3.0
+	ip_domain varchar(100)                             -- Added in 0.3.0	
 	-- Page
 	page_url varchar(4096),
 	page_title varchar(2000),
@@ -181,6 +186,10 @@ INSERT INTO atomic.events
 	geo_zipcode,
 	geo_latitude,
 	geo_longitude,
+	NULL AS geo_region_name,                           -- Added in 0.4.0
+	NULL AS ip_isp,                                    -- Added in 0.4.0
+	NULL AS ip_org,                                    -- Added in 0.4.0
+	NULL AS ip_domain,                                 -- Added in 0.4.0
 	page_url,
 	page_title,
 	page_referrer,
