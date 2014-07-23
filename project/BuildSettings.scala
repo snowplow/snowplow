c125e9d1e91d1664a41591ed48791dca76fc79b1
Alex Dean <alex@snowplowanalytics.com>
2014-07-23 18:50:11 +0100

    Bumped version to 0.2.2

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -22,7 +22,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "com.snowplowanalytics",
-    version       := "0.2.1",
+    version       := "0.2.2",
     description   := "Library for extracting marketing attribution data from referer URLs",
     scalaVersion  := "2.9.1",
     crossScalaVersions := Seq("2.9.1", "2.10.4", "2.11.1"),
