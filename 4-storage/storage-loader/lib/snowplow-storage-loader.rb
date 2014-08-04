961c629cfea6d61550806703b813c5e7bcf606bd
Alex Dean <alex@snowplowanalytics.com>
2014-08-04 14:45:52 +0100

    StorageLoader: bumped to 0.3.2

--- 4-storage/storage-loader/lib/snowplow-storage-loader.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader.rb
@@ -26,6 +26,6 @@ require_relative 'snowplow-storage-loader/redshift_loader'
 module Snowplow
   module StorageLoader
   	NAME          = "snowplow-storage-loader"
-    VERSION       = "0.3.1"
+    VERSION       = "0.3.2"
   end
 end
