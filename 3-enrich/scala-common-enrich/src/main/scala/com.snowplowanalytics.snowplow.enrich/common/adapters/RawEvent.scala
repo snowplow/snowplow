7830e5a367e2ee09174ac4f343c8961407f9ee0c
Alex Dean <alex@snowplowanalytics.com>
2014-08-05 12:46:24 +0100

    Interleaved the RawEvent into the EnrichmentManager

---
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/RawEvent.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.enrich.common
+package adapters
+
+// Joda-Time
+import org.joda.time.DateTime
+
+// This project
+import loaders.InputSource
+
+/**
+ * The canonical input format for the ETL
+ * process: it should be possible to
+ * convert any collector payload to this
+ * raw event format via an _adapter_,
+ * ready for the main, collector-agnostic
+ * stage of the Enrichment.
+ */
+final case class RawEvent(
+    timestamp:   DateTime, // Collector timestamp
+    vendor:      String,
+    version:     String,
+    parameters:  Map[String, String],
+    // body:     String,
+    source:      InputSource,    // See below for defn.
+    encoding:    String, 
+    ipAddress:   Option[String],
+    userAgent:   Option[String],
+    refererUri:  Option[String],
+    headers:     List[String],   // May be Nil so not a Nel
+    userId:      Option[String])
