4aa7d4ccfb5a9c309f3004b6dc29fab2abeb3bc5
Alex Dean <alex@snowplowanalytics.com>
2014-08-21 22:23:28 +0300

    Renamed deviceVendor -> deviceManufacturer and added appleIdfv /cc @jonalmeida

--- 4-storage/redshift-storage/sql/com.snowplowanalytics.snowplow/mobile_context_1.sql
+++ 4-storage/redshift-storage/sql/com.snowplowanalytics.snowplow/mobile_context_1.sql
@@ -17,25 +17,26 @@
 
 CREATE TABLE atomic.com_snowplowanalytics_snowplow_mobile_context_1 (
     -- Schema of this type
-    schema_vendor  varchar(128)  encode runlength not null,
-    schema_name    varchar(128)  encode runlength not null,
-    schema_format  varchar(128)  encode runlength not null,
-    schema_version varchar(128)  encode runlength not null,
+    schema_vendor       varchar(128)  encode runlength not null,
+    schema_name         varchar(128)  encode runlength not null,
+    schema_format       varchar(128)  encode runlength not null,
+    schema_version      varchar(128)  encode runlength not null,
     -- Parentage of this type
-    root_id        char(36)      encode raw not null,
-    root_tstamp    timestamp     encode raw not null,
-    ref_root       varchar(255)  encode runlength not null,
-    ref_tree       varchar(1500) encode runlength not null,
-    ref_parent     varchar(255)  encode runlength not null,
+    root_id             char(36)      encode raw not null,
+    root_tstamp         timestamp     encode raw not null,
+    ref_root            varchar(255)  encode runlength not null,
+    ref_tree            varchar(1500) encode runlength not null,
+    ref_parent          varchar(255)  encode runlength not null,
     -- Properties of this type
-    os_type        varchar(255)  encode text255 not null,
-    os_version     varchar(255)  encode text32k not null,
-    device_vendor  varchar(255)  encode text255 not null,
-    device_model   varchar(255)  encode text32k not null,
-    carrier        varchar(255)  encode text32k,
-    open_idfa      varchar(128)  encode runlength,
-    apple_idfa     varchar(128)  encode runlength,
-    android_idfa   varchar(128)  encode runlength
+    os_type             varchar(255)  encode text255 not null,
+    os_version          varchar(255)  encode text32k not null,
+    device_manufacturer varchar(255)  encode text255 not null,
+    device_model        varchar(255)  encode text32k not null,
+    carrier             varchar(255)  encode text32k,
+    open_idfa           varchar(128)  encode runlength,
+    apple_idfa          varchar(128)  encode runlength,
+    apple_idfv          varchar(128)  encode runlength,
+    android_idfa        varchar(128)  encode runlength
 )
 DISTSTYLE KEY
 -- Optimized join to atomic.events
