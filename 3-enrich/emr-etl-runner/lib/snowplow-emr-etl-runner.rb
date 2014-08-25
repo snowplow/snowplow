302e6cea01a68477ba5f06e21de608666131528e
Alex Dean <alex@snowplowanalytics.com>
2014-08-25 19:49:43 +0300

    EmrEtlRunner: bumped to 0.9.1

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -26,6 +26,6 @@ require_relative 'snowplow-emr-etl-runner/runner'
 module Snowplow
   module EmrEtlRunner
     NAME          = "snowplow-emr-etl-runner"
-    VERSION       = "0.9.0"
+    VERSION       = "0.9.1"
   end
 end
