7b55808868892ae8766ad2e916b329c79ebff732
Alex Dean <alex@snowplowanalytics.com>
2014-10-29 00:51:11 +0000

    Bumped version for testing

--- 3-enrich/scala-hadoop-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/JobSpecHelpers.scala
+++ 3-enrich/scala-hadoop-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/JobSpecHelpers.scala
@@ -51,7 +51,7 @@ object JobSpecHelpers {
   /**
    * The current version of our Hadoop ETL
    */
-  val EtlVersion = "hadoop-0.9.0-M2-common-0.8.0-M2"
+  val EtlVersion = "hadoop-0.9.0-M3-common-0.8.0-M3"
 
   val EtlTimestamp = "2001-09-09 01:46:40.000"
 
