9579f294ea293e01ca443c90b0a0fa19904784db
jbeemster <joshua.a.beemster@gmail.com>
2014-10-29 18:17:31 +0100

    Test suite for mailchimp adapter updated

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MailchimpAdapterSpec.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MailchimpAdapterSpec.scala
@@ -21,6 +21,12 @@ import org.joda.time.DateTime
 import scalaz._
 import Scalaz._
 
+// json4s
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.scalaz.JsonScalaz._
+
 // Snowplow
 import loaders.{
   CollectorApi,
@@ -38,11 +44,14 @@ import org.specs2.scalaz.ValidationMatchers
 
 class MailchimpAdapterSpec extends Specification with DataTables with ValidationMatchers with ScalaCheck { def is =
 
-  "This is a specification to test the MailchimpAdapter functionality"                                                     ^
+  "This is a specification to test the MailchimpAdapter functionality"                                                       ^
                                                                                                                             p^
-  "toRawEvents should return a NEL containing one RawEvent if the CloudFront querystring is minimally populated"             ! e1^
+  "getSchema should return the correct schema for the type of event or throw a runtime exception"                            ! e1^
+  "toKeys should return a valid List of Keys from a string containing braces (or not)"                                       ! e2^
+  "recurse should return a valid JObject which contains the toKeys list and value supplied"                                  ! e3^
+  "getJsonObject should return a valid list JObjects which pertain to the map supplied"                                      ! e4^
+  "mergeJObjects should return a correctly merged JSON which matches the expectation"                                        ! e5^
                                                                                                                              end
-
   implicit val resolver = SpecHelpers.IgluResolver
 
   object Shared {
@@ -65,9 +74,38 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
   val ContentType = "application/x-www-form-urlencoded; charset=utf-8"
 
   def e1 = {
-    val body = "type=unsubscribe&fired_at=2014-10-22+13%3A10%3A40&data%5Baction%5D=unsub&data%5Breason%5D=manual&data%5Bid%5D=94826aa750&data%5Bemail%5D=josh%40snowplowanalytics.com&data%5Bemail_type%5D=html&data%5Bip_opt%5D=82.225.169.220&data%5Bweb_id%5D=203740265&data%5Bmerges%5D%5BEMAIL%5D=josh%40snowplowanalytics.com&data%5Bmerges%5D%5BFNAME%5D=Joshua&data%5Bmerges%5D%5BLNAME%5D=Beemster&data%5Blist_id%5D=f1243a3b12"
-    val payload = CollectorPayload(Shared.api, toNameValuePairs("tv" -> "0", "nuid" -> "123"), ContentType.some, body.some, Shared.cfSource, Shared.context)
-    val actual = MailchimpAdapter.toRawEvents(payload)
-    actual must beSuccessful(NonEmptyList(RawEvent(Shared.api, Map("tv" -> "ios-0.1.0", "p" -> "mob", "e" -> "se"), None, Shared.cfSource, Shared.context)))
+    val schemaType = "subscribe"
+    val schemaReturn = MailchimpAdapter.getSchema(Some(schemaType))
+    val expected = "iglu:com.mailchimp/subscribe/jsonschema/1-0-0"
+    schemaReturn mustEqual expected
+  }
+
+  def e2 = {
+    val toKeysTest = MailchimpAdapter.toKeys("data[merges][LNAME]")
+    val expected = List("data","merges","LNAME")
+    toKeysTest mustEqual expected
+  }
+
+  def e3 = {
+    val keysArray = List("data","merges","LNAME")
+    val value = "Beemster"
+    val expected = JObject(List(("data",JObject(List(("merges",JObject(List(("LNAME",JString("Beemster"))))))))))
+    val testRecursive = MailchimpAdapter.recurse(keysArray, value)
+    testRecursive mustEqual expected
+  }
+
+  def e4 = {
+    val m = Map("data[merges][LNAME]" -> "Beemster")
+    val expected = List(JObject(List(("data",JObject(List(("merges",JObject(List(("LNAME",JString("Beemster")))))))))))
+    val testMap = MailchimpAdapter.getJsonObject(m)
+    testMap mustEqual expected
+  }
+
+  def e5 = {
+    val m = Map("data[merges][LNAME]" -> "Beemster", "data[merges][FNAME]" -> "Joshua")
+    val jsonObject = MailchimpAdapter.getJsonObject(m)
+    val mergedJsonString = compact(render(MailchimpAdapter.mergeJObjects(jsonObject)))
+    val expected = "{\"data\":{\"merges\":{\"LNAME\":\"Beemster\",\"FNAME\":\"Joshua\"}}}"
+    mergedJsonString mustEqual expected
   }
 }
