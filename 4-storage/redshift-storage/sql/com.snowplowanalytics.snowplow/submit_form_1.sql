85e0440c68b2ef45e3a6c8109ee0289a2e7ba4b7
Fred Blundun <fred@snowplowanalytics.com>
2014-10-30 09:11:34 +0000

    Fixed duplicate data type declaration

--- 4-storage/redshift-storage/sql/com.snowplowanalytics.snowplow/submit_form_1.sql
+++ 4-storage/redshift-storage/sql/com.snowplowanalytics.snowplow/submit_form_1.sql
@@ -30,7 +30,7 @@ CREATE TABLE atomic.com_snowplowanalytics_snowplow_submit_form_1 (
 	-- Properties of this type
 	form_id         varchar(255)  encode text32k not null,
 	form_classes    varchar(2048) encode raw, -- Holds a JSON array. TODO: will replace with a ref_ following https://github.com/snowplow/snowplow/issues/647
-	elements        varchar(4096) varchar(2048) encode raw -- Holds a nested JSON array.
+	elements        varchar(4096) encode raw -- Holds a nested JSON array.
 )
 DISTSTYLE KEY
 -- Optimized join to atomic.events
