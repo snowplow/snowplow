468a567af25510a42f657b3b3c4b246142a2d847
jbeemster <joshua.a.beemster@gmail.com>
2014-10-31 18:33:18 +0100

    Updated test suite and adapter

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MailchimpAdapterSpec.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MailchimpAdapterSpec.scala
@@ -50,11 +50,12 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
   "recurse should return a valid JObject which contains the toKeys list and value supplied"                           ! e2^
   "getJsonObject should return a valid list JObjects which pertain to the map supplied"                               ! e3^
   "mergeJObjects should return a correctly merged JSON which matches the expectation"                                 ! e4^
-  "getSchema should return the correct schema for a valid event type"                                                 ! e5^
-  "getSchema should return a Nel Failure error for a bad event type"                                                  ! e6^
-  "toRawEvents should return a NEL containing an unsubscribe event and query string parameters"                       ! e7^
-  "toRawEvents must be failing if no type parameter is passed in the body"                                            ! e8^
-  "toRawEvents must be failing if the body content is empty"                                                          ! e9^
+  "mergeJObjects should return a Nel Failure for an empty List of JObjects"                                           ! e5^
+  "getSchema should return the correct schema for a valid event type"                                                 ! e6^
+  "getSchema should return a Nel Failure error for a bad event type"                                                  ! e7^
+  "toRawEvents should return a NEL containing an unsubscribe event and query string parameters"                       ! e8^
+  "toRawEvents must be failing if no type parameter is passed in the body"                                            ! e9^
+  "toRawEvents must be failing if the body content is empty"                                                          ! e10^
                                                                                                                      end
   implicit val resolver = SpecHelpers.IgluResolver
 
@@ -101,12 +102,17 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
   def e4 = {
     val m = Map("data[merges][LNAME]" -> "Beemster", "data[merges][FNAME]" -> "Joshua")
     val jsonObject = MailchimpAdapter.getJsonObject(m)
-    val mergedJsonString = compact(render(MailchimpAdapter.mergeJObjects(jsonObject)))
-    val expected = "{\"data\":{\"merges\":{\"LNAME\":\"Beemster\",\"FNAME\":\"Joshua\"}}}"
-    mergedJsonString mustEqual expected
+    val actual = MailchimpAdapter.mergeJObjects(jsonObject)
+    val expected = JObject(List(("data",JObject(List(("merges",JObject(List(("LNAME",JString("Beemster")), ("FNAME",JString("Joshua"))))))))))
+    actual must beSuccessful(expected)
   }
 
-  def e5 = 
+  def e5 = {
+    val actual = MailchimpAdapter.mergeJObjects(List())
+    actual must beFailing(NonEmptyList("No JObjects to merge"))
+  }
+
+  def e6 = 
     "SPEC NAME"               || "SCHEMA TYPE"  | "EXPECTED OUTPUT"                                               |
     "Valid, type subscribe"   !! "subscribe"    ! "iglu:com.mailchimp/subscribe/jsonschema/1-0-0"                 |
     "Valid, type unsubscribe" !! "unsubscribe"  ! "iglu:com.mailchimp/unsubscribe/jsonschema/1-0-0"               |
@@ -117,11 +123,14 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
       (_, schema, expected) => MailchimpAdapter.getSchema(Some(schema)) must beSuccessful(expected)
   }
 
-  def e6 = {
-    MailchimpAdapter.getSchema(Some("")) must beFailing(NonEmptyList("Invalid event type passed"))
+  def e7 = 
+    "SPEC NAME"               || "SCHEMA TYPE"  | "EXPECTED OUTPUT"                                               |
+    "Invalid, bad type"       !! Some("bad")    ! "Invalid event type passed to getSchema - bad"                  |
+    "Invalid, no type"        !! None           ! "No event type passed to getSchema"                             |> {
+      (_, schema, expected) => MailchimpAdapter.getSchema(schema) must beFailing(NonEmptyList(expected))
   }
 
-  def e7 = {
+  def e8 = {
     val body = "type=unsubscribe&fired_at=2014-10-22+13%3A10%3A40&data%5Baction%5D=unsub&data%5Breason%5D=manual&data%5Bid%5D=94826aa750&data%5Bemail%5D=josh%40snowplowanalytics.com&data%5Bemail_type%5D=html&data%5Bip_opt%5D=82.225.169.220&data%5Bweb_id%5D=203740265&data%5Bmerges%5D%5BEMAIL%5D=josh%40snowplowanalytics.com&data%5Bmerges%5D%5BFNAME%5D=Joshua&data%5Bmerges%5D%5BLNAME%5D=Beemster&data%5Blist_id%5D=f1243a3b12"
     val qs = toNameValuePairs("nuid" -> "123")
     val payload = CollectorPayload(Shared.api, qs, ContentType.some, body.some, Shared.cfSource, Shared.context)
@@ -157,7 +166,7 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
     actual must beSuccessful(NonEmptyList(RawEvent(Shared.api, Map("tv" -> "com.mailchimp-v1", "e" -> "ue", "p" -> "app", "ue_pr" -> expectedJson), ContentType.some, Shared.cfSource, Shared.context)))
   }
 
-  def e8 = {
+  def e9 = {
     val body = "fired_at=2014-10-22+13%3A10%3A40"
     val payload = CollectorPayload(Shared.api, Nil, ContentType.some, body.some, Shared.cfSource, Shared.context)
     val actual = MailchimpAdapter.toRawEvents(payload)
@@ -165,7 +174,7 @@ class MailchimpAdapterSpec extends Specification with DataTables with Validation
     actual must beFailing(NonEmptyList("No event type passed with event body"))
   }
 
-  def e9 = {
+  def e10 = {
     val body = ""
     val payload = CollectorPayload(Shared.api, Nil, ContentType.some, body.some, Shared.cfSource, Shared.context)
     val actual = MailchimpAdapter.toRawEvents(payload)
