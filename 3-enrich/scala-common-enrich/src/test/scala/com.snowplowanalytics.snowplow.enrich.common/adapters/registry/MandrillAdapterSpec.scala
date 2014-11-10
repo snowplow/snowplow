24d9ec5334623256a5bd4384041d848f3740b524
jbeemster <joshua.a.beemster@gmail.com>
2014-11-10 15:47:27 +0100

    Fleshed out MandrillAdapter test suite

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MandrillAdapterSpec.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/MandrillAdapterSpec.scala
@@ -44,13 +44,22 @@ import org.specs2.scalaz.ValidationMatchers
 
 class MandrillAdapterSpec extends Specification with DataTables with ValidationMatchers with ScalaCheck { def is =
 
-  "This is a specification to test the MailchimpAdapter functionality"                                                ^
-                                                                                                                     p^
-  "extractKeyValueFromJson should return an Option[String] or None if it can or cannot find a valid value"          ! e1^
-  "extractKeyValueFromJson should return an Option[String] or None if it can or cannot find a valid value"          ! e2^
-  "extractKeyValueFromJson should return an Option[String] or None if it can or cannot find a valid value"          ! e3^
-  "extractKeyValueFromJson should return an Option[String] or None if it can or cannot find a valid value"          ! e4^
-                                                                                                                     end
+  "This is a specification to test the MandrillAdapter functionality"                                                  ^
+                                                                                                                       p^
+  "extractKeyValueFromJson must return an Option[String] or None if it can or cannot find a valid value"            ! e1^
+  "lookupSchema must return a Success String for a valid event type"                                                ! e2^
+  "lookupSchema must return a Failure String if the event type is not valid"                                        ! e3^
+  "jsonToRawEvent must return a Success RawEvent if the JSON passed is valid"                                       ! e4^
+  "jsonToRawEvent must return a Failure Nel if the JSON does not contain the 'event' key or an invalid event type"  ! e5^
+  "bodyToEventList must return a Success List[JValue] for a valid events string"                                    ! e6^
+  "bodyToEventList must return a Failure String if the mapped events string is not in a valid format"               ! e7^
+  "bodyToEventList must return a Failure String if the event string could not be parsed into JSON"                  ! e8^
+  //"toRawEvents must return a Success Nel for a valid payload.body being passed"                                     ! e9^
+  "toRawEvents must return a Failure Nel if the payload body is empty"                                              ! e10^
+  "toRawEvents must return a Failure Nel if the payload content type is empty"                                      ! e11^
+  "toRawEvents must return a Failure Nel if the payload content type does not match expectation"                    ! e12^
+  //"toRawEvents must return a Failure Nel if any Failure's occured with any other function used within toRawEvents"  ! e15^
+                                                                                                                       end
   implicit val resolver = SpecHelpers.IgluResolver
 
   object Shared {
@@ -59,7 +68,7 @@ class MandrillAdapterSpec extends Specification with DataTables with ValidationM
     val context = CollectorContext(DateTime.parse("2013-08-29T00:18:48.000+00:00"), "37.157.33.123".some, None, None, Nil, None)
   }
 
-  val ContentType = "application/x-www-form-urlencoded; charset=utf-8"
+  val ContentType = "application/x-www-form-urlencoded"
 
   def e1 = 
     "SPEC NAME"               || "KEY"   | "JSON"                             | "EXPECTED OUTPUT"  |
@@ -68,23 +77,86 @@ class MandrillAdapterSpec extends Specification with DataTables with ValidationM
       (_, key, json, expected) => MandrillAdapter.extractKeyValueFromJson(key,json) mustEqual expected
   }
 
-  def e2 = {
+  def e2 = 
+    "SPEC NAME"                         || "SCHEMA TYPE" | "EXPECTED OUTPUT"                                           |
+    "Valid, type MessageBounced"        !! "hard_bounce" ! "iglu:com.mandrill/message_bounced/jsonschema/1-0-0"        |
+    "Valid, type MessageClicked"        !! "click"       ! "iglu:com.mandrill/message_clicked/jsonschema/1-0-0"        |
+    "Valid, type MessageDelayed"        !! "deferral"    ! "iglu:com.mandrill/message_delayed/jsonschema/1-0-0"        |
+    "Valid, type MessageMarkedAsSpam"   !! "spam"        ! "iglu:com.mandrill/message_marked_as_spam/jsonschema/1-0-0" |
+    "Valid, type MessageOpened"         !! "open"        ! "iglu:com.mandrill/message_opened/jsonschema/1-0-0"         |
+    "Valid, type MessageRejected"       !! "reject"      ! "iglu:com.mandrill/message_rejected/jsonschema/1-0-0"       |
+    "Valid, type MessageSent"           !! "send"        ! "iglu:com.mandrill/message_sent/jsonschema/1-0-0"           |
+    "Valid, type MessageSoftBounced"    !! "soft_bounce" ! "iglu:com.mandrill/message_soft_bounced/jsonschema/1-0-0"   |
+    "Valid, type RecipientUnsubscribed" !! "unsub"       ! "iglu:com.mandrill/recipient_unsubscribed/jsonschema/1-0-0" |> {
+      (_, schema, expected) => MandrillAdapter.lookupSchema(schema) must beSuccessful(expected)
+  }
+
+  def e3 = 
+    "SPEC NAME"               || "SCHEMA TYPE"  | "EXPECTED OUTPUT"                                                |
+    "Invalid, bad type"       !! "bad"          ! "Mandrill event parameter [bad] not recognized"                  |
+    "Invalid, no type"        !! ""             ! "Mandrill event parameter is empty: cannot determine event type" |> {
+      (_, schema, expected) => MandrillAdapter.lookupSchema(schema) must beFailing(expected)
+  }
+
+  def e4 = {
+    val json = JObject(List(("event",JString("click"))))  
+    val payload = CollectorPayload(Shared.api, Nil, ContentType.some, None, Shared.cljSource, Shared.context)
+    val expectedMap = Map("tv" -> "com.mandrill-v1", "e" -> "ue", "p" -> "srv", "ue_pr" -> """{"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0","data":{"schema":"iglu:com.mandrill/message_clicked/jsonschema/1-0-0","data":{"event":"click"}}}""")
+    val actual = MandrillAdapter.jsonToRawEvent(payload, json)
+    actual must beSuccessful(RawEvent(Shared.api, expectedMap, ContentType.some, Shared.cljSource, Shared.context))
+  }
+
+  def e5 = 
+    "SPEC NAME"                      || "JSON"                                       | "EXPECTED OUTPUT"                                                    |
+    "Invalid, no event key"          !! JObject(List(("type",JString("subscribe")))) ! "Mandrill event parameter not provided: cannot determine event type" |
+    "Invalid, event value not valid" !! JObject(List(("event",JString("bad"))))      ! "Mandrill event parameter [bad] not recognized"                      |> {
+      (_, json, expected) => {
+        val payload = CollectorPayload(Shared.api, Nil, ContentType.some, None, Shared.cljSource, Shared.context)
+        val actual = MandrillAdapter.jsonToRawEvent(payload, json)
+        actual must beFailing(NonEmptyList(expected))
+      }
+  }
+
+  def e6 = {
+    val bodyStr = "mandrill_events=%5B%7B%22event%22%3A%20%22subscribe%22%7D%5D"
+    val expected = List(JObject(List(("event",JString("subscribe")))))
+    val actual = MandrillAdapter.bodyToEventList(bodyStr)
+    actual must beSuccessful(expected)
+  }
+
+  def e7 = 
+    "SPEC NAME"                         || "STRING TO PROCESS"                        | "EXPECTED OUTPUT"                                                      |
+    "Failure, empty events string"      !! "mandrill_events="                         ! "Mandrill events string is empty: nothing to process"                  |
+    "Failure, too many key-value pairs" !! "mandrill_events=some&mandrill_extra=some" ! "Mapped Mandrill body has invalid count of keys"                       |
+    "Failure, incorrect key"            !! "events_mandrill=something"                ! "Mapped Mandrill body does not have 'mandrill_events' as its only key" |> {
+      (_, str, expected) => MandrillAdapter.bodyToEventList(str) must beFailing(expected)
+  }
+
+  def e8 = {
+    val bodyStr = "mandrill_events=%5B%7B%22event%22%3A%22click%7D%5D"
+    val expected = ""
+    val actual = MandrillAdapter.bodyToEventList(bodyStr)
+    actual must beFailing(expected)
+  }
+
+  def e10 = {
     val payload = CollectorPayload(Shared.api, Nil, ContentType.some, None, Shared.cljSource, Shared.context)
     val actual = MandrillAdapter.toRawEvents(payload)
     actual must beFailing(NonEmptyList("Request body is empty: no Mandrill events to process"))
   }
 
-  def e3 = {
-    val body = "doesnotstartwithmandrill_events="
-    val payload = CollectorPayload(Shared.api, Nil, ContentType.some, body.some, Shared.cljSource, Shared.context)
+  def e11 = {
+    val body = "mandrill_events=%5B%7B%22event%22%3A%20%22subscribe%22%7D%5D"
+    val payload = CollectorPayload(Shared.api, Nil, None, body.some, Shared.cljSource, Shared.context)
     val actual = MandrillAdapter.toRawEvents(payload)
-    actual must beFailing(NonEmptyList("Request body is formatted incorrectly: cannot process"))
+    actual must beFailing(NonEmptyList("Request body provided but content type empty, expected application/x-www-form-urlencoded for Mandrill"))
   }
 
-  def e4 = {
-    val body = "mandrill_events=%5B%7B%22event%22%3A%22send%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%5D%2C%22clicks%22%3A%5B%5D%2C%22state%22%3A%22sent%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22deferral%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%5D%2C%22clicks%22%3A%5B%5D%2C%22state%22%3A%22deferred%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa1%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%2C%22smtp_events%22%3A%5B%7B%22destination_ip%22%3A%22127.0.0.1%22%2C%22diag%22%3A%22451+4.3.5+Temporarily+unavailable%2C+try+again+later.%22%2C%22source_ip%22%3A%22127.0.0.1%22%2C%22ts%22%3A1365111111%2C%22type%22%3A%22deferred%22%2C%22size%22%3A0%7D%5D%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa1%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22hard_bounce%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22state%22%3A%22bounced%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa2%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%2C%22bounce_description%22%3A%22bad_mailbox%22%2C%22bgtools_code%22%3A10%2C%22diag%22%3A%22smtp%3B550+5.1.1+The+email+account+that+you+tried+to+reach+does+not+exist.+Please+try+double-checking+the+recipient%27s+email+address+for+typos+or+unnecessary+spaces.%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa2%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22soft_bounce%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22state%22%3A%22soft-bounced%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa3%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%2C%22bounce_description%22%3A%22mailbox_full%22%2C%22bgtools_code%22%3A22%2C%22diag%22%3A%22smtp%3B552+5.2.2+Over+Quota%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa3%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22open%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%7B%22ts%22%3A1365111111%7D%5D%2C%22clicks%22%3A%5B%7B%22ts%22%3A1365111111%2C%22url%22%3A%22http%3A%5C%2F%5C%2Fmandrill.com%22%7D%5D%2C%22state%22%3A%22sent%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa4%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa4%22%2C%22ip%22%3A%22127.0.0.1%22%2C%22location%22%3A%7B%22country_short%22%3A%22US%22%2C%22country%22%3A%22United+States%22%2C%22region%22%3A%22Oklahoma%22%2C%22city%22%3A%22Oklahoma+City%22%2C%22latitude%22%3A35.4675598145%2C%22longitude%22%3A-97.5164337158%2C%22postal_code%22%3A%2273101%22%2C%22timezone%22%3A%22-05%3A00%22%7D%2C%22user_agent%22%3A%22Mozilla%5C%2F5.0+%28Macintosh%3B+U%3B+Intel+Mac+OS+X+10.6%3B+en-US%3B+rv%3A1.9.1.8%29+Gecko%5C%2F20100317+Postbox%5C%2F1.1.3%22%2C%22user_agent_parsed%22%3A%7B%22type%22%3A%22Email+Client%22%2C%22ua_family%22%3A%22Postbox%22%2C%22ua_name%22%3A%22Postbox+1.1.3%22%2C%22ua_version%22%3A%221.1.3%22%2C%22ua_url%22%3A%22http%3A%5C%2F%5C%2Fwww.postbox-inc.com%5C%2F%22%2C%22ua_company%22%3A%22Postbox%2C+Inc.%22%2C%22ua_company_url%22%3A%22http%3A%5C%2F%5C%2Fwww.postbox-inc.com%5C%2F%22%2C%22ua_icon%22%3A%22http%3A%5C%2F%5C%2Fcdn.mandrill.com%5C%2Fimg%5C%2Femail-client-icons%5C%2Fpostbox.png%22%2C%22os_family%22%3A%22OS+X%22%2C%22os_name%22%3A%22OS+X+10.6+Snow+Leopard%22%2C%22os_url%22%3A%22http%3A%5C%2F%5C%2Fwww.apple.com%5C%2Fosx%5C%2F%22%2C%22os_company%22%3A%22Apple+Computer%2C+Inc.%22%2C%22os_company_url%22%3A%22http%3A%5C%2F%5C%2Fwww.apple.com%5C%2F%22%2C%22os_icon%22%3A%22http%3A%5C%2F%5C%2Fcdn.mandrill.com%5C%2Fimg%5C%2Femail-client-icons%5C%2Fmacosx.png%22%2C%22mobile%22%3Afalse%7D%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22click%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%7B%22ts%22%3A1365111111%7D%5D%2C%22clicks%22%3A%5B%7B%22ts%22%3A1365111111%2C%22url%22%3A%22http%3A%5C%2F%5C%2Fmandrill.com%22%7D%5D%2C%22state%22%3A%22sent%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa5%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa5%22%2C%22ip%22%3A%22127.0.0.1%22%2C%22location%22%3A%7B%22country_short%22%3A%22US%22%2C%22country%22%3A%22United+States%22%2C%22region%22%3A%22Oklahoma%22%2C%22city%22%3A%22Oklahoma+City%22%2C%22latitude%22%3A35.4675598145%2C%22longitude%22%3A-97.5164337158%2C%22postal_code%22%3A%2273101%22%2C%22timezone%22%3A%22-05%3A00%22%7D%2C%22user_agent%22%3A%22Mozilla%5C%2F5.0+%28Macintosh%3B+U%3B+Intel+Mac+OS+X+10.6%3B+en-US%3B+rv%3A1.9.1.8%29+Gecko%5C%2F20100317+Postbox%5C%2F1.1.3%22%2C%22user_agent_parsed%22%3A%7B%22type%22%3A%22Email+Client%22%2C%22ua_family%22%3A%22Postbox%22%2C%22ua_name%22%3A%22Postbox+1.1.3%22%2C%22ua_version%22%3A%221.1.3%22%2C%22ua_url%22%3A%22http%3A%5C%2F%5C%2Fwww.postbox-inc.com%5C%2F%22%2C%22ua_company%22%3A%22Postbox%2C+Inc.%22%2C%22ua_company_url%22%3A%22http%3A%5C%2F%5C%2Fwww.postbox-inc.com%5C%2F%22%2C%22ua_icon%22%3A%22http%3A%5C%2F%5C%2Fcdn.mandrill.com%5C%2Fimg%5C%2Femail-client-icons%5C%2Fpostbox.png%22%2C%22os_family%22%3A%22OS+X%22%2C%22os_name%22%3A%22OS+X+10.6+Snow+Leopard%22%2C%22os_url%22%3A%22http%3A%5C%2F%5C%2Fwww.apple.com%5C%2Fosx%5C%2F%22%2C%22os_company%22%3A%22Apple+Computer%2C+Inc.%22%2C%22os_company_url%22%3A%22http%3A%5C%2F%5C%2Fwww.apple.com%5C%2F%22%2C%22os_icon%22%3A%22http%3A%5C%2F%5C%2Fcdn.mandrill.com%5C%2Fimg%5C%2Femail-client-icons%5C%2Fmacosx.png%22%2C%22mobile%22%3Afalse%7D%2C%22url%22%3A%22http%3A%5C%2F%5C%2Fmandrill.com%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22spam%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%7B%22ts%22%3A1365111111%7D%5D%2C%22clicks%22%3A%5B%7B%22ts%22%3A1365111111%2C%22url%22%3A%22http%3A%5C%2F%5C%2Fmandrill.com%22%7D%5D%2C%22state%22%3A%22sent%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa6%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa6%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22unsub%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%7B%22ts%22%3A1365111111%7D%5D%2C%22clicks%22%3A%5B%7B%22ts%22%3A1365111111%2C%22url%22%3A%22http%3A%5C%2F%5C%2Fmandrill.com%22%7D%5D%2C%22state%22%3A%22sent%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa7%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa7%22%2C%22ts%22%3A1415267366%7D%2C%7B%22event%22%3A%22reject%22%2C%22msg%22%3A%7B%22ts%22%3A1365109999%2C%22subject%22%3A%22This+an+example+webhook+message%22%2C%22email%22%3A%22example.webhook%40mandrillapp.com%22%2C%22sender%22%3A%22example.sender%40mandrillapp.com%22%2C%22tags%22%3A%5B%22webhook-example%22%5D%2C%22opens%22%3A%5B%5D%2C%22clicks%22%3A%5B%5D%2C%22state%22%3A%22rejected%22%2C%22metadata%22%3A%7B%22user_id%22%3A111%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa8%22%2C%22_version%22%3A%22exampleaaaaaaaaaaaaaaa%22%7D%2C%22_id%22%3A%22exampleaaaaaaaaaaaaaaaaaaaaaaaaa8%22%2C%22ts%22%3A1415267366%7D%5D"
-    val payload = CollectorPayload(Shared.api, Nil, ContentType.some, body.some, Shared.cljSource, Shared.context)
+  def e12 = {
+    val body = "mandrill_events=%5B%7B%22event%22%3A%20%22subscribe%22%7D%5D"
+    val ct = "application/x-www-form-urlencoded; charset=utf-8"
+    val payload = CollectorPayload(Shared.api, Nil, ct.some, body.some, Shared.cljSource, Shared.context)
     val actual = MandrillAdapter.toRawEvents(payload)
-    actual must beFailing(NonEmptyList("Request body is formatted incorrectly: cannot process"))
+    actual must beFailing(NonEmptyList("Content type of application/x-www-form-urlencoded; charset=utf-8 provided, expected application/x-www-form-urlencoded for Mandrill"))
   }
 }
