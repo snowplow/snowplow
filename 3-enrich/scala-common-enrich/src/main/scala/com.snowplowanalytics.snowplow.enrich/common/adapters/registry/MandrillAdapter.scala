3392f094f2d76c82563678959a8c64dff2e2a52e
jbeemster <joshua.a.beemster@gmail.com>
2014-11-10 15:46:53 +0100

    MandrillAdapter Updates: Added content type checking, restructured bodyToEventList method

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/registry/MandrillAdapter.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/registry/MandrillAdapter.scala
@@ -26,8 +26,6 @@ import iglu.client.validation.ValidatableJsonMethods._
 
 // Java
 import java.net.URI
-import java.net.URLDecoder
-import org.apache.commons.lang3.StringUtils
 import org.apache.http.client.utils.URLEncodedUtils
 
 // Jackson
@@ -212,6 +210,7 @@ object MandrillAdapter extends Adapter {
    */
   private[registry] def toUnstructEventParamsMandrill(tracker: String, params: RawEventParameters, eventJson: JValue, 
     schema: String, platform: String = "app"): RawEventParameters = {
+
     val json = compact {
       ("schema" -> SchemaUris.UnstructEvent) ~
       ("data"   -> (
