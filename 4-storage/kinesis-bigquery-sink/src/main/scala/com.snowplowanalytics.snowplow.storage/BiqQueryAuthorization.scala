cf36ae0843e80e9eca321622f76de2c8560c648d
Andrew <amc@maths.qmul.ac.uk>
2015-01-08 13:57:46 +0000

    added docs

--- 4-storage/kinesis-bigquery-sink/src/main/scala/com.snowplowanalytics.snowplow.storage/BiqQueryAuthorization.scala
+++ 4-storage/kinesis-bigquery-sink/src/main/scala/com.snowplowanalytics.snowplow.storage/BiqQueryAuthorization.scala
@@ -1,28 +1,23 @@
-package com.google.cloud.helix.samples // TODO: change package and where this file lives on disk. see Fred's ES Sink for package naming conventions and folder layout
+ /*
+ * Copyright (c) 2014 Snowplow Analytics Ltd.
+ * All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache
+ * License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the Apache License Version 2.0 for the specific language
+ * governing permissions and limitations there under.
+ */
 
-// TODO: remove all ;s
-// TODO: group all imports, like java.io below. note that you can only group things at the same level
-// TODO: move these imports below the Scala group. We import in this order: Java, Scala, then Java libraries, then Scala libraries
-//
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.Data;
-import com.google.api.services.bigquery.Bigquery;
-import com.google.api.services.bigquery.BigqueryScopes;
-import com.google.api.services.bigquery.model.GetQueryResultsResponse;
-import com.google.api.services.bigquery.model.QueryRequest;
-import com.google.api.services.bigquery.model.QueryResponse;
-import com.google.api.services.bigquery.model.TableCell;
-import com.google.api.services.bigquery.model.TableRow;
+package com.snowplowanalytics.snowplow.storage.kinesis.bigquery
 
 // Java
 import java.io.{
@@ -31,22 +26,64 @@ import java.io.{
   InputStreamReader,
   PrintStream
 }
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.{
+  Arrays,
+  Collections,
+  List,
+  Scanner
+}
 
 // Scala
 import collection.JavaConversions._
 
-// TODO: add a description. see other snowplow projects for examples
+//Java Libraries
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.googleapis.auth.oauth2.{
+  GoogleAuthorizationCodeFlow,
+  GoogleAuthorizationCodeRequestUrl,
+  GoogleClientSecrets,
+  GoogleTokenResponse
+}
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.util.Data
+import com.google.api.services.bigquery.{
+  Bigquery,
+  BigqueryScopes
+}
+import com.google.api.services.bigquery.model.{
+  GetQueryResultsResponse,
+  QueryRequest,
+  QueryResponse,
+  TableCell,
+  TableRow
+}
+
+/**
+ * Authenticates the user and makes a query to the wikipedia database
+ */
 object bigQueryAuth {
 
+  /**
+   * Obtained from the Google developers console.
+   */
   val ProjectId = "742196692985"
+  /**
+   * Location of the client secrets. This file is obtained from
+   * the Google developers console.
+   */
   val ClientSecretsLocation = "client_secrets.json"
   val HttpTransport = new NetHttpTransport
   val JsonFactory = new JacksonFactory
 
+  /**
+   * Handles OAuth authorization and makes a single query to the public 
+   * wikipedia sample database
+   */
   def main(args: Array[String]) {
    
     val credentials = {
@@ -63,7 +100,16 @@ object bigQueryAuth {
     runQueryRpcAndPrint(bigquery, ProjectId, query, System.out);
   }
 
-  // TODO: add description
+  /**
+   * Prompts the user to visit the google API authorization page. The user 
+   * can then grant access to the API, and if so is given an access code. The 
+   * user is prompted to paste this code in to the command line. The code grants
+   * the applicatin access to the database.
+   *
+   * @param clientSecrets
+   *
+   * @return valid credentials
+   */
   def getCredentials(clientSecrets: GoogleClientSecrets): Credential = {
 
     val scopes = Collections.singleton(BigqueryScopes.BIGQUERY)
@@ -78,7 +124,16 @@ object bigQueryAuth {
     flow.createAndStoreCredential(response,null)
   }
 
-  // TODO: add description
+  /**
+   * Makes a request to the database and displays the response.
+   *
+   * @param bigquery
+   * @param projectId
+   * @param query [[https://cloud.google.com/bigquery/query-reference bigquery database query]]
+   * @param out
+   *
+   * @return Unit
+   */
   def runQueryRpcAndPrint(bigquery: Bigquery, projectId: String, query: String, out: PrintStream) {
     val queryRequest = new QueryRequest().setQuery(query)
     val queryResponse = bigquery.jobs.query(projectId, queryRequest).execute()
@@ -90,14 +145,14 @@ object bigQueryAuth {
       }
     }
 
-    while(true){
+    while(true) {
       var pageToken: String = null
 
       val queryResults = bigquery.jobs
         .getQueryResults(projectId, queryResponse.getJobReference.getJobId)
         .setPageToken(pageToken).execute()
 
-        if (queryResults.getJobComplete) {
+      if (queryResults.getJobComplete) {
         printRows(queryResponse.getRows, out)
         pageToken = queryResults.getPageToken
 
@@ -108,9 +163,12 @@ object bigQueryAuth {
     }
   }
   
+  /**
+   * Prints the query response to standars output.
+   */
   def printRows(rows: List[TableRow], out: PrintStream){
 
-    def outputCell(cell: TableCell){
+    def outputCell(cell: TableCell) {
       val cellData = if (Data.isNull(cell.getV)) {
         "Null"
       } else {
