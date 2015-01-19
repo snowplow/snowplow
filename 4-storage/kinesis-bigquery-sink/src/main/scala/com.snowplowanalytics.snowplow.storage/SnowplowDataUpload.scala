a68a94243012c2c1c3814c7990219e2d4c64680f
Andrew <amc@maths.qmul.ac.uk>
2015-01-19 18:13:42 +0000

    now uses Argot for parsing command line arguments

--- 4-storage/kinesis-bigquery-sink/src/main/scala/com.snowplowanalytics.snowplow.storage/SnowplowDataUpload.scala
+++ 4-storage/kinesis-bigquery-sink/src/main/scala/com.snowplowanalytics.snowplow.storage/SnowplowDataUpload.scala
@@ -19,79 +19,114 @@
 
 package com.snowplowanalytics.snowplow.storage.kinesis.bigquery
 
+// Java
 import java.io.File
 
-object SnowplowDataUploadSetup{
+// Argot
+import org.clapper.argot.{
+  ArgotParser,
+  ArgotUsageException
+}
+import org.clapper.argot.ArgotConverters._
+
+object SnowplowDataUpload{
+
+  val parser = new ArgotParser("bigquery-loader-cli")
+  val createTable = parser.flag[Boolean](
+    List("c", "create-table"), 
+    "If needed create dataset and table." 
+  )
+  val projectId = parser.parameter[String](
+    "projectId",
+    "The project id as obtained from the google developers console.",
+    false
+  )
+  val datasetId = parser.parameter[String](
+    "datasetId",
+    "Name for the dataset. If create-table flag is not included then this" + 
+      " dataset must already exist",
+    false
+  )
+  val tableId = parser.parameter[String](
+    "tableId",
+    "Name for the table. If create-table flag is not included then this" + 
+      " table must already exist",
+    false
+  )
+  val tsvFileLocation = parser.parameter[String](
+    "tsvFileLocation",
+    "Either the filename of a TSV file, or the name of a directory" +
+      " containing TSV files (and only TSV files)",
+    false
+  )
+
   
+  def main (args: Array[String]) {
+    
+    try{ 
+
+      parser.parse(args)
+
+      //initializes, checks for authorization and if not authorized 
+      //starts authorization process
+      val bigqueryInterface = new BigqueryInterface(projectId.value.get)
+
+      if (createTable.value.get) createDatasetAndTable(projectId.value.get, datasetId.value.get, tableId.value.get, bigqueryInterface)
+
+      uploadData(projectId.value.get, datasetId.value.get, tableId.value.get, tsvFileLocation.value.get, bigqueryInterface)
+
+    }
+    catch{
+      case e: ArgotUsageException => println(e.message) 
+    }
+
+  }
+
+
   /**
    * Creates a dataset and table with snowplow events schema for the 
    * given project.
    *
-   * @param args - command line arguments consisting of:
-   *    - ProjectId (must already exist)
-   *    - DatasetId (will be created if not prexisting)
-   *    - TableId (will be created if not prexisting)
+   * @param projectId (must already exist)
+   * @param datasetId (will be created if not prexisting)
+   * @param tableId (will be created if not prexisting)
    */
-  def main (args: Array[String]){
+  def createDatasetAndTable(
+    projectId: String, 
+    datasetId: String, 
+    tableId: String, 
+    bigqueryInterface: BigqueryInterface
+  ){
 
-    require(args.length == 3, 
-      """Requires three arguments:
-      |  - ProjectId (must already exist)
-      |  - DatasetId (will be created if not prexisting)
-      |  - TableId (will be created if not prexisting)"""
-      .stripMargin
-      )
-    val projectId = args(0)
-    val datasetId = args(1)
-    val tableId = args(2)
-
-    //initializes, checks for authorization and if not authorized 
-    //starts authorization process
-    val bigQueryInterface = new BigqueryInterface(projectId)
-
-    //check if dataset exist
+    //TODO: check if dataset exist
 
     // creates dataset
-    bigQueryInterface.createDataset(datasetId)
+    bigqueryInterface.createDataset(datasetId)
 
-    //check if table exists
+    //TODO: check if table exists
 
     //create table
     val schema = TSVParser.createBigQuerySchema( BasicSchema.fields ) 
-    bigQueryInterface.createTable(datasetId, schema, tableId)
+    bigqueryInterface.createTable(datasetId, schema, tableId)
   }
-}
 
-object SnowplowDataUpload{
-  
+
   /**
    * Upload TSV data file to the given database table.
    *
-   * @param args - command line arguments consisting of:
-   *    - ProjectId (must already exist
-   *    - DatabaseId (must already exist)
-   *    - TableId (must already exist)
-   *    - TSV file location
+   * @param projectId (must already exist
+   * @param databaseId (must already exist)
+   * @param tableId (must already exist)
+   * @param fileLocation Either the filename of a TSV file, or the name of a directory 
+   *      containing TSV files (and only TSV files)
    */
-  def main (args: Array[String]) {
-    
-    require(args.length == 4, 
-      """Requires four arguments:
-      |  - ProjectId (must already exist)
-      |  - DatasetId (must already exist)
-      |  - TableId (must already exist)
-      |  - TSV File location"""
-      .stripMargin
-      )
-    val projectId = args(0)
-    val datasetId = args(1)
-    val tableId = args(2)
-    val tsvFileLocation = args(3)
-    //
-    //initializes, checks for authorization and if not authorized 
-    //starts authorization process
-    val bigQueryInterface = new BigqueryInterface(projectId)
-
+  def uploadData (
+    projectId: String, 
+    datasetId: String, 
+    tableId: String, 
+    fileLocation: String, 
+    bigqueryInterface: BigqueryInterface
+  ){
 
     def sendBatch (fileName: String) {
 
@@ -102,19 +137,21 @@ object SnowplowDataUpload{
       val tableData = TSVParser.createUploadData(dataSet)
 
       //send the data
-      bigQueryInterface.insertRows(datasetId, tableId, tableData)
+      bigqueryInterface.insertRows(datasetId, tableId, tableData)
 
     }
 
     //create array of files to be uploaded
-    val fileObj = new File(tsvFileLocation)
+    val fileObj = new File(fileLocation)
     val files =
       if (fileObj.isFile) 
-        Array(tsvFileLocation)
+        Array(fileLocation)
       else
-        fileObj.list.map(file => tsvFileLocation+"/"+file)
+        fileObj.list.map(file => fileLocation+"/"+file)
 
     //send the batches
     for (file <- files) sendBatch(file)
+
   }
+
 }
