b34ea35a52aa8caa406f729a3fb36b7acfd9c47c
Fred Blundun <fred@snowplowanalytics.com>
2015-01-21 13:30:00 +0000

    Scala Hadoop Shred: bumped Scala Common Enrich to 0.12.0 (closes #1343)

--- 3-enrich/scala-hadoop-shred/project/Dependencies.scala
+++ 3-enrich/scala-hadoop-shred/project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
     val yodaConvert     = "1.2"
     // Scala
     val json4sJackson   = "3.2.11-SNAPSHOT"
-    val commonEnrich    = "0.10.0"
+    val commonEnrich    = "0.12.0"
     val scalding        = "0.10.0"
     val scalaz7         = "7.0.0"
     val igluClient      = "0.2.0"
