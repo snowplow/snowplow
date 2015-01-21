046769b20af2776e5286c5d89a257ba63f17d84f
Fred Blundun <fred@snowplowanalytics.com>
2015-01-21 17:23:29 +0000

    Kinesis Elasticsearch Sink: bumped to 0.2.0

--- 4-storage/kinesis-elasticsearch-sink/project/BuildSettings.scala
+++ 4-storage/kinesis-elasticsearch-sink/project/BuildSettings.scala
@@ -20,7 +20,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization          :=  "Snowplow Analytics Ltd",
-    version               :=  "0.1.0",
+    version               :=  "0.2.0",
     description           :=  "Kinesis sink for Elasticsearch",
     scalaVersion          :=  "2.10.1",
     scalacOptions         :=  Seq("-deprecation", "-encoding", "utf8",
