2330f898646422c516007fd94ef86d95228c544e
Alex Dean <alex@snowplowanalytics.com>
2015-04-06 23:43:20 +0100

    StorageLoader: removed use of symbols for properties in YAML configuration (closes #1573)

--- 4-storage/storage-loader/lib/snowplow-storage-loader/config.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/config.rb
@@ -34,7 +34,7 @@ module Snowplow
       def get_config()
 
         options = Config.parse_args()
-        config = YAML.load_file(options[:config])
+        config = Config.recursive_symbolize_keys(YAML.load_file(options[:config]))
 
         # Add in our skip and include settings
         config[:skip] = options[:skip]
@@ -163,6 +163,23 @@ module Snowplow
         options
       end
       module_function :parse_args
+
+      # Convert all keys in arbitrary hash into symbols
+      # Taken from http://stackoverflow.com/a/10721936/255627
+      def self.recursive_symbolize_keys(h)
+        case h
+        when Hash
+          Hash[
+            h.map do |k, v|
+              [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+            end
+          ]
+        when Enumerable
+          h.map { |v| recursive_symbolize_keys(v) }
+        else
+          h
+        end
+      end
 
     end
   end
