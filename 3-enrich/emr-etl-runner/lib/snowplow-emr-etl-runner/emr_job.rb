3467cf2662b0d285001be7d24268b08133def9bf
Alex Dean <alex@snowplowanalytics.com>
2015-04-07 10:42:10 +0100

    EmrEtlRunner: now allowing nil for config.yml's bootstrap field (closes #1575)

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/emr_job.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/emr_job.rb
@@ -97,8 +97,10 @@ module Snowplow
 
         # Add custom bootstrap actions
         bootstrap_actions = config[:emr][:bootstrap]
-        bootstrap_actions.each do |bootstrap_action|
-          @jobflow.add_bootstrap_action(Elasticity::BootstrapAction.new(bootstrap_action))
+        unless bootstrap_actions.nil?
+          bootstrap_actions.each do |bootstrap_action|
+            @jobflow.add_bootstrap_action(Elasticity::BootstrapAction.new(bootstrap_action))
+          end
         end
 
         # Prepare a 3.x AMI for Snowplow
