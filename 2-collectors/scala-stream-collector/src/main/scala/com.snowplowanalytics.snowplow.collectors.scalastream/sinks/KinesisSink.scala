b5f190139ac34afbbda636d0d4ba2ee13c2e0b01
John Ferrolino <john@ferrl.in>
2015-04-13 10:46:23 +0800

    fix cyclomatic complexity on method createKinesisClient as notified by scalastyle

--- 2-collectors/scala-stream-collector/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/KinesisSink.scala
+++ 2-collectors/scala-stream-collector/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/KinesisSink.scala
@@ -75,8 +75,14 @@ class KinesisSink(config: CollectorConfig) extends AbstractSink {
     concurrent.ExecutionContext.fromExecutorService(executorService)
   }
 
+  import scala.util.{ Try, Success, Failure }
+
   // Create a Kinesis client for stream interactions.
-  private implicit val kinesis = createKinesisClient
+  // private implicit val kinesis = createKinesisClient
+  private implicit val kinesis = createKinesisClient match {
+    case Success(client) => client
+    case Failure(err)    => throw new Exception(err) // do something with a failure
+  }
 
   // The output stream for enriched events.
   private val enrichedStream = createAndLoadStream()
@@ -134,7 +140,6 @@ class KinesisSink(config: CollectorConfig) extends AbstractSink {
       }
     }
   }
-
   /**
    * Creates a new Kinesis client from provided AWS access key and secret
    * key. If both are set to "cpf", then authenticate using the classpath
@@ -142,27 +147,34 @@ class KinesisSink(config: CollectorConfig) extends AbstractSink {
    *
    * @return the initialized AmazonKinesisClient
    */
-  private def createKinesisClient: Client = {
+  private def createKinesisClient: Try[Client] = {
     val accessKey = config.awsAccessKey
     val secretKey = config.awsSecretKey
-    val client = if (isCpf(accessKey) && isCpf(secretKey)) {
-      new AmazonKinesisClient(new ClasspathPropertiesFileCredentialsProvider())
-    } else if (isCpf(accessKey) || isCpf(secretKey)) {
-      throw new RuntimeException("access-key and secret-key must both be set to 'cpf', or neither of them")
-    } else if (isIam(accessKey) && isIam(secretKey)) {
-      new AmazonKinesisClient(InstanceProfile)
-    } else if (isIam(accessKey) || isIam(secretKey)) {
-      throw new RuntimeException("access-key and secret-key must both be set to 'iam', or neither of them")
-    } else if (isEnv(accessKey) && isEnv(secretKey)) {
-      new AmazonKinesisClient()
-    } else if (isEnv(accessKey) || isEnv(secretKey)) {
-      throw new RuntimeException("access-key and secret-key must both be set to 'env', or neither of them")
-    } else {
-      new AmazonKinesisClient(new BasicAWSCredentials(accessKey, secretKey))
-    }
 
+    def create(aKey: String, sKey: String): AmazonKinesisClient =
+      (createCpfClient orElse createIamClient orElse createEnvClient).lift(aKey, sKey).get
+
+    val client = if (accessKey == secretKey) {
+      create(accessKey, secretKey)
+    } else { new AmazonKinesisClient(new BasicAWSCredentials(accessKey, secretKey)) }
     client.setEndpoint(config.streamEndpoint)
-    Client.fromClient(client)
+
+    Try(Client.fromClient(client))
+  }
+
+  def createCpfClient: PartialFunction[(String, String), AmazonKinesisClient] = {
+    case (aKey, sKey) if (isCpf(aKey) && isCpf(sKey)) =>
+      new AmazonKinesisClient(new ClasspathPropertiesFileCredentialsProvider())
+  }
+
+  def createIamClient: PartialFunction[(String, String), AmazonKinesisClient] = {
+    case (aKey, sKey) if (isIam(aKey) && isIam(sKey)) =>
+      new AmazonKinesisClient(InstanceProfile)
+  }
+
+  def createEnvClient: PartialFunction[(String, String), AmazonKinesisClient] = {
+    case (aKey, sKey) if (isEnv(aKey) && isEnv(sKey)) =>
+      new AmazonKinesisClient()
   }
 
   def storeRawEvent(event: CollectorPayload, key: String): Option[Array[Byte]] = {
