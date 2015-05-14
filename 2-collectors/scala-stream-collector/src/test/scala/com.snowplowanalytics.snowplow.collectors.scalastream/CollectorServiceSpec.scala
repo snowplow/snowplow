f4cf831a5b4f4b8cc5ae2b8757bfa203817e6bc0
Fred Blundun <fred@snowplowanalytics.com>
2015-05-14 15:40:48 +0100

    Scala Stream Collector: bumped to 0.5.0

--- 2-collectors/scala-stream-collector/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/CollectorServiceSpec.scala
+++ 2-collectors/scala-stream-collector/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/CollectorServiceSpec.scala
@@ -182,7 +182,7 @@ collector {
       storedEvent.timestamp must beCloseTo(DateTime.now.clicks, 1000)
       storedEvent.encoding must beEqualTo("UTF-8")
       storedEvent.ipAddress must beEqualTo("127.0.0.1")
-      storedEvent.collector must beEqualTo("ssc-0.4.0-test")
+      storedEvent.collector must beEqualTo("ssc-0.5.0-test")
       storedEvent.path must beEqualTo("/i")
       storedEvent.querystring must beEqualTo(payloadData)
     }
