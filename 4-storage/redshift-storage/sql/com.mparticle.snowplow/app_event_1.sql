74ea0e685d94f28c185874bbf5e32db93b5be185
Alex Dean <alex@snowplowanalytics.com>
2015-05-19 12:07:24 +0100

    Added the foreign key constraints for performance

--- 4-storage/redshift-storage/sql/com.mparticle.snowplow/app_event_1.sql
+++ 4-storage/redshift-storage/sql/com.mparticle.snowplow/app_event_1.sql
@@ -29,7 +29,8 @@ CREATE TABLE atomic.com_mparticle_snowplow_app_event_1 (
   ref_parent varchar(255) encode runlength not null,
   -- Properties of this type
   name varchar(64) encode text32k not null,
-  attributes varchar(10000) encode raw
+  attributes varchar(10000) encode raw,
+  FOREIGN KEY(root_id) REFERENCES atomic.events(event_id)
 )
 DISTSTYLE KEY
 -- Optimized join to atomic.events
