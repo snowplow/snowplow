12687f02414338cca585b4454ae6ea867db452fd
Fred Blundun <fred@snowplowanalytics.com>
2015-06-17 17:31:52 +0100

    StorageLoader: added script to build app (closes #1787)

--- 4-storage/storage-loader/bin/jarx-stub.sh
+++ 4-storage/storage-loader/bin/jarx-stub.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env rake
+#!/bin/sh
 
-# Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+# Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0,
 # and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -11,21 +11,15 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
-# Author::    Fred Blundun (mailto:support@snowplowanalytics.com)
-# Copyright:: Copyright (c) 2015 Snowplow Analytics Ltd
+# Author::    Alex Dean (mailto:support@snowplowanalytics.com)
+# Copyright:: Copyright (c) 2014 Snowplow Analytics Ltd
 # License::   Apache License Version 2.0
 
-require 'rake/clean'
-
-APP_NAME = 'snowplow-storage-loader'
-JAR_PATH = "deploy/#{APP_NAME}.jar"
-EXECUTABLE_PATH = "deploy/#{APP_NAME}"
-
-task :build_executable do
-  `bundle exec warble`
-  `cat bin/jarx-stub.sh #{JAR_PATH} > #{EXECUTABLE_PATH}`
-  `chmod +x #{EXECUTABLE_PATH}`
-  CLEAN.include(JAR_PATH)
-end
-
-task :default => [:build_executable, :clean]
+MYSELF=`which "$0" 2>/dev/null`
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+exec "$java" $java_args -jar $MYSELF "$@"
+exit 1
