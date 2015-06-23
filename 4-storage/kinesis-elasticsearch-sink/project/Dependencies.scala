a30926f06fc8c17e1531298c2641e44c1fffe1c0
Josh <joshua.a.beemster@gmail.com>
2015-06-23 15:24:00 +0200

    Kinesis Elasticsearch Sink: update amazon-kinesis-connectors to 1.1.2 (closes #1675)

--- 4-storage/kinesis-elasticsearch-sink/project/Dependencies.scala
+++ 4-storage/kinesis-elasticsearch-sink/project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val logging              = "1.1.3"
     val slf4j                = "1.7.5"
     val kinesisClient        = "1.0.0"
-    val kinesisConnector     = "1.1.1"
+    val kinesisConnector     = "1.1.2"
     val elasticsearch        = "1.4.4"
     // Scala
     val argot                = "1.0.1"
@@ -48,23 +48,23 @@ object Dependencies {
 
   object Libraries {
     // Java
-    val logging              = "commons-logging"            %  "commons-logging"          % V.logging
-    val slf4j                = "org.slf4j"                  %  "slf4j-simple"             % V.slf4j
-    val kinesisClient        = "com.amazonaws"              %  "amazon-kinesis-client"    % V.kinesisClient
-    val kinesisConnector     = "com.amazonaws"              %  "amazon-kinesis-connector" % V.kinesisConnector
-    val elasticsearch        = "org.elasticsearch"          %  "elasticsearch"            % V.elasticsearch
+    val logging              = "commons-logging"            %  "commons-logging"           % V.logging
+    val slf4j                = "org.slf4j"                  %  "slf4j-simple"              % V.slf4j
+    val kinesisClient        = "com.amazonaws"              %  "amazon-kinesis-client"     % V.kinesisClient
+    val kinesisConnector     = "com.amazonaws"              %  "amazon-kinesis-connectors" % V.kinesisConnector
+    val elasticsearch        = "org.elasticsearch"          %  "elasticsearch"             % V.elasticsearch
     // Scala
-    val argot                = "org.clapper"                %% "argot"                    % V.argot
-    val config               = "com.typesafe"               %  "config"                   % V.config
-    val scalaUtil            = "com.snowplowanalytics"      %  "scala-util"               % V.scalaUtil
-    val snowplowCommonEnrich = "com.snowplowanalytics"      % "snowplow-common-enrich"    % V.snowplowCommonEnrich
-    val scalazon             = "io.github.cloudify"         %% "scalazon"                 % V.scalazon
-    val scalaz7              = "org.scalaz"                 %% "scalaz-core"              % V.scalaz7
-    val snowplowTracker      = "com.snowplowanalytics"      %% "snowplow-scala-tracker"   % V.snowplowTracker
+    val argot                = "org.clapper"                %% "argot"                     % V.argot
+    val config               = "com.typesafe"               %  "config"                    % V.config
+    val scalaUtil            = "com.snowplowanalytics"      %  "scala-util"                % V.scalaUtil
+    val snowplowCommonEnrich = "com.snowplowanalytics"      % "snowplow-common-enrich"     % V.snowplowCommonEnrich
+    val scalazon             = "io.github.cloudify"         %% "scalazon"                  % V.scalazon
+    val scalaz7              = "org.scalaz"                 %% "scalaz-core"               % V.scalaz7
+    val snowplowTracker      = "com.snowplowanalytics"      %% "snowplow-scala-tracker"    % V.snowplowTracker
     // Scala (test only)
-    val specs2               = "org.specs2"                 %% "specs2"                   % V.specs2         % "test"
-    val scalazSpecs2         = "org.typelevel"              %% "scalaz-specs2"            % V.scalazSpecs2   % "test"
+    val specs2               = "org.specs2"                 %% "specs2"                    % V.specs2         % "test"
+    val scalazSpecs2         = "org.typelevel"              %% "scalaz-specs2"             % V.scalazSpecs2   % "test"
     // Scala (compile only)
-    val commonsLang3         = "org.apache.commons"         % "commons-lang3"             % V.commonsLang3   % "compile"
+    val commonsLang3         = "org.apache.commons"         % "commons-lang3"              % V.commonsLang3   % "compile"
   }
 }
