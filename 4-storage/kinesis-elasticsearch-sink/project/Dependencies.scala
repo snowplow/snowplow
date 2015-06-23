bae29f5e3bd8dba179319196b70e8df5061a2a4d
Josh <joshua.a.beemster@gmail.com>
2015-06-23 17:18:47 +0200

    Scala Kinesis Enrich: bump to Scala Common Enrich 0.15.0 (closes #1685)

--- 4-storage/kinesis-elasticsearch-sink/project/Dependencies.scala
+++ 4-storage/kinesis-elasticsearch-sink/project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     val argot                = "1.0.1"
     val config               = "1.0.2"
     val scalaUtil            = "0.1.0"
-    val snowplowCommonEnrich = "0.9.0"
+    val snowplowCommonEnrich = "0.15.0"
     val scalazon             = "0.5"
     val scalaz7              = "7.0.0"
     val snowplowTracker      = "0.1.0"
