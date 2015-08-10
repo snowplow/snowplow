dfdae1d3505ac8ad5073f5e397f00f305673b84e
Dani Solà <dani.sola@simplybusiness.co.uk>
2015-08-10 15:52:00 +0100

    Postgres: add event vendor/name/format/version to atomic.events (closes #1802)

---
+++ 4-storage/postgres-storage/sql/migrate_0.5.0_to_0.6.0.sql
@@ -0,0 +1,25 @@
+-- Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+--
+-- This program is licensed to you under the Apache License Version 2.0,
+-- and you may not use this file except in compliance with the Apache License Version 2.0.
+-- You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the Apache License Version 2.0 is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+--
+-- Version:     Ports version 0.5.0 to version 0.6.0
+-- URL:         -
+--
+-- Authors:     Dani Sola
+-- Copyright:   Copyright (c) 2015 Snowplow Analytics Ltd
+-- License:     Apache License Version 2.0
+
+-- Add new columns in atomic.events
+BEGIN;
+ALTER TABLE "atomic"."events" ADD COLUMN "event_vendor" varchar(128) DEFAULT NULL;
+ALTER TABLE "atomic"."events" ADD COLUMN "event_name" varchar(128) DEFAULT NULL;
+ALTER TABLE "atomic"."events" ADD COLUMN "event_format" varchar(25) DEFAULT NULL;
+ALTER TABLE "atomic"."events" ADD COLUMN "event_version" varchar(25) DEFAULT NULL;
+COMMIT;
