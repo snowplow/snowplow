e4a88be02693b9732f7dcb95fc9f2192ec34e605
Fred Blundun <fred@snowplowanalytics.com>
2015-08-24 15:01:50 +0100

    Scala Common Enrich: bumped Iglu Scala Client to 0.3.0 (closes #1989)

--- 3-enrich/scala-common-enrich/project/Dependencies.scala
+++ 3-enrich/scala-common-enrich/project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
     val refererParser    = "0.2.3"
     val maxmindIplookups = "0.3.0"
     val json4s           = "3.2.11"
-    val igluClient       = "0.2.0"
+    val igluClient       = "0.3.0"
     val scalaForex       = "0.3.0"
     // Scala (test only)
     val specs2           = "1.14"
