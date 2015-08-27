20e956eeea34859d9b0696fdb4be6984da5f615e
Fred Blundun <fred@snowplowanalytics.com>
2015-08-27 15:17:52 +0100

    Working locally

--- 4-storage/hadoop-elasticsearch-sink/project/Dependencies.scala
+++ 4-storage/hadoop-elasticsearch-sink/project/Dependencies.scala
@@ -23,13 +23,14 @@ object Dependencies {
     // For Snowplow libs
     "Snowplow Analytics Maven repo" at "http://maven.snplow.com/releases/",
     "Snowplow Analytics Maven snapshot repo" at "http://maven.snplow.com/snapshots/",
-    "user-agent-utils repo" at "https://raw.github.com/HaraldWalker/user-agent-utils/mvn-repo/"
+    "user-agent-utils repo" at "https://raw.github.com/HaraldWalker/user-agent-utils/mvn-repo/",
+
+    "Conjars" at "http://conjars.org/repo"
   )
   object V {
     // Java
     val hadoop              = "2.4.1"
     val cascading           = "2.6.0"
-    val elasticsearchHadoop = "2.1.0"
     // Scala
     val scalding            = "0.11.2"
     val scalaz7             = "7.0.0"
@@ -48,12 +49,12 @@ object Dependencies {
     val cascadingCore       = "cascading"                 %  "cascading-core"               % V.cascading
     val cascadingLocal      = "cascading"                 %  "cascading-local"              % V.cascading
     val cascadingHadoop     = "cascading"                 %  "cascading-hadoop2-mr1"        % V.cascading
-    val elasticsearchHadoop = "org.elasticsearch"         %  "elasticsearch-hadoop"         % V.elasticsearchHadoop
     // Scala
     val scaldingCore        = "com.twitter"               %% "scalding-core"                % V.scalding exclude( "cascading", "cascading-local" ) exclude( "cascading", "cascading-hadoop" )
     val scaldingArgs        = "com.twitter"               %% "scalding-args"                % V.scalding exclude( "cascading", "cascading-local" ) exclude( "cascading", "cascading-hadoop" )
     val scaldingCommons     = "com.twitter"               %% "scalding-commons"             % V.scalding exclude( "cascading", "cascading-local" ) exclude( "cascading", "cascading-hadoop" )
     val scaldingJson        = "com.twitter"               %% "scalding-json"                % V.scalding exclude( "cascading", "cascading-local" ) exclude( "cascading", "cascading-hadoop" )
+    val scaldingEs          = "io.scalding"               %% "scalding-taps"                % "0.6_scalding0.10"
     val scalaz7             = "org.scalaz"                %% "scalaz-core"                  % V.scalaz7
     val commonEnrich        = "com.snowplowanalytics"     %  "snowplow-common-enrich"       % V.commonEnrich
     // Scala (test only)
---
+++ 4-storage/hadoop-elasticsearch-sink/src/main/scala/mypackage/ElasticsearchJob.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics
+package snowplow
+package storage
+package hadoop
+
+// Cascading
+import cascading.tap.SinkMode
+import cascading.tuple.Fields
+
+// Scalaz
+import scalaz._
+import Scalaz._
+
+// Scalding
+import com.twitter.scalding._
+
+// import org.elasticsearch.hadoop.cascading._
+import io.scalding.taps.elasticsearch.EsSource
+
+/**
+ * Helpers for our data processing pipeline.
+ */
+object ElasticsearchJob {
+
+}
+
+/**
+ * The Snowplow Shred job, written in Scalding
+ * (the Scala DSL on top of Cascading).
+ */
+class ElasticsearchJob(args : Args) extends Job(args) {
+
+  // TODO: use withJsonInput instead of this Properties object to indicate the data is already JSON
+  val props = new java.util.Properties
+  props.setProperty("es.input.json", "true")
+
+  val writeToES = EsSource("index1/type1").withSettings(props)
+
+  val schema = ('name, 'age, 'address, 'useless)
+  val source = TextLine("/vagrant/inputs/trial")
+    .read
+    .mapTo('line -> 'output) {l: String => l}
+    .write(writeToES)
+}
+
+object App {
+  def main(args: Array[String]) {
+    val args = Array[String]("com.snowplowanalytics.snowplow.storage.hadoop.ElasticsearchJob", "--local")
+    Tool.main(args)
+  }
+}
