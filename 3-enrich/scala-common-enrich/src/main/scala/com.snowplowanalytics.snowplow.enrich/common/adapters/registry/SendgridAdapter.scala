f31f85dc0bb09730cdcf456df8bc12ac8069f30d
Josh <joshua.a.beemster@gmail.com>
2015-11-02 16:21:25 +0000

    Initial Sendgrid Setup

---
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/registry/SendgridAdapter.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics
+package snowplow
+package enrich
+package common
+package adapters
+package registry
+
+// Java
+import java.net.URI
+import org.apache.http.client.utils.URLEncodedUtils
+
+// Joda-Time
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+
+// Jackson
+import com.fasterxml.jackson.databind.JsonNode
+
+// Scala
+import scala.collection.JavaConversions._
+
+// Scalaz
+import scalaz._
+import Scalaz._
+
+// json4s
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.scalaz.JsonScalaz._
+
+// Iglu
+import iglu.client.{
+  SchemaKey,
+  Resolver
+}
+import iglu.client.validation.ValidatableJsonMethods._
+
+// This project
+import loaders.CollectorPayload
+import utils.{JsonUtils => JU}
+
+/**
+ * Transforms a collector payload which conforms to
+ * a known version of the Sendgrid Tracking webhook
+ * into raw events.
+ */
+object SendgridAdapter extends Adapter {
+
+  // Vendor name for Failure Message
+  private val VendorName = "Sendgrid"
+
+  // Expected content type for a request body
+  private val ContentType = "application/x-www-form-urlencoded"
+
+  // Tracker version for an Sendgrid Tracking webhook
+  private val TrackerVersion = "com.sendgrid-v1"
+
+  // Schemas for reverse-engineering a Snowplow unstructured event
+  private val EventSchemaMap = Map (
+    "subscribe" -> SchemaKey("com.sendgrid", "new", "jsonschema", "1-0-0").toSchemaUri
+  )
+
+  /**
+   * Converts a CollectorPayload instance into raw events.
+   * A Sendgrid Tracking payload only contains a single event.
+   * We expect the name parameter to be 1 of 6 options otherwise
+   * we have an unsupported event type.
+   *
+   * @param payload The CollectorPayload containing one or more
+   *        raw events as collected by a Snowplow collector
+   * @param resolver (implicit) The Iglu resolver used for
+   *        schema lookup and validation. Not used
+   * @return a Validation boxing either a NEL of RawEvents on
+   *         Success, or a NEL of Failure Strings
+   */
+  def toRawEvents(payload: CollectorPayload)(implicit resolver: Resolver): ValidatedRawEvents =
+    (payload.body, payload.contentType) match {
+      case (None, _)                          => s"Request body is empty: no ${VendorName} event to process".failNel
+      case (_, None)                          => s"Request body provided but content type empty, expected ${ContentType} for ${VendorName}".failNel
+      case (_, Some(ct)) if ct != ContentType => s"Content type of ${ct} provided, expected ${ContentType} for ${VendorName}".failNel
+      case (Some(body), _)                    => {
+
+        println(body)
+        return s"FAIL".failNel
+
+        /**
+        val params = toMap(URLEncodedUtils.parse(URI.create("http://localhost/?" + body), "UTF-8").toList)
+        params.get("type") match {
+          case None => s"No ${VendorName} type parameter provided: cannot determine event type".failNel
+          case Some(eventType) => {
+
+            val allParams = toMap(payload.querystring) ++ reformatParameters(params)
+            for {
+              schema <- lookupSchema(eventType.some, VendorName, EventSchemaMap)
+            } yield {
+              NonEmptyList(RawEvent(
+                api          = payload.api,
+                parameters   = toUnstructEventParams(TrackerVersion, allParams, schema, null, "srv"),
+                contentType  = payload.contentType,
+                source       = payload.source,
+                context      = payload.context
+              ))
+            }
+          }
+        }*/
+      }
+    }
+}
---
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/SendgridAdapterSpec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.enrich.common
+package adapters
+package registry
+
+// Joda-Time
+import org.joda.time.DateTime
+
+// Scalaz
+import scalaz._
+import Scalaz._
+
+// json4s
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.scalaz.JsonScalaz._
+
+// Snowplow
+import loaders.{
+  CollectorApi,
+  CollectorSource,
+  CollectorContext,
+  CollectorPayload
+}
+import utils.ConversionUtils
+import SpecHelpers._
+
+// Specs2
+import org.specs2.{Specification, ScalaCheck}
+import org.specs2.matcher.DataTables
+import org.specs2.scalaz.ValidationMatchers
+
+class SendgridAdapterSpec extends Specification with DataTables with ValidationMatchers with ScalaCheck { def is =
+
+  "This is a specification to test the SendgridAdapter functionality"                                               ^
+                                                                                                                   p^
+  "toRawEvents must do something"                                                                                 ! e1^
+                                                                                                                   end
+  implicit val resolver = SpecHelpers.IgluResolver
+
+  object Shared {
+    val api = CollectorApi("com.sendgrid", "v1")
+    val cljSource = CollectorSource("clj-tomcat", "UTF-8", None)
+    val context = CollectorContext(DateTime.parse("2013-08-29T00:18:48.000+00:00").some, "37.157.33.123".some, None, None, Nil, None)
+  }
+
+  val ContentType = "application/x-www-form-urlencoded"
+
+  def e1 = {
+    val body = "type=subscribe&data%5Bmerges%5D%5BLNAME%5D=Beemster"
+    val payload = CollectorPayload(Shared.api, Nil, ContentType.some, body.some, Shared.cljSource, Shared.context)
+    val expectedJson = 
+      """|{
+            |"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",
+            |"data":{
+              |"schema":"iglu:com.sendgrid/subscribe/jsonschema/1-0-0",
+              |"data":{
+                |"type":"subscribe",
+                |"data":{
+                  |"merges":{
+                    |"LNAME":"Beemster"
+                  |}
+                |}
+              |}
+            |}
+          |}""".stripMargin.replaceAll("[\n\r]","")
+
+    val actual = SendgridAdapter.toRawEvents(payload)
+    actual must beSuccessful(NonEmptyList(RawEvent(Shared.api, Map("tv" -> "com.sendgrid-v1", "e" -> "ue", "p" -> "srv", "ue_pr" -> expectedJson), ContentType.some, Shared.cljSource, Shared.context)))
+  }
+}
