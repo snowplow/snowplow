b79bb893caa5a9e5350a4c60f151611e2295b885
Ed Lewis <edward.richard.lewis@gmail.com>
2015-11-06 14:45:14 +0000

    added nested items test

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/AdapterSpec.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/adapters/registry/AdapterSpec.scala
@@ -147,11 +147,12 @@ class AdapterSpec extends Specification with DataTables with ValidationMatchers 
     }
 
   def e10 =
-      "SPEC NAME"                         || "JSON"                                                   | "EXPECTED OUTPUT"                                                                                                        |
-      "Remove 'event'->'type'"            !! """{"event":"type"}"""                                   ! JObject(List())                                                                                                          |
-      "Not remove existing values"        !! """{"abc":1415709559, "event":"type", "cba":"type"}"""   ! JObject(List(("abc",JInt(1415709559)),("cba",JString("type"))))                                                          |
-      "Works with ts value subs"          !! """{"ts":1415709559, "event":"type", "abc":"type"}"""    ! JObject(List(("ts",JString("2014-11-11T12:39:19.000Z")),("abc",JString("type"))))                                                      |> {
-      (_, json, expected) => BaseAdapter.cleanupJsonEventValues(parse(json), ("event", "type").some, "ts") mustEqual expected
+      "SPEC NAME"                         || "JSON"                                                      | "EXPECTED OUTPUT"                                                                                                        |
+      "Remove 'event'->'type'"            !! """{"an_event":"type"}"""                                   ! JObject(List())                                                                                                          |
+      "Not remove existing values"        !! """{"abc":1415709559, "an_event":"type", "cba":"type"}"""   ! JObject(List(("abc",JInt(1415709559)),("cba",JString("type"))))                                                          |
+      "Works with ts value subs"          !! """{"ts":1415709559, "an_event":"type", "abc":"type"}"""    ! JObject(List(("ts",JString("2014-11-11T12:39:19.000Z")),("abc",JString("type"))))                                        |
+      "Removes nested values"             !! """{"abc":"abc","nested":{"an_event":"type"}}"""            ! JObject(List(("abc", JString("abc")),("nested", JObject(List()))))                                                                |> {
+      (_, json, expected) => BaseAdapter.cleanupJsonEventValues(parse(json), ("an_event", "type").some, "ts") mustEqual expected
   }
 
 }
