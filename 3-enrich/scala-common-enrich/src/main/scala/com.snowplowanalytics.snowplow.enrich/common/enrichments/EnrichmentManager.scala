35ea3192ad24a67535462aee0278f4c993343c1d
Anton Parkhomenko <mailbox@chuwy.me>
2015-12-09 22:19:36 +0300

    Scala Common Enrich: added weather enrichment (close #456)

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/EnrichmentManager.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/EnrichmentManager.scala
@@ -451,9 +451,22 @@ object EnrichmentManager {
       case None => Nil
     }
 
+    // Fetch weather context
+    val weatherContext = registry.getWeatherEnrichment match {
+      case Some(we) => {
+        we.getWeatherContext(
+          Option(event.geo_latitude),
+          Option(event.geo_longitude),
+          Option(event.derived_tstamp).map(EventEnrichments.fromTimestamp)).map(_.some)
+      }
+      case None => None.success
+    }
+
     // Assemble array of derived contexts
     val derived_contexts = List(uaParser).collect {
       case Success(Some(context)) => context
+    } ++ List(weatherContext).collect {
+     case Success(Some(context)) => context
     } ++ jsScript.getOrElse(Nil) ++ cookieExtractorContext
 
     if (derived_contexts.size > 0) {
@@ -483,8 +496,9 @@ object EnrichmentManager {
       jsScript.toValidationNel                |@|
       campaign                                |@|
       shred                                   |@|
-      extractSchema.toValidationNel) {
-      (_,_,_,_,_,_,_,_,_) => ()
+      extractSchema.toValidationNel           |@|
+      weatherContext.toValidationNel) {
+      (_,_,_,_,_,_,_,_,_,_) => ()
     }
     (first |@| second) {
       (_,_) => event
