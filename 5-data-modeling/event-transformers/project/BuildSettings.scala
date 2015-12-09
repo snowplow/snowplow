a4e61e019d3aa126dbfff787437903f6d3665f58
Alex Dean <alex@snowplowanalytics.com>
2015-12-09 16:50:37 +0000

    WIP for a standalone event transformers lib and jar for use on Hadoop and Spark

---
+++ 5-data-modeling/event-transformers/project/BuildSettings.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+ // SBT
+import sbt._
+import Keys._
+
+object BuildSettings {
+
+  // Basic settings for our app
+  lazy val basicSettings = Seq[Setting[_]](
+    organization          :=  "com.snowplowanalytics",
+    version               :=  "0.1.0",
+    description           :=  "Contains transformers for making Snowplow enriched events easier to work with",
+    scalaVersion          :=  "2.10.4",
+    scalacOptions         :=  Seq("-deprecation", "-encoding", "utf8",
+                                  "-feature", "-target:jvm-1.7"),
+    scalacOptions in Test :=  Seq("-Yrangepos"),
+    resolvers             ++= Dependencies.resolutionRepos
+  )
+
+  // Makes our SBT app settings available from within the app
+  lazy val scalifySettings = Seq(sourceGenerators in Compile <+= (sourceManaged in Compile, version, name, organization) map { (d, v, n, o) =>
+    val file = d / "settings.scala"
+    IO.write(file, """package com.snowplowanalytics.snowplow.datamodeling.transformers.generated
+      |object Settings {
+      |  val organization = "%s"
+      |  val version = "%s"
+      |  val name = "%s"
+      |}
+      |""".stripMargin.format(o, v, n))
+    Seq(file)
+  })
+
+  // sbt-assembly settings for building a fat jar
+  import sbtassembly.Plugin._
+  import AssemblyKeys._
+  lazy val sbtAssemblySettings = assemblySettings ++ Seq(
+
+    // Slightly cleaner jar name
+    jarName in assembly := {
+      name.value + "-" + version.value + ".jar"
+    },
+
+    // Drop these jars
+    excludedJars in assembly <<= (fullClasspath in assembly) map { cp =>
+      val excludes = Set()
+      cp filter { jar => excludes(jar.data.getName) }
+    },
+
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+      (old) => {
+        // case "project.clj" => MergeStrategy.discard // Leiningen build files
+        case x if x.startsWith("META-INF") => MergeStrategy.discard // Bumf
+        case x if x.endsWith(".html") => MergeStrategy.discard // More bumf
+        case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last // For Log$Logger.class
+        case x => old(x)
+      }
+    }
+  )
+
+  lazy val buildSettings = basicSettings ++ scalifySettings ++ sbtAssemblySettings
+}
---
+++ 5-data-modeling/event-transformers/project/Dependencies.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+import sbt._
+
+object Dependencies {
+
+  val resolutionRepos = Seq()
+
+  object V {
+    // Scala
+    val json4s               = "3.2.10" // See https://github.com/json4s/json4s/issues/212
+    val scalaz7              = "7.0.0"
+    // Scala (test only)
+  }
+
+  object Libraries {
+    // Scala
+    val json4sJackson        = "org.json4s"                 %% "json4s-jackson"           % V.json4s         % "provided"
+    val scalaz7              = "org.scalaz"                 %% "scalaz-core"              % V.scalaz7
+    // Scala (test only)
+  }
+}
