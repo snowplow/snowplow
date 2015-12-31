0999dcb82d79bd6b573d6fd1b6a061bbdc7741e7
Alex Dean <alex@snowplowanalytics.com>
2015-12-31 15:54:26 +0200

    Scala Hadoop Shred: fixed good tests' checks for empty paths (fixes #2278)

--- 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/jobs/good/DerivedContextsSpec.scala
+++ 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/jobs/good/DerivedContextsSpec.scala
@@ -95,18 +95,17 @@ class DerivedContextsSpec extends Specification {
     }
 
     "not shred any unexpected JSONs" in {
-      1 must_== 1 // TODO
+      val expectedFiles = List("atomic-events", DerivedContextsSpec.expected.path)
+      JobSpecHelpers.listFilesWithExclusions(Sinks.output, expectedFiles) must be empty
     }
     "not trap any exceptions" in {
-      // TODO: not working
-      Sinks.exceptions must beEmptyDir
+      Sinks.exceptions must beEmptyFile
     }
     "not write any bad row JSONs" in {
-      // TODO: not working
-      Sinks.badRows must beEmptyDir
+      Sinks.badRows must beEmptyFile
     }
 
-    // Sinks.deleteAll()
+    Sinks.deleteAll()
     ()
   }
 }
--- 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/jobs/good/WebsitePageContextSpec.scala
+++ 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/jobs/good/WebsitePageContextSpec.scala
@@ -94,18 +94,17 @@ class WebsitePageContextSpec extends Specification {
     }
 
     "not shred any unexpected JSONs" in {
-      1 must_== 1 // TODO
+      val expectedFiles = List("atomic-events", WebsitePageContextSpec.expected.path)
+      JobSpecHelpers.listFilesWithExclusions(Sinks.output, expectedFiles) must be empty
     }
     "not trap any exceptions" in {
-      // TODO: not working
-      Sinks.exceptions must beEmptyDir
+      Sinks.exceptions must beEmptyFile
     }
     "not write any bad row JSONs" in {
-      // TODO: not working
-      Sinks.badRows must beEmptyDir
+      Sinks.badRows must beEmptyFile
     }
 
-    // Sinks.deleteAll()
+    Sinks.deleteAll()
     ()
   }
 }
