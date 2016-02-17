6c3c8c90db104ad0be75ef151a76b053ee14bdf7
Anton Parkhomenko <mailbox@chuwy.me>
2016-02-17 15:11:41 +0300

    Scala Hadoop Enrich: bumped Scala Common Enrich to 0.21.0 (closes #2442)

--- 3-enrich/scala-hadoop-enrich/project/Dependencies.scala
+++ 3-enrich/scala-hadoop-enrich/project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
     // Scala
     val scalding         = "0.11.2"
     val scalaz7          = "7.0.0"
-    val commonEnrich     = "0.20.1"
+    val commonEnrich     = "0.21.0"
     val igluClient       = "0.3.2"
     // Scala (test only)
     val specs2           = "1.14"
