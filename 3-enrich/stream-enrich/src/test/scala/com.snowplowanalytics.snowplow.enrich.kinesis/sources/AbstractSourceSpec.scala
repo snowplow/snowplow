1f64a59c9195f538e8be35b4db5fb4e6afdd10bb
Fred Blundun <fred@snowplowanalytics.com>
2016-04-07 15:00:16 +0100

    Stream Enrich: add standard copyright notice to AbstractSourceSpec.scala (closes #2580)

--- 3-enrich/stream-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.kinesis/sources/AbstractSourceSpec.scala
+++ 3-enrich/stream-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.kinesis/sources/AbstractSourceSpec.scala
@@ -1,9 +1,20 @@
-/**
- * PROPRIETARY AND CONFIDENTIAL
+/*
+ * Copyright (c) 2013-2016 Snowplow Analytics Ltd.
+ * All rights reserved.
  *
- * Unauthorized copying of this file via any medium is strictly prohibited.
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache
+ * License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the Apache License Version 2.0 for the specific language
+ * governing permissions and limitations there under.
  */
 package com.snowplowanalytics
 package snowplow.enrich
