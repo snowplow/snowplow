7638a384e5cb6b8716a7c81bc74065d130a3b82a
Fred Blundun <frederick.blundun@gmail.com>
2016-05-11 16:43:21 +0100

    WIP Improve approach to regex generation

--- 3-enrich/hadoop-event-recovery/period_to_patterns.py
+++ 3-enrich/hadoop-event-recovery/period_to_patterns.py
@@ -19,82 +19,94 @@
 from datetime import datetime, date
 import sys
 
-def to_double_digit_string(n):
-	if n < 10:
-		return '0' + str(n)
-	else:
-		return str(n)
+def get_patterns(start, end):
+	'''
+	e.g. start = [1, 6, 0, 3, 2, 9]
+	e.g. end = [1, 6, 0, 9, 0, 8]
+	'''
+	divergent_position = 0
+	while divergent_position < len(start) and start[divergent_position] == end[divergent_position]:
+		divergent_position += 1
+	prefix = start[:divergent_position]
+	low_suffix = everything_above(start[divergent_position + 1 :])
+	high_suffix = everything_below(end[divergent_position + 1 :])
+	divergent_low = start[divergent_position]
+	divergent_high = end[divergent_position]
+	output = [[divergent_low] + p for p in low_suffix] + [[divergent_high] + p for p in high_suffix]
+	if high_suffix:
+		divergent_high -= 1
+	if low_suffix:
+		divergent_low += 1
+	if divergent_high >= divergent_low:
+		output.append([DigitRange(divergent_low, divergent_high)])
+	return [prefix + x for x in output]
 
-def doit(start_date, end_date):
-	y1 = start_date.year
-	y2 = end_date.year
-	m1 = start_date.month
-	m2 = end_date.month
-	d1 = start_date.day
-	d2 = end_date.day
+def doit(start, end):
+	patterns = get_patterns(start, end)
+	for p in patterns:
+		if len(p) >= 5:
+			p.insert(4, '-')
+		if len(p) >= 3:
+			p.insert(2, '-')
+		print(make_pattern(p))
 
-	y1s = str(y1)
-	y2s = str(y2)
-	m1s = to_double_digit_string(m1)
-	m2s = to_double_digit_string(m2)
-	d1s = to_double_digit_string(d1)
-	d2s = to_double_digit_string(d2)
-
+def everything_below(start):
 	output = []
-
-	if y1 == y2:
-		if m1 == m2:
-			prefix = y1s + '-' + m1s + '-'
-			if d1 == d2:
-				output.append(prefix + d1s)
-			elif d1 < d2:
-				if d1s[0] == d2s[0]:
-					output.append(prefix + d1s[0] + '[' + d1s[1] + '-' + d2s[1] + ']')
-				else:
-					output.append(prefix + d1s[0] + '[' + d1s[1] + '-9]')
-					output.append(prefix + d2s[0] + '[0-' + d2s[1] + ']')
-					if int(d1s[0]) < int(d2s[0]) - 1:
-						output.append(prefix + '[' + str(int(d1s[0])+1) + '-' + str(int(d2s[0]) - 1) + ']')
-
-		elif m1 < m2:
-			prefix = y1s + '-'
-
-			output.append(prefix + m1s + '-' + d1s[0] + '[' + d1s[1] + '-9]' )
-			output.append(prefix + m1s + '-[' + str(int(d1s[0])+1) + '-9]')
-			if d2s[0] != '0':
-				output.append(prefix + m2s + '-[0-' + str(int(d2s[0])-1) + ']')
-			output.append(prefix + m2s + '-' + d2s[0] + '[0-' + d2s[1] + ']')
-
-			if m1 < m2 - 1:
-				if m1s[0] == m2s[0]:
-					output.append(prefix + m1s[0] + '[' + str(int(m1s[1])+1) + '-' +  str(int(m2s[1])-1) + ']')
-				else:
-					output.append(prefix + m1s[0] + '[' + str(int(m1s[1])+1) + '-9]')
-					output.append(prefix + m2s[0] + '[0-' + str(int(m2s[1])-1) + ']')
-
-	elif y1 < y2:
-		output.append(y1s + '-' + m1s + '-' + d1s[0] + '[' + d1s[1] + '-' + '9]')
-		output.append(y1s + '-' + m1s + '-[' + str(int(d1s[0])+1) + '-9]')
-		output.append(y1s + '-' + m1s[0] + '[' + str(int(m1s[1])+1) + '-9]')
-		output.append(y1s + '-[' + str(int(m1s[0])+1) + '-9]')
-
-		output.append(y2s + '-' + m2s + '-' + d2s[0] + '[0-' + d2s[1] + ']')
-		if d2s[0] != '0':
-			output.append(y2s + '-' + m2s + '-[0-' + str(int(d2s[0])-1) + ']')
-		output.append(y2s + '-' + m2s[0] + '[0-' + m2s[1] + ']')
-		if m2s[0] != '0':
-			output.append(y2s + '-[0-' + str(int(m2s[0])-1) + ']')
-
-		for year in range(y1 + 1, y2):
-			output.append(str(year))
-
+	while start and start[-1] == 9:
+		start.pop()
+	if start:
+		last = start.pop()
+		output.append(start + [DigitRange(0, last)])
+		while start:
+			last = start.pop()
+			if last > 0:
+				output.append(start + [DigitRange(0, last - 1)])
 	return output
+
+def everything_above(start):
+	output = []
+	while start and start[-1] == 0:
+		start.pop()
+	if start:
+		last = start.pop()
+		output.append(start + [DigitRange(last, 9)])
+		while start:
+			last = start.pop()
+			output.append(start + [DigitRange(last + 1, 9)])
+	return output
+
+def make_pattern(start):
+	return ''.join(map(str, start))
+
+class DigitRange(object):
+	def __init__(self, low, high):
+		self.low = low
+		self.high = high
+	def to_pattern(self):
+		if self.low == self.high:
+			return str(self.low)
+		elif self.low == self.high - 1:
+			return '[' + str(self.low) + str(self.high) + ']'
+		else:
+			return '[' + str(self.low) + '-' + str(self.high) + ']'
+	def __str__(self):
+		return self.to_pattern()
+
+def date_component_to_array(component):
+	if component < 10:
+		return [0, component]
+	else:
+		return list(map(int, str(component % 100)))
+
+def date_to_array(input_date):
+	return [digit for component in [input_date.year, input_date.month, input_date.day] for digit in date_component_to_array(component)]
 
 def main():
 	start_date = datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
 	end_date = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
-	for pattern in doit(start_date, end_date):
-		print(pattern)
+	start = date_to_array(start_date)
+	end = date_to_array(end_date)
+	doit(start, end)
 
 if __name__ == '__main__':
 	main()
