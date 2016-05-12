f0628078c499c4cf27431381537f7e02fdd0c198
Fred Blundun <fred@snowplowanalytics.com>
2016-05-12 13:42:23 +0100

    Temp

--- .travis/is_hadoop_event_recovery_release_tag.sh
+++ .travis/is_hadoop_event_recovery_release_tag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tag=$1
-cicd=${tag:0:8}
-release=${tag:8}
+cicd=${tag:0:22}
+release=${tag:22}
 
-if [ "${cicd}" == "kinesis/" ]; then
+if [ "${cicd}" == "hadoop_event_recovery/" ]; then
     if [ "${release}" == "" ]; then 
         echo "Warning! No release specified! Ignoring."
         exit 2
@@ -13,3 +13,4 @@ if [ "${cicd}" == "kinesis/" ]; then
  else 
     exit 1
  fi
+ 
--- event-recovery-ci.bash
+++ event-recovery-ci.bash
@@ -2,17 +2,12 @@
 set -e
 
 # Constants
-bintray_package=snowplow
-bintray_artifact_prefix=snowplow_kinesis_
-bintray_user=$BINTRAY_SNOWPLOW_GENERIC_USER
-bintray_repository=snowplow/snowplow-generic
 scala_version=2.10
-dist_path=dist
 root=$(pwd)
 # Next two arrays MUST match up: number of elements and order
-declare -a kinesis_app_paths=( "2-collectors/scala-stream-collector" "3-enrich/stream-enrich" "4-storage/kinesis-elasticsearch-sink" )
+app_path="3-enrich/hadoop-event-recovery"
 # TODO: version numbers shouldn't be hard-coded
-declare -a kinesis_fatjars=( "snowplow-stream-collector-0.7.0-rc2" "snowplow-stream-enrich-0.8.0-rc2" "snowplow-elasticsearch-sink-0.6.0-rc2" )
+fatjar="snowplow-hadoop-event-recovery-0.1.0-rc1.jar"
 
 # Similar to Perl die
 function die() {
@@ -25,132 +20,22 @@ function cd_root() {
 }
 
 # Assemble our fat jars
-function assemble_fatjars() {
-    for kinesis_app_path in "${kinesis_app_paths[@]}"
-        do
-            :
-            app="${kinesis_app_path##*/}"
-            echo "================================================"
-            echo "ASSEMBLING FATJAR FOR ${app}"
-            echo "------------------------------------------------"
-            cd ${kinesis_app_path} && sbt assembly
-            cd_root
-        done
+function assemble_fatjar() {
+
+    echo "================================================"
+    echo "ASSEMBLING FATJAR FOR Hadoop-Event-Recovery"
+    echo "------------------------------------------------"
+
+    cd $app_path && sbt assembly
+    cd_root
 }
-
-# Create our version in BinTray. Does nothing
-# if the version already exists
-#
-# Parameters:
-# 1. package_version
-# 2. out_error (out parameter)
-function create_bintray_package() {
-    [ "$#" -eq 2 ] || die "2 arguments required, $# provided"
-    local __package_version=$1
-    local __out_error=$2
-
-    echo "========================================"
-    echo "CREATING BINTRAY VERSION ${__package_version}*"
-    echo "* if it doesn't already exist"
-    echo "----------------------------------------"
-
-    http_status=`echo '{"name":"'${__package_version}'","desc":"Release of '${bintray_package}'"}' | curl -d @- \
-        "https://api.bintray.com/packages/${bintray_repository}/${bintray_package}/versions" \
-        --write-out "%{http_code}\n" --silent --output /dev/null \
-        --header "Content-Type:application/json" \
-        -u${bintray_user}:${bintray_api_key}`
-
-    http_status_class=${http_status:0:1}
-    ok_classes=("2" "3")
-
-    if [ ${http_status} == "409" ] ; then
-        echo "... version ${__package_version} already exists, skipping."
-    elif [[ ! ${ok_classes[*]} =~ ${http_status_class} ]] ; then
-        eval ${__out_error}="'BinTray API response ${http_status} is not 409 (package already exists) nor in 2xx or 3xx range'"
-    fi
-}
-
-# Zips all of our applications
-#
-# Parameters:
-# 1. artifact_version
-# 2. out_artifact_name (out parameter)
-# 3. out_artifact_[atj] (out parameter)
-function build_artifact() {
-    [ "$#" -eq 3 ] || die "3 arguments required, $# provided"
-    local __artifact_version=$1
-    local __out_artifact_name=$2
-    local __out_artifact_path=$3
-
-    artifact_root="${bintray_artifact_prefix}${__artifact_version}"
-    artifact_name=`echo ${artifact_root}.zip|tr '-' '_'`
-    echo "==========================================="
-    echo "BUILDING ARTIFACT ${artifact_name}"
-    echo "-------------------------------------------"
-
-    artifact_folder=./${dist_path}/${artifact_root}
-    mkdir -p ${artifact_folder}
-
-    for i in "${!kinesis_app_paths[@]}"
-        do 
-            :
-            fatjar_path="./${kinesis_app_paths[$i]}/target/scala-${scala_version}/${kinesis_fatjars[$i]}"
-            [ -f "${fatjar_path}" ] || die "Cannot find required fatjar: ${fatjar_path}. Did you forget to update fatjar versions?"
-            cp ${fatjar_path} ${artifact_folder}
-        done
-
-    artifact_path=./${dist_path}/${artifact_name}
-    zip -rj ${artifact_path} ${artifact_folder}
-    eval ${__out_artifact_name}=${artifact_name}
-    eval ${__out_artifact_path}=${artifact_path}
-}
-
-# Uploads our artifact to BinTray
-#
-# Parameters:
-# 1. artifact_name
-# 2. artifact_path
-# 3. out_error (out parameter)
-function upload_artifact_to_bintray() {
-    [ "$#" -eq 3 ] || die "3 arguments required, $# provided"
-    local __artifact_name=$1
-    local __artifact_path=$2
-    local __out_error=$3
-
-    echo "==============================="
-    echo "UPLOADING ARTIFACT TO BINTRAY*"
-    echo "* 5-10 minutes"
-    echo "-------------------------------"
-
-    http_status=`curl -T ${__artifact_path} \
-        "https://api.bintray.com/content/${bintray_repository}/${bintray_package}/${version}/${__artifact_name}?publish=1&override=1" \
-        -H "Transfer-Encoding: chunked" \
-        --write-out "%{http_code}\n" --silent --output /dev/null \
-        -u${bintray_user}:${bintray_api_key}`
-
-    http_status_class=${http_status:0:1}
-    ok_classes=("2" "3")
-
-    if [[ ! ${ok_classes[*]} =~ ${http_status_class} ]] ; then
-        eval ${__out_error}="'BinTray API response ${http_status} is not in 2xx or 3xx range'"
-    fi
-}
-
 
 cd_root
 
 version=$1
 
-bintray_api_key=$BINTRAY_SNOWPLOW_GENERIC_API_KEY
+# TODO: get AWS credentials
 
-assemble_fatjars
+assemble_fatjar
 
-create_bintray_package "${version}" "error"
-[ "${error}" ] && die "Error creating package: ${error}"
-
-artifact_name="" && artifact_path="" && build_artifact "${version}" "artifact_name" "artifact_path"
-
-upload_artifact_to_bintray "${artifact_name}" "${artifact_path}" "error"
-if [ "${error}" != "" ]; then
-    die "Error uploading package: ${error}"
-fi
+aws s3 cp 
