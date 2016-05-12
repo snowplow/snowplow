147bf13e11652368ee47c537678a94829565142e
Fred Blundun <fred@snowplowanalytics.com>
2016-05-12 17:30:17 +0100

    Add logging to tag checker

--- .travis/is_hadoop_event_recovery_release_tag.sh
+++ .travis/is_hadoop_event_recovery_release_tag.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+echo 'checking tag...'
 tag=$1
 cicd=${tag:0:22}
 release=${tag:22}
-
+echo $tag
+echo cicd
 if [ "${cicd}" == "hadoop_event_recovery/" ]; then
     if [ "${release}" == "" ]; then 
         echo "Warning! No release specified! Ignoring."
