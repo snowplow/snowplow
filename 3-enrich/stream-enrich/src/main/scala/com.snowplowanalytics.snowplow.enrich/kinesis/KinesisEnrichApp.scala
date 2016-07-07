d790ffc8b39593071a773bf80490d8cbf99fce87
Shin <shin@endource.com>
2016-07-07 17:50:40 +0100

    call Config.resolve() so environment variables get resolved in hocon config files

--- 3-enrich/stream-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/kinesis/KinesisEnrichApp.scala
+++ 3-enrich/stream-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/kinesis/KinesisEnrichApp.scala
@@ -93,7 +93,7 @@ object KinesisEnrichApp extends App {
     (c, opt) =>
       val file = new File(c)
       if (file.exists) {
-        ConfigFactory.parseFile(file)
+        ConfigFactory.parseFile(file).resolve()
       } else {
         parser.usage("Configuration file \"%s\" does not exist".format(c))
         ConfigFactory.empty()
