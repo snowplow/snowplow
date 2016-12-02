56b05c3b821b0393e4e317c6d526802289f7977f
Anton Parkhomenko <mailbox@chuwy.me>
2016-12-02 19:13:22 +0700

    Common: migrate Hadoop Event Recovery deployment to Release Manager (close #2983)

--- .travis/deploy_hadoop_event_recovery.sh
+++ .travis/deploy_hadoop_event_recovery.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-echo "--- beginning deployment ---"
+tag=$1
 
+cicd=${tag:0:22}
+release=${tag:22}
+
+if [ "${cicd}" == "hadoop_event_recovery/" ]; then
+    if [ "${release}" == "" ]; then
+        echo "Warning! No release specified! Ignoring."
+        exit 2
+    fi
+else
+    echo "This can't be deployed - there's no hadoop_event_recovery tag! (is the travis condition set?)"
+    exit 1
+fi
+ 
 cd $TRAVIS_BUILD_DIR
-./event-recovery-ci.bash
+
+export TRAVIS_BUILD_RELEASE_TAG=${release}
+release-manager --config ./.travis/release_event_recovery.yml --check-version --make-artifact --upload-artifact
