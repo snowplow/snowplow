b8678f4c1445ffcddd653ba60fb70f3f6228518a
Alex Dean <alex@snowplowanalytics.com>
2016-12-03 20:39:24 +0000

    Common: use AWS environment variables to sync Snowplow Hosted Assets buckets (close #2985)

--- vagrant/push/sync-hosted-assets.bash
+++ vagrant/push/sync-hosted-assets.bash
@@ -9,10 +9,18 @@ set -e
 #   $ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 #   $ aws configure --profile=snowplow
 
+# Similar to Perl die
+function die() {
+    echo "$@" 1>&2 ; exit 1;
+}
+
 master="snowplow-hosted-assets"
 master_region="eu-west-1"
 slave_stem="${master}-"
 declare -a regions=( "us-east-1" "us-west-1" "us-west-2" "sa-east-1" "eu-central-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ap-south-1" )
+
+[ -z "${AWS_ACCESS_KEY_ID}" ] && die "Need to set AWS_ACCESS_KEY_ID"
+[ -z "${AWS_SECRET_ACCESS_KEY}" ] && die "Need to set AWS_SECRET_ACCESS_KEY"
 
 for region in "${regions[@]}"
 do
@@ -20,5 +28,5 @@ do
 	echo "============================================"
 	echo "SYNCHRONIZING ASSETS TO ${region}"
 	echo "--------------------------------------------"
-	aws s3 --profile=snowplow cp s3://${master} s3://${slave_stem}${region} --include "*.*" --recursive --source-region=${master_region} --region=${region}
+	aws s3 cp s3://${master} s3://${slave_stem}${region} --include "*.*" --recursive --source-region=${master_region} --region=${region}
 done
