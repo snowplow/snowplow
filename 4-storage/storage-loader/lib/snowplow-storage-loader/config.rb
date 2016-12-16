139bf21256a0df6146efe4d235ab66a2a02ad358
Anton Parkhomenko <mailbox@chuwy.me>
2016-12-16 17:49:57 +0700

    StorageLoader: use storage target JSONs instead of targets section in config.yml (close #2992)

--- 4-storage/storage-loader/lib/snowplow-storage-loader/config.rb
+++ 4-storage/storage-loader/lib/snowplow-storage-loader/config.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+# Copyright (c) 2012-2016 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0,
 # and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -20,6 +20,8 @@ require 'yaml'
 require 'erb'
 require 'aws-sdk'
 require 'sluice'
+require 'json'
+require 'iglu-client'
 
 # Config module to hold functions related to CLI argument parsing
 # and config file reading to support storage loading.
@@ -27,10 +29,7 @@ module Snowplow
   module StorageLoader
     module Config
 
-      # TODO: would be nice to move this to using Kwalify
-      # TODO: would be nice to support JSON as well as YAML
-
-      @@storage_targets = Set.new(%w(redshift postgres elasticsearch))
+      @@storage_targets = Set.new(%w(redshift_config postgresql_config elasticsearch_config amazon_dynamodb_config))
 
       # Return the configuration loaded from the supplied YAML file, plus
       # the additional constants above.
@@ -52,6 +51,9 @@ module Snowplow
 
         config = Config.recursive_symbolize_keys(YAML.load(erb_config))
 
+        # Build resolver to validate targets
+        resolver = Iglu::Resolver.parse(JSON.parse(File.read(options[:resolver]), {:symbolize_names => true}))
+        config[:storage][:targets] = load_storage_targets(options[:targets], resolver)
 
         # Add in our skip and include settings
         config[:skip] = options[:skip]
@@ -72,15 +74,8 @@ module Snowplow
         
         config[:storage][:download][:folder] = Sluice::Storage::trail_slash(config[:storage][:download][:folder])
 
-        config[:storage][:targets].each { |t|
-          # Check we recognise the storage target 
-          unless @@storage_targets.include?(t[:type]) 
-            raise ConfigError, "Storage type '#{t[:type]}' not supported"
-          end
-        }
-            
         # Determine whether we need to download events
-        config[:download_required] = config[:storage][:targets].count { |t| t[:type] == "postgres" } > 0
+        config[:download_required] = config[:storage][:targets].count { |t| check_target("postgresql_config").call(t) } > 0
 
         # If Postgres is the target, check that the download folder exists and is empty
         if config[:download_required]
@@ -99,6 +94,42 @@ module Snowplow
 
         config
       end  
+
+      # Load configuration in self-describing JSONs from specified path
+      # Abort whole program on any error
+      def self.load_storage_targets(targets_path, resolver)
+        Dir.entries(targets_path).select do |f| 
+          not f.start_with?('.') 
+        end.map do |f| 
+          {:file => f, :json => JSON.parse(File.read(targets_path + '/' + f), {:symbolize_names => true}) }
+        end.map do |j|
+          begin
+            self_describing_json = Iglu::SelfDescribingJson.parse_json(j[:json])
+            self_describing_json.validate(resolver)
+            j[:json] = self_describing_json
+            j
+          rescue Exception => e
+            print "Error in [#{j[:file]}] "
+            puts e.message
+            abort("Shutting down")
+          end
+        end.map do |j|
+          target = j[:json].schema.name
+          unless @@storage_targets.include?(target)
+            print "Error in [#{j[:file]}] "
+            puts "StorageLoader doesn't support storage target configuration with name '#{target}'"
+            abort("Shutting down")
+          end
+          j[:json]
+        end
+      end
+
+      # Return lambda checking that self-describing JSON with target conforms condition
+      def self.check_target(name, model=nil, revision=nil)
+        lambda do |t|
+          t.schema.name == name and (model.nil? or t.schema.version.model == model) and (revision.nil? or t.schema.version.revision == revision)
+        end
+      end
 
     private
 
@@ -123,7 +154,7 @@ module Snowplow
       end
 
       # Parse the command-line arguments
-      # Returns: the hash of parsed options
+      # Returns: the hash of parsed options[
       def self.parse_args()
 
         # Handle command-line arguments
@@ -135,10 +166,12 @@ module Snowplow
           opts.banner = "Usage: %s [options]" % NAME
           opts.separator ""
           opts.separator "Specific options:"
-          opts.on('-c', '--config CONFIG', 'configuration file') { |config| options[:config] = config }
+          opts.on('-c', '--config CONFIG', 'configuration fgle') { |config| options[:config] = config }
           opts.on('-b', '--base64-config-string CONFIG', 'base64-encoded configuration string') { |config| options[:b64config] = config }
+          opts.on('-t', '--targets TARGETS_DIR', 'targets directory') { |config| options[:targets] = config }
           opts.on('-i', '--include compupdate,vacuum', Array, 'include optional work step(s)') { |config| options[:include] = config }
           opts.on('-s', '--skip download|delete,load,shred,analyze,archive_enriched', Array, 'skip work step(s)') { |config| options[:skip] = config }
+          opts.on('-r', '--resolver RESOLVER', 'Iglu resolver config file') { |config| options[:resolver] = config }
 
           opts.separator ""
           opts.separator "Common options:"
@@ -175,11 +208,23 @@ module Snowplow
           raise ConfigError, "Missing option: config or base64-config-string\n#{optparse}"
         end
 
+        if options[:targets].nil?
+          raise ConfigError, "Missing option: targets\n#{optparse}"
+        end
+
         # Check the config file exists if config is not read from stdin
         if options[:b64config].nil?
           unless Config.indicates_read_from_stdin?(options[:config]) || File.file?(options[:config])
             raise ConfigError, "Configuration file '#{options[:config]}' does not exist, or is not a file."
           end
+        end
+
+        if not File.directory?(options[:targets])
+          raise ConfigError, "Targets argument '#{options[:targets]}' does not exist, or is not a directory."
+        end
+
+        if options[:resolver].nil? || (not File.readable?(options[:resolver]))
+          raise ConfigError, "Resolver config '#{options[:resolver]}' isn't available"
         end
 
         options
