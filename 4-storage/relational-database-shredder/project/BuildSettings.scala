96a6bdfe8a70932efd18457ddf7a1e50b08c7fc9
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-01-06 10:17:49 +0100

    RDB Shredder: port from Scalding to Spark (closes #3034)

--- 4-storage/relational-database-shredder/project/BuildSettings.scala
+++ 4-storage/relational-database-shredder/project/BuildSettings.scala
@@ -1,40 +1,65 @@
 /*
- * Copyright (c) 2014-2017 Snowplow Analytics Ltd. All rights reserved.
+ * Copyright (c) 2012-2017 Snowplow Analytics Ltd. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the Apache License Version 2.0 is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ * See the Apache License Version 2.0 for the specific language governing permissions and
+ * limitations there under.
  */
 import sbt._
 import Keys._
 
 object BuildSettings {
 
-  // Basic settings for our app
-  lazy val basicSettings = Seq[Setting[_]](
+  lazy val buildSettings = Seq(
     organization  := "com.snowplowanalytics",
-    version       := "0.12.0",
-    description   := "Hadoop job to shred event and context JSONs from enriched event TSVs",
     scalaVersion  := "2.10.4",
-    scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
-    parallelExecution in Test := false, // Parallel tests cause havoc with MapReduce
-    resolvers     ++= Dependencies.resolutionRepos
+    scalacOptions := compilerOptions,
+    parallelExecution in Test := false, // Parallel tests cause havoc with Spark
+    oneJvmPerTestSetting // ensures that only CrossBatchDeduplicationSpec has a DuplicateStorage
   )
 
-  // sbt-assembly settings for building a fat jar
-  import sbtassembly._
-  import sbtassembly.AssemblyPlugin._
-  import sbtassembly.AssemblyKeys._
-  lazy val sbtAssemblySettings = assemblySettings ++ Seq(
+  lazy val compilerOptions = Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-language:existentials",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-unchecked",
+    "-Yno-adapted-args",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Xfuture",
+    "-Xlint",
+    "-target:jvm-1.7"
+  )
 
+  lazy val oneJvmPerTestSetting =
+    testGrouping in Test := (definedTests in Test).value map { test =>
+      Tests.Group(name = test.name, tests = Seq(test), runPolicy = Tests.SubProcess(
+        ForkOptions(
+          javaHome.value,
+          outputStrategy.value,
+          Nil,
+          Some(baseDirectory.value),
+          javaOptions.value,
+          connectInput.value,
+          envVars.value
+        )))
+    }
+
+  // sbt-assembly settings for building a fat jar
+  import sbtassembly.AssemblyPlugin.autoImport._
+  lazy val sbtAssemblySettings = Seq(
     // Slightly cleaner jar name
     assemblyJarName in assembly := { name.value + "-" + version.value + ".jar" },
-
     // For AMI 4.5.0, could be removed in future versions
     assemblyShadeRules in assembly := Seq(
       ShadeRule.rename(
@@ -43,7 +68,6 @@ object BuildSettings {
         "org.apache.http.**" -> "shadehttp.@1"
       ).inAll
     ),
-
     // Drop these jars
     assemblyExcludedJars in assembly := {
       val cp = (fullClasspath in assembly).value
@@ -57,17 +81,19 @@ object BuildSettings {
         "commons-beanutils-1.7.0.jar",      // "
         "hadoop-core-1.1.2.jar", // Provided by Amazon EMR. Delete this line if you're not on EMR
         "hadoop-tools-1.1.2.jar" // "
-      ) 
+      )
       cp.filter { jar => excludes(jar.data.getName) }
     },
-
     assemblyMergeStrategy in assembly := {
       case "project.clj" => MergeStrategy.discard // Leiningen build files
+      case x if x.startsWith("META-INF") => MergeStrategy.discard
+      case x if x.endsWith(".html") => MergeStrategy.discard
+      case x if x.endsWith("package-info.class") => MergeStrategy.first
+      case PathList("com", "google", "common", tail@_*) => MergeStrategy.first
+      case PathList("org", "apache", "spark", "unused", tail@_*) => MergeStrategy.first
       case x =>
         val oldStrategy = (assemblyMergeStrategy in assembly).value
         oldStrategy(x)
     }
   )
-
-  lazy val buildSettings = basicSettings ++ sbtAssemblySettings
 }
