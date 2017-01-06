b5804889aad866c37ff1f16d27b8f284a67b4110
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-01-06 10:17:49 +0100

    RDB Shredder: port from Scalding to Spark (closes #3034)

---
+++ 4-storage/relational-database-shredder/src/main/scala/com.snowplowanalytics.snowplow.storage/spark/ResolverSingleton.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2012-2017 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and
+ * limitations there under.
+ */
+package com.snowplowanalytics
+package snowplow
+package storage.spark
+
+// Jackson
+import com.fasterxml.jackson.databind.JsonNode
+
+// Snowplow
+import iglu.client.Resolver
+import enrich.common.{FatalEtlError, ValidatedMessage, ValidatedNelMessage}
+
+/** Singleton for Iglu's Resolver to maintain one Resolver per node. */
+object ResolverSingleton {
+  @volatile private var instance: Resolver = _
+  /**
+   * Retrieve or build an instance of Iglu's Resolver.
+   * @param igluConfig JSON representing the Iglu configuration
+   */
+  def get(igluConfig: String): Resolver = {
+    if (instance == null) {
+      synchronized {
+        if (instance == null) {
+          instance = getIgluResolver(igluConfig)
+            .valueOr(e => throw new FatalEtlError(e.toString))
+        }
+      }
+    }
+    instance
+  }
+
+  /**
+   * Build an Iglu resolver from a JSON.
+   * @param json JSON representing the Iglu resolver
+   * @return A Resolver or one or more error messages boxed in a Scalaz ValidationNel
+   */
+  private[spark] def getIgluResolver(json: String): ValidatedNelMessage[Resolver] =
+    for {
+      node <- (utils.base64.base64ToJsonNode(json, "iglu")
+        .toValidationNel: ValidatedNelMessage[JsonNode])
+      reso <- Resolver.parse(node)
+    } yield reso
+}
