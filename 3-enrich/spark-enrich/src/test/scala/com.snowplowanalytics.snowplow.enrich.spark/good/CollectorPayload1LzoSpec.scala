dcf27322b70ac5a3d7903c1b531c30100fc98521
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-01-25 14:50:20 +0100

    Spark Enrich: change the package from hadoop to spark (closes #3076)

--- 3-enrich/spark-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.spark/good/CollectorPayload1LzoSpec.scala
+++ 3-enrich/spark-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.spark/good/CollectorPayload1LzoSpec.scala
@@ -11,7 +11,7 @@
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 package com.snowplowanalytics.snowplow.enrich
-package hadoop
+package spark
 package good
 
 // Scala
@@ -124,7 +124,7 @@ object CollectorPayload1LzoSpec {
     null, //
     null, //
     null, // Unstructured event field empty
-    null, // Transaction fields empty 
+    null, // Transaction fields empty
     null, //
     null, //
     null, //
