b17175dcec2c699eb030149636289b579505bc04
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-02-10 14:01:42 +0000

    EmrEtlRunner: move current behavior into a `run` command (closes #3104)

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/cli.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/cli.rb
@@ -19,6 +19,11 @@ require 'json'
 require 'contracts'
 require 'erb'
 
+# To have a command's description
+class OptionParser
+  attr_accessor :description
+end
+
 module Snowplow
   module EmrEtlRunner
     module Cli
@@ -48,39 +53,40 @@ module Snowplow
           :debug => false
         }
 
-        optparse = OptionParser.new do |opts|
+        commands = {
+          'run' => OptionParser.new do |opts|
+            opts.banner = "Usage: run [options]"
+            opts.description = "Run the Snowplow pipeline on EMR"
+            opts.on('-c', '--config CONFIG', 'configuration file') { |config| options[:config_file] = config }
+            opts.on('-n', '--enrichments ENRICHMENTS', 'enrichments directory') {|config| options[:enrichments_directory] = config }
+            opts.on('-r', '--resolver RESOLVER', 'Iglu resolver file') {|config| options[:resolver_file] = config }
+            opts.on('-t', '--targets TARGETS', 'targets directory') { |config| options[:targets_directory] = config }
+            opts.on('-d', '--debug', 'enable EMR Job Flow debugging') { |config| options[:debug] = true }
+            opts.on('-s', '--start YYYY-MM-DD', 'optional start date *') { |config| options[:start] = config }
+            opts.on('-e', '--end YYYY-MM-DD', 'optional end date *') { |config| options[:end] = config }
+            opts.on('-x', '--skip staging,s3distcp,emr{enrich,shred,elasticsearch,archive_raw,analyze,archive_enriched}', Array, 'skip work step(s)') { |config| options[:skip] = config }
+            opts.on('-E', '--process-enrich LOCATION', 'run enrichment only on specified location. Implies --skip staging,shred,archive_raw,archive_enriched,analyze,rdb_load') { |config|
+              options[:process_enrich_location] = config
+              options[:skip] = %w(staging shred archive_raw archive_enriched analyze)
+            }
+            opts.on('-S', '--process-shred LOCATION', 'run shredding only on specified location. Implies --skip staging,enrich,archive_raw,archive_enriched,analyze,rdb_load') { |config|
+              options[:process_shred_location] = config
+              options[:skip] = %w(staging enrich archive_raw archive_enriched analyze)
+            }
+          end
+        }
 
-          opts.banner = "Usage: %s [options]" % NAME
+        global = OptionParser.new do |opts|
+          opts.banner = "Usage %s [options] [command [options]]" % NAME
           opts.separator ""
-          opts.separator "Specific options:"
-
-          opts.on('-c', '--config CONFIG', 'configuration file') { |config| options[:config_file] = config }
-          opts.on('-n', '--enrichments ENRICHMENTS', 'enrichments directory') { |config| options[:enrichments_directory] = config }
-          opts.on('-r', '--resolver RESOLVER', 'Iglu resolver file') {|config| options[:resolver_file] = config}
-          opts.on('-t', '--targets TARGETS', 'targets directory') { |config| options[:targets_directory] = config }
-          opts.on('-d', '--debug', 'enable EMR Job Flow debugging') { |config| options[:debug] = true }
-          opts.on('-s', '--start YYYY-MM-DD', 'optional start date *') { |config| options[:start] = config }
-          opts.on('-e', '--end YYYY-MM-DD', 'optional end date *') { |config| options[:end] = config }
-          opts.on('-i', '--include vacuum', Array, 'include additional step(s)') { |config| options[:include] = config }
-          opts.on('-x', '--skip staging,s3distcp,emr{enrich,shred,elasticsearch,archive_raw,analyze,archive_enriched}', Array, 'skip work step(s)') { |config| options[:skip] = config }
-          opts.on('-E', '--process-enrich LOCATION', 'run enrichment only on specified location. Implies --skip staging,shred,archive_raw,archive_enriched,analyze,rdb_load') { |config|
-            options[:process_enrich_location] = config
-            options[:skip] = %w(staging shred archive_raw archive_enriched analyze)
-          }
-          opts.on('-S', '--process-shred LOCATION', 'run shredding only on specified location. Implies   --skip staging,enrich,archive_raw,archive_enriched,analyze,rdb_load') { |config|
-            options[:process_shred_location] = config
-            options[:skip] = %w(staging enrich archive_raw analyze archive_enriched)
-          }
-
+          opts.separator "Available commands are:"
+          commands.each_pair do |c, opt|
+            desc = opt.description
+            opts.separator "#{c}: #{desc}"
+          end
           opts.separator ""
-          opts.separator "* filters the raw event logs processed by EmrEtlRunner by their timestamp. Only"
-          opts.separator "  supported with 'cloudfront' collector format currently."
-
-          opts.separator ""
-          opts.separator "Common options:"
-
-          opts.on_tail('-h', '--help', 'Show this message') { puts opts; exit }
-          opts.on_tail('-v', "--version", "Show version") do
+          opts.separator "Global options are:"
+          opts.on("-v", "--version", "Show version") do
             puts "%s %s" % [NAME, VERSION]
             exit
           end
@@ -88,12 +94,26 @@ module Snowplow
 
         # Run OptionParser's structural validation
         begin
-          optparse.parse!
+          global.order!
+          cmd_name = ARGV.shift
+          if cmd_name
+            cmd = commands[cmd_name]
+            if cmd
+              cmd.order!
+            else
+              puts "Invalid command: #{cmd_name}"
+              puts global
+              exit 1
+            end
+          else
+            puts "Empty command!"
+            puts global
+            exit 1
+          end
+          process_options(options, cmd)
         rescue OptionParser::InvalidOption, OptionParser::MissingArgument
-          raise ConfigError, "#{$!.to_s}\n#{optparse}"
+          raise ConfigError, "#{$!.to_s}\n#{cmd}"
         end
-
-        process_options(options, optparse)
       end
 
       def self.process_options(options, optparse)
@@ -107,10 +127,10 @@ module Snowplow
           :process_shred_location  => options[:process_shred_location]
         }
 
-        optp = optparse.to_s
-        config = load_config(options[:config_file], optp)
-        enrichments = load_enrichments(options[:enrichments_directory], optp)
-        resolver = load_resolver(options[:resolver_file], optp)
+        summary = optparse.to_s
+        config = load_config(options[:config_file], summary)
+        enrichments = load_enrichments(options[:enrichments_directory], summary)
+        resolver = load_resolver(options[:resolver_file], summary)
         targets = load_targets(options[:targets_directory])
 
         [args, config, enrichments, resolver, targets]
@@ -137,11 +157,11 @@ module Snowplow
 
       # Validate our args, load our config YAML, check config and args don't conflict
       Contract Maybe[String], String => ConfigHash
-      def self.load_config(config_file, optparse)
+      def self.load_config(config_file, summary)
 
         # Check we have a config file argument and it exists
         if config_file.nil?
-          raise ConfigError, "Missing option: config\n#{optparse}"
+          raise ConfigError, "Missing option: config\n#{summary}"
         end
 
         # A single hyphen indicates that the config should be read from stdin
@@ -150,7 +170,7 @@ module Snowplow
         elsif File.file?(config_file)
           config = File.new(config_file).read
         else
-          raise ConfigError, "Configuration file '#{config_file}' does not exist, or is not a file\n#{optparse}"
+          raise ConfigError, "Configuration file '#{config_file}' does not exist, or is not a file\n#{summary}"
         end
 
         erb_config = ERB.new(config).result(binding)
@@ -160,12 +180,12 @@ module Snowplow
 
       # Load the enrichments directory into an array
       Contract Maybe[String], String => ArrayOf[String]
-      def self.load_enrichments(enrichments_dir, optparse)
+      def self.load_enrichments(enrichments_dir, summary)
         return [] if enrichments_dir.nil?
 
         # Check the enrichments directory exists and is a directory
         unless Dir.exist?(enrichments_dir)
-          raise ConfigError, "Enrichments directory '#{enrichments_dir}' does not exist, or is not a directory\n#{optparse}"
+          raise ConfigError, "Enrichments directory '#{enrichments_dir}' does not exist, or is not a directory\n#{summary}"
         end
 
         json_glob = Sluice::Storage::trail_slash(enrichments_dir) + '*.json'
@@ -177,15 +197,15 @@ module Snowplow
 
       # Validate our args, load our Iglu resolver
       Contract Maybe[String], String => String
-      def self.load_resolver(resolver_file, optparse)
+      def self.load_resolver(resolver_file, summary)
 
         # Check we have a resolver file argument and it exists
         if resolver_file.nil?
-          raise ConfigError, "Missing option: resolver\n#{optparse}"
+          raise ConfigError, "Missing option: resolver\n#{summary}"
         end
 
         unless File.file?(resolver_file)
-          raise ConfigError, "Iglu resolver file '#{resolver_file}' does not exist, or is not a file\n#{optparse}"
+          raise ConfigError, "Iglu resolver file '#{resolver_file}' does not exist, or is not a file\n#{summary}"
         end
 
         File.read(resolver_file)
