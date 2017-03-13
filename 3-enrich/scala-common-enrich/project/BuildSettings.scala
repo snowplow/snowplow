1af4912df0e25f1bfecbbcb9712c12af8f22ec46
Anton Parkhomenko <mailbox@chuwy.me>
2017-03-13 20:01:29 +0700

    Bump Scala Common Enrich to M1

--- 3-enrich/scala-common-enrich/project/BuildSettings.scala
+++ 3-enrich/scala-common-enrich/project/BuildSettings.scala
@@ -21,7 +21,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization          :=  "com.snowplowanalytics",
-    version               :=  "0.24.1",
+    version               :=  "0.24.1-M1",
     description           :=  "Common functionality for enriching raw Snowplow events",
     scalaVersion          :=  "2.10.1",
     scalacOptions         :=  Seq("-deprecation", "-encoding", "utf8",
