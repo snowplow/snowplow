8ced68769c6ba3226abbea0a6e38987ada454e4d
Anton Parkhomenko <mailbox@chuwy.me>
2017-03-14 13:39:01 +0700

    Bump to rc3

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -29,6 +29,6 @@ require_relative 'snowplow-emr-etl-runner/runner'
 module Snowplow
   module EmrEtlRunner
     NAME          = "snowplow-emr-etl-runner"
-    VERSION       = "0.24.0-rc1"
+    VERSION       = "0.24.0-rc2"
   end
 end
