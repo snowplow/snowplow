a20aa1a5e8effbef8a8692438949ff47d1bc0f49
Ben Fradet <benjamin.fradet@gmail.com>
2017-05-09 15:24:22 +0100

    rc1/M1

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -28,6 +28,6 @@ require_relative 'snowplow-emr-etl-runner/runner'
 module Snowplow
   module EmrEtlRunner
     NAME          = "snowplow-emr-etl-runner"
-    VERSION       = "0.25.0"
+    VERSION       = "0.25.0-rc1"
   end
 end
