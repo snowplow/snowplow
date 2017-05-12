fad7cf2a92c42f302f494b68625f79c378c1ae0b
Ben Fradet <benjamin.fradet@gmail.com>
2017-05-12 12:53:08 +0100

    Spark Enrich: fix SqlQueryEnrichmentCfLinesSpec (closes #3224)

--- integration-tests/sql-enrichment-test.sql
+++ integration-tests/sql-enrichment-test.sql
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS enrichment_test(
 INSERT INTO enrichment_test(city,date_time,name,speed,aux,country,pk) VALUES ('New York','2016-02-07T10:10:00.000Z','eve',2.5,'ue_test_ny','USA',3);
 INSERT INTO enrichment_test(city,date_time,name,speed,aux,country,pk) VALUES ('Krasnoyarsk','2016-01-07T10:10:34.000Z','alice',10.0,'ue_test_krsk','Russia',1);
 INSERT INTO enrichment_test(city,date_time,name,speed,aux,country,pk) VALUES ('London','2016-01-08T10:00:34.000Z','bob',25.0,'ue_test_london','England',2);
-INSERT INTO enrichment_test(city,date_time,name,speed,aux,country,pk) VALUES ('Delray Beach','2016-01-08T10:00:34.000Z','boris',25.0,'ue_test_delray','US',2);
+INSERT INTO enrichment_test(city,date_time,name,speed,aux,country,pk) VALUES ('Delray Beach','2016-01-08T10:00:34.000Z','boris',25.0,'ue_test_delray','US',4);
