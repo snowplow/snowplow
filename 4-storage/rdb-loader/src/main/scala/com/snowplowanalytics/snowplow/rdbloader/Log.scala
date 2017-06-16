f4d57bff47f90ee4c51971af511e4a770bb79b0d
Anton Parkhomenko <mailbox@chuwy.me>
2017-06-16 17:57:36 +0700

    StorageLoader: add logging around database load, analyze and vacuum (close #2935)

--- 4-storage/rdb-loader/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/Log.scala
+++ 4-storage/rdb-loader/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/Log.scala
@@ -28,7 +28,7 @@ object Log {
    */
   case class LoadingSucceeded(steps: List[Step]) extends Log {
     override def toString: String = {
-      s"RDB Loader successfully loading"
+      s"RDB Loader successfully completed following steps: [${steps.mkString(", ")}]"
     }
   }
 
@@ -37,7 +37,7 @@ object Log {
    */
   case class LoadingFailed(error: String, steps: List[Step]) extends Log {
     override def toString: String = {
-      s"ERROR: $error\n"
+      s"ERROR: $error\nFollowing steps completed: [${steps.mkString(",")}]"
     }
   }
 }
