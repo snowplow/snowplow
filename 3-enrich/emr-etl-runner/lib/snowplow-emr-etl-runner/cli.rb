78ad63c3c808084003c17995948a609a8d264bfb
Anton Parkhomenko <mailbox@chuwy.me>
2017-06-27 23:57:27 +0700

    EmrEtlRunner: add S3DistCp step to move enriched and shredded files to archive (close #1777)

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/cli.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/cli.rb
@@ -63,13 +63,13 @@ module Snowplow
           opts.on('-e', '--end YYYY-MM-DD', 'optional end date *') { |config| options[:end] = config }
           opts.on('-i', '--include vacuum', Array, 'include additional step(s)') { |config| options[:include] = config }
           opts.on('-x', '--skip staging,s3distcp,emr{enrich,shred,elasticsearch,archive_raw,analyze,archive_enriched}', Array, 'skip work step(s)') { |config| options[:skip] = config }
-          opts.on('-E', '--process-enrich LOCATION', 'run enrichment only on specified location. Implies --skip staging,shred,archive_raw,analyze,archive_enriched,rdb_load') { |config|
+          opts.on('-E', '--process-enrich LOCATION', 'run enrichment only on specified location. Implies --skip staging,shred,archive_raw,archive_enriched,analyze,rdb_load') { |config|
             options[:process_enrich_location] = config
-            options[:skip] = %w(staging shred archive_raw archive_enriched rdb_load analyze)
+            options[:skip] = %w(staging shred archive_raw archive_enriched analyze)
           }
-          opts.on('-S', '--process-shred LOCATION', 'run shredding only on specified location. Implies --skip staging,enrich,archive_raw,analyze,archive_enriched,rdb_load') { |config|
+          opts.on('-S', '--process-shred LOCATION', 'run shredding only on specified location. Implies   --skip staging,enrich,archive_raw,archive_enriched,analyze,rdb_load') { |config|
             options[:process_shred_location] = config
-            options[:skip] = %w(staging enrich archive_raw analyze rdb_load archive_enriched)
+            options[:skip] = %w(staging enrich archive_raw analyze archive_enriched)
           }
 
           opts.separator ""
