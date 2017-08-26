a7ad2eb1908231e6d3f6c047a79a56016f804e13
Enes Aldemir <aldemirenes@live.com>
2017-08-27 00:01:46 +0300

    Stream Enrich: add NSQ sink (closes #3337)

--- 3-enrich/stream-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/model.scala
+++ 3-enrich/stream-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/model.scala
@@ -43,6 +43,7 @@ object model {
   case object KafkaSink extends Sink
   case object KinesisSink extends Sink
   case object StdouterrSink extends Sink
+  case object NsqSink extends Sink
 
   /** Whether the sink is for good rows or bad rows */
   sealed trait InputType
@@ -138,6 +139,7 @@ object model {
       case "kinesis"   => KinesisSink
       case "kafka"     => KafkaSink
       case "stdouterr" => StdouterrSink
+      case "nsq"       => NsqSink
       case o           => throw new IllegalArgumentException(s"Unknown enrich.sink: $o")
     }
   }
