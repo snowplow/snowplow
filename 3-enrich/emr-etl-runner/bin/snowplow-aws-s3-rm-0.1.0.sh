8bb87e44c22bb46483f57a8dd71ed3d4cc54adea
Ben Fradet <benjamin.fradet@gmail.com>
2017-09-27 17:20:12 +0100

    EmrEtlRuner: add S3 bootstrap action removing empty $folder$ files (closes #3439)

---
+++ 3-enrich/emr-etl-runner/bin/snowplow-aws-s3-rm-0.1.0.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copyright (c) 2012-2017 Snowplow Analytics Ltd. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+
+# Author::    Ben Fradet (mailto:support@snowplowanalytics.com)
+# Copyright:: Copyright (c) 2012-2017 Snowplow Analytics Ltd
+# License::   Apache License Version 2.0
+
+# Removes all the _$folder$ files in the buckets given as arguments
+for f in "$@"
+do
+  # aws s3 cli doesn't work with s3a s3n schemes
+  normalized="$(echo "$f" | sed -e 's/s3n:\/\/\|s3a:\/\//s3:\/\//g')"
+  aws s3 rm $normalized \
+    --exclude "*" \
+    --include "*$folder$" \
+    --recursive
+done
