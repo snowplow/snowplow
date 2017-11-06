32492c88c05d970a88193cfba03d9d7461c2a8d0
Ben Fradet <benjamin.fradet@gmail.com>
2017-11-06 14:37:48 +0000

    eer 0.29.0-rc6

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -35,6 +35,6 @@ require_relative 'snowplow-emr-etl-runner/linter'
 module Snowplow
   module EmrEtlRunner
     NAME    = "snowplow-emr-etl-runner"
-    VERSION = "0.29.0-rc5"
+    VERSION = "0.29.0-rc6"
   end
 end
