c817467279f6ec0c5be51c782ed2e5c083f6ded6
miike <mike@snowflake-analytics.com>
2017-11-28 08:11:55 +1100

    Redshift: update version of atomic.events to 0.9.0 (closes #3517)

--- 4-storage/redshift-storage/sql/atomic-def.sql
+++ 4-storage/redshift-storage/sql/atomic-def.sql
@@ -1,4 +1,4 @@
--- Copyright (c) 2013-2015 Snowplow Analytics Ltd. All rights reserved.
+-- Copyright (c) 2013-2017 Snowplow Analytics Ltd. All rights reserved.
 --
 -- This program is licensed to you under the Apache License Version 2.0,
 -- and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -9,11 +9,11 @@
 -- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 --
--- Version:     0.8.0
+-- Version:     0.9.0
 -- URL:         -
 --
 -- Authors:     Yali Sassoon, Alex Dean, Peter van Wesep, Fred Blundun
--- Copyright:   Copyright (c) 2013-2015 Snowplow Analytics Ltd
+-- Copyright:   Copyright (c) 2013-2017 Snowplow Analytics Ltd
 -- License:     Apache License Version 2.0
 
 -- Create the schema
