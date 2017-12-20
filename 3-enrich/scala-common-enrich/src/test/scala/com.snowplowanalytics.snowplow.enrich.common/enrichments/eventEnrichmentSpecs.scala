7d0fbf273c450ceb9af1ed9cca491c6d481fc106
Ben Fradet <benjamin.fradet@gmail.com>
2017-12-20 15:25:19 +0000

    Scala Common Enrich: add validation of tracker-sent timestamps (closes #336)

--- 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/eventEnrichmentSpecs.scala
+++ 3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/eventEnrichmentSpecs.scala
@@ -76,9 +76,10 @@ class ExtractEventTypeSpec extends Specification with DataTables with Validation
     }
 
   def e4 =
-    "SPEC NAME"          || "INPUT VAL"           | "EXPECTED OUTPUT"                                                    |
-    "Not a long"         !! ("f", "v")            ! "Field [f]: [v] is not in the expected format (ms since epoch)".fail |
-    "Valid timestamp"    !! ("f", "1")            ! "1970-01-01 00:00:00.001".success                                    |> {
+    "SPEC NAME" || "INPUT VAL"               | "EXPECTED OUTPUT"                                                                                               |
+    "Not long"  !! ("f", "v")                ! "Field [f]: [v] is not in the expected format (ms since epoch)".fail                                            |
+    "Too long"  !! ("f", "1111111111111111") ! "Field [f]: [1111111111111111] is formatted as [37179-09-17 07:18:31.111] which isn't Redshift-compatible".fail |
+    "Valid ts"  !! ("f", "1")                ! "1970-01-01 00:00:00.001".success                                                                               |> {
       (_, input, expected) => EventEnrichments.extractTimestamp(input._1, input._2) must_== (expected)
     }
 }
