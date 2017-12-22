709c9cd8b9471fe4cea7b40be69e97ac83085b6a
Ben Fradet <benjamin.fradet@gmail.com>
2017-12-22 16:04:50 +0000

    Stream Enrich: bump scala-common-enrich to 0.29.0 (closes #3553)

--- 3-enrich/stream-enrich/project/Dependencies.scala
+++ 3-enrich/stream-enrich/project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     val json4s               = "3.2.11"
     val pureconfig           = "0.8.0"
     val snowplowRawEvent     = "0.1.0"
-    val snowplowCommonEnrich = "0.26.0"
+    val snowplowCommonEnrich = "0.29.0"
     val igluClient           = "0.5.0"
     val snowplowTracker      = "0.3.0"
     // Scala (test only)
