553e069847ce88e5947b7e64be3450a57a365718
Konstantinos Servis <knservis@gmail.com>
2018-01-17 14:42:54 +0200

    Scala Common Enrich: extend PII Enrichment to include identification events in EnrichedEvent (closes #3580)

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/outputs/EnrichedEvent.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/outputs/EnrichedEvent.scala
@@ -244,4 +244,7 @@ class EnrichedEvent extends Serializable {
 
   // True timestamp
   @BeanProperty var true_tstamp: String = _
+
+  // Fields modified form PII enrichemnt (JSON String)
+  @BeanProperty var pii: String = _
 }
