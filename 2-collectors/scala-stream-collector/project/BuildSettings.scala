9643cd3291e17a5c49290d5085590ef431b734d5
Ben Fradet <benjamin.fradet@gmail.com>
2018-02-20 15:17:39 +0000

    Scala Stream Collector: use sbt-buildinfo (closes #3626)

--- 2-collectors/scala-stream-collector/project/BuildSettings.scala
+++ 2-collectors/scala-stream-collector/project/BuildSettings.scala
@@ -41,26 +41,10 @@ object BuildSettings {
     "-target", "1.8"
   )
 
-  // Makes our SBT app settings available from within the app
-  lazy val scalifySettings = Seq(
-    sourceGenerators in Compile += Def.task {
-      val file = (sourceManaged in Compile).value / "settings.scala"
-      IO.write(file, """package com.snowplowanalytics.snowplow.collectors.scalastream.generated
-        |object Settings {
-        |  val organization = "%s"
-        |  val version = "%s"
-        |  val name = "%s"
-        |  val shortName = "ssc"
-        |}
-        |""".stripMargin.format(organization.value, version.value, name.value))
-      Seq(file)
-    }.taskValue
-  )
-
   // sbt-assembly settings for building an executable
   import sbtassembly.AssemblyPlugin.autoImport._
   lazy val sbtAssemblySettings = Seq(
-    assemblyJarName in assembly := { s"${name.value}-${version.value}.jar" },
+    assemblyJarName in assembly := { s"${moduleName.value}-${version.value}.jar" },
     // merge strategy for fixing netty conflict
     assemblyMergeStrategy in assembly := {
       case PathList("io", "netty", xs @ _*) => MergeStrategy.first
