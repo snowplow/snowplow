40f0ed9c2f0801e06b658787bc4781684e4e5f38
jspc <james.condron@beamly.com>
2018-02-22 12:28:25 +0000

    Scala Stream Collector: expose number of requests over JMX (closes #3637)

---
+++ 2-collectors/scala-stream-collector/core/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/monitoring/JMX.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2013-2018 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0, and
+ * you may not use this file except in compliance with the Apache License
+ * Version 2.0.  You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License Version 2.0 is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the Apache License Version 2.0 for the specific language
+ * governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.collectors.scalastream
+package monitoring
+
+import java.lang.management.ManagementFactory
+import javax.management.{MBeanServer, ObjectName}
+import scala.beans.BeanProperty
+
+trait SnowplowMXBean {
+  def getLatest(): Long
+  def getRequests(): Int
+  def getSuccessfulRequests(): Int
+  def getFailedRequests(): Int
+
+  def incrementSuccessfulRequests(): Unit
+  def incrementFailedRequests(): Unit
+}
+
+class SnowplowScalaCollectorMetrics extends SnowplowMXBean {
+  @BeanProperty
+  var latest = now()
+
+  @BeanProperty
+  var requests = 0
+
+  @BeanProperty
+  var successfulRequests = 0
+
+  @BeanProperty
+  var failedRequests = 0
+
+  override def incrementSuccessfulRequests(): Unit = {
+    successfulRequests += 1
+    incrementRequests()
+  }
+
+  override def incrementFailedRequests(): Unit = {
+    failedRequests += 1
+    incrementRequests()
+  }
+
+  private def incrementRequests(): Unit = {
+    requests += 1
+    latest = now()
+  }
+
+  def now(): Long = System.currentTimeMillis() / 1000L
+}
+
+object BeanRegistry {
+  val collectorBean = new SnowplowScalaCollectorMetrics
+
+  val mbs: MBeanServer = ManagementFactory.getPlatformMBeanServer
+  val mBeanName: ObjectName =
+    new ObjectName(s"${this.getClass.getPackage.getName}:type=ScalaCollector")
+
+  mbs.registerMBean(collectorBean, mBeanName)
+}
