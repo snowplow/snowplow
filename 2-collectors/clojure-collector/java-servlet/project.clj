878e80c7977749bdee7c5d31c5b4c433c26863d9
Ben Fradet <benjamin.fradet@gmail.com>
2018-05-30 12:17:38 +0100

    Clojure Collector: bump ring to 1.6.3 (closes #3778)

--- 2-collectors/clojure-collector/java-servlet/project.clj
+++ 2-collectors/clojure-collector/java-servlet/project.clj
@@ -22,14 +22,14 @@
   :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :description "A SnowPlow event collector written in Clojure. AWS Elastic Beanstalk compatible."
   :dependencies     [[org.clojure/clojure "1.4.0"]
-                     [ring/ring-core "1.1.8"]
-                     [ring/ring-devel "1.1.8"]
+                     [ring/ring-core "1.6.3"]
+                     [ring/ring-devel "1.6.3"]
                      [compojure "1.1.3"]
                      [metrics-clojure "0.9.2"]
                      [metrics-clojure-ring "0.9.2"]
                      [commons-codec/commons-codec "1.7"]]
   ;; The jetty adapter is only used during development
-  :profiles         {:dev {:dependencies [[ring/ring-jetty-adapter "1.1.8"]]}}
+  :profiles         {:dev {:dependencies [[ring/ring-jetty-adapter "1.6.3"]]}}
   :war-resources-path   "war-resources"
   :plugins          [[lein-ring "0.8.3"]
                      [lein-beanstalk "0.2.6"]]
