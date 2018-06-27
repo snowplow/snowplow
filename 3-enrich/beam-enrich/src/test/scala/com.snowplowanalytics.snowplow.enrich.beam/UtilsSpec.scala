d5963ea7db45f21b8d15db422c6eb76f53f44855
Ben Fradet <benjamin.fradet@gmail.com>
2018-06-27 18:33:56 +0100

    Beam Enrich: add metrics (closes #3737)

--- 3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/UtilsSpec.scala
+++ 3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/UtilsSpec.scala
@@ -16,15 +16,66 @@ package com.snowplowanalytics
 package snowplow.enrich
 package beam
 
+import java.net.URI
+import java.nio.file.{Files, Paths}
+
 import org.scalatest._
-import Matchers._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
 import scalaz._
 
 import common.outputs.{EnrichedEvent, BadRow}
 import utils._
 
-class UtilsSpec extends FreeSpec {
+class UtilsSpec extends FreeSpec with Matchers {
   "the utils object should" - {
+    "make a timeMs function available" - {
+      "which report the time spent doing an operation" in {
+        val (res, time) = timeMs((1L to 20L).foldRight(1L) { case (a, b) => a * b })
+        res shouldBe 2432902008176640000L
+        assert(time >= 1)
+      }
+    }
+    "make a createSymLink function available" - {
+      "which creates a symbolic link" in {
+        val f = Files.createTempFile("test1", ".txt").toFile
+        val s = "/tmp/test1.txt-symlink"
+        createSymLink(f, s) shouldEqual Right(Paths.get(s))
+        f.delete
+        Paths.get(s).toFile.delete
+      }
+      "which fails if the symlink already exists" in {
+        val f = Files.createTempFile("test2", ".txt").toFile
+        createSymLink(f, f.toString) shouldEqual Left(s"Symlink ${f.toString} already exists")
+        f.delete
+      }
+      "which fails if the symbolic link can't be created" in {
+        val f = Files.createTempFile("test3", ".txt").toFile
+        val s = "/proc/test3.txt-symlink"
+        createSymLink(f, s) shouldEqual Left(s"Symlink can't be created: ${Paths.get(s)}")
+        f.delete
+      }
+    }
+    "make a getFilesToCache function available" - {
+      "which sends back the files that need caching" in {
+        val res = parse(SpecHelpers.resolverConfig)
+        val reg = (("schema" -> "iglu:com.snowplowanalytics.snowplow/enrichments/jsonschema/1-0-0") ~
+          ("data" -> List(parse(SpecHelpers.ipLookupsEnrichmentConfig))))
+        getFilesToCache(res, reg) should contain only (
+          (new URI("http://acme.com/GeoLite2-City.mmdb"), "./ip_geo"))
+      }
+    }
+    "make a getEnrichedEventMetrics function available" - {
+      "which sends back vendor and tracker metrics" in {
+        val event = {
+          val e = new EnrichedEvent()
+          e.v_tracker = "go-1.0.0"
+          e.event_vendor = "com.acme"
+          e
+        }
+        getEnrichedEventMetrics(event) should contain only ("vendor_com_acme", "tracker_go_1_0_0")
+      }
+    }
     "make a getStringSize function available" - {
       "which sends back the size of a string in bytes" in {
         getStringSize("a" * 10) shouldEqual 10
