7d8c6f6a1729fb640ac3683f50ec52ddb96b7e02
Ben Fradet <benjamin.fradet@gmail.com>
2019-02-25 14:15:48 +0100

    Stream Enrich: extend copyright notice to 2019 (closes #4000)

--- 3-enrich/stream-enrich/core/src/test/scala/com.snowplowanalytics.snowplow.enrich.stream/sources/TestSource.scala
+++ 3-enrich/stream-enrich/core/src/test/scala/com.snowplowanalytics.snowplow.enrich.stream/sources/TestSource.scala
@@ -1,5 +1,5 @@
  /*
- * Copyright (c) 2013-2018 Snowplow Analytics Ltd.
+ * Copyright (c) 2013-2019 Snowplow Analytics Ltd.
  * All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0, and
