f60de0c650a64b4f13475d1e0e4d1a0112d3b9be
Ben Fradet <benjamin.fradet@gmail.com>
2019-03-29 16:00:22 +0100

    [WIP] Scala Common Enrich: bump scala-forex to 0.7.0 (closes #4031)

--- 3-enrich/scala-common-enrich/project/Dependencies.scala
+++ 3-enrich/scala-common-enrich/project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
     val circeOptics      = "0.11.0"
     val circeJackson     = "0.11.1"
     val igluClient       = "0.6.0-M3"
-    val scalaForex       = "0.5.0"
+    val scalaForex       = "0.7.0-M1"
     val scalaWeather     = "0.3.0"
     val scalaj           = "2.3.0"
     val gatlingJsonpath  = "0.6.14"
