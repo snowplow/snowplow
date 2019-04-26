c69ae8f7cf87be4bb715acd7d774d689f8974e5a
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2019-04-26 12:59:30 +0200

    Scala Common Enrich: support IPv6 addresses in IAB enrichment (closes #3951)

--- 3-enrich/spark-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.spark/good/IabEnrichmentCfLineSpec.scala
+++ 3-enrich/spark-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.spark/good/IabEnrichmentCfLineSpec.scala
@@ -23,7 +23,9 @@ object IabEnrichmentCfLineSpec {
 
   val lines = Lines(
     "2012-05-27  11:35:53  DFW3  3343  216.160.83.56 GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla/5.0%20(Windows%20NT%206.1;%20WOW64;%20rv:12.0)%20Gecko/20100101%20Firefox/12.0  &e=ue&ue_pr=%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow%2Funstruct_event%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow.input-adapters%2Fsegment_webhook_config%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22vendor%22%3A%22%CE%A7%CE%B1%CF%81%CE%B9%CF%84%CE%AF%CE%BD%CE%B7%20NEW%20Unicode%20test%22%2C%22name%22%3A%22alex%2Btest%40snowplowanalytics.com%22%2C%22parameters%22%3A%7B%22mappings%22%3A%7B%22eventsPerMonth%22%3A%22%3C%201%20million%22%2C%22serviceType%22%3A%22unsure%22%7D%7D%7D%7D%7D&dtm=1364230969450&evn=com.acme&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards",
-    "2012-05-27  11:35:53  DFW3  3343  216.160.83.56 GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla%2F5.0%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko%3B%20compatible%3B%20Googlebot%2F2.1%3B%20%2Bhttp%3A%2F%2Fwww.google.com%2Fbot.html)%20Safari%2F537.36  &e=ue&ue_pr=%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow%2Funstruct_event%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow.input-adapters%2Fsegment_webhook_config%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22vendor%22%3A%22%CE%A7%CE%B1%CF%81%CE%B9%CF%84%CE%AF%CE%BD%CE%B7%20NEW%20Unicode%20test%22%2C%22name%22%3A%22alex%2Btest%40snowplowanalytics.com%22%2C%22parameters%22%3A%7B%22mappings%22%3A%7B%22eventsPerMonth%22%3A%22%3C%201%20million%22%2C%22serviceType%22%3A%22unsure%22%7D%7D%7D%7D%7D&dtm=1364230969450&evn=com.acme&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards"
+    "2012-05-27  11:35:53  DFW3  3343  216.160.83.56 GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla%2F5.0%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko%3B%20compatible%3B%20Googlebot%2F2.1%3B%20%2Bhttp%3A%2F%2Fwww.google.com%2Fbot.html)%20Safari%2F537.36  &e=ue&ue_pr=%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow%2Funstruct_event%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow.input-adapters%2Fsegment_webhook_config%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22vendor%22%3A%22%CE%A7%CE%B1%CF%81%CE%B9%CF%84%CE%AF%CE%BD%CE%B7%20NEW%20Unicode%20test%22%2C%22name%22%3A%22alex%2Btest%40snowplowanalytics.com%22%2C%22parameters%22%3A%7B%22mappings%22%3A%7B%22eventsPerMonth%22%3A%22%3C%201%20million%22%2C%22serviceType%22%3A%22unsure%22%7D%7D%7D%7D%7D&dtm=1364230969450&evn=com.acme&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards",
+    "2012-05-27  11:35:53  DFW3  3343  216.160.83.56:8080 GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla%2F5.0%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko%3B%20compatible%3B%20Googlebot%2F2.1%3B%20%2Bhttp%3A%2F%2Fwww.google.com%2Fbot.html)%20Safari%2F537.36  &e=ue&ue_pr=%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow%2Funstruct_event%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow.input-adapters%2Fsegment_webhook_config%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22vendor%22%3A%22%CE%A7%CE%B1%CF%81%CE%B9%CF%84%CE%AF%CE%BD%CE%B7%20NEW%20Unicode%20test%22%2C%22name%22%3A%22alex%2Btest%40snowplowanalytics.com%22%2C%22parameters%22%3A%7B%22mappings%22%3A%7B%22eventsPerMonth%22%3A%22%3C%201%20million%22%2C%22serviceType%22%3A%22unsure%22%7D%7D%7D%7D%7D&dtm=1364230969450&evn=com.acme&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards",
+    "2012-05-27  11:35:53  DFW3  3343  2001:db8:0:0:0:ff00:42:8329  GET d3gs014xn8p70.cloudfront.net  /ice.png  200 http://www.psychicbazaar.com/oracles/119-psycards-book-and-deck-starter-pack.html?view=print#detail Mozilla%2F5.0%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko%3B%20compatible%3B%20Googlebot%2F2.1%3B%20%2Bhttp%3A%2F%2Fwww.google.com%2Fbot.html)%20Safari%2F537.36  &e=ue&ue_pr=%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow%2Funstruct_event%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22schema%22%3A%22iglu%3Acom.snowplowanalytics.snowplow.input-adapters%2Fsegment_webhook_config%2Fjsonschema%2F1-0-0%22%2C%22data%22%3A%7B%22vendor%22%3A%22%CE%A7%CE%B1%CF%81%CE%B9%CF%84%CE%AF%CE%BD%CE%B7%20NEW%20Unicode%20test%22%2C%22name%22%3A%22alex%2Btest%40snowplowanalytics.com%22%2C%22parameters%22%3A%7B%22mappings%22%3A%7B%22eventsPerMonth%22%3A%22%3C%201%20million%22%2C%22serviceType%22%3A%22unsure%22%7D%7D%7D%7D%7D&dtm=1364230969450&evn=com.acme&tid=598951&vp=2560x934&ds=2543x1420&vid=43&duid=9795bd0203804cd1&p=web&tv=js-0.11.1&fp=2876815413&aid=pbzsite&lang=en-GB&cs=UTF-8&tz=Europe%2FLondon&refr=http%3A%2F%2Fwww.psychicbazaar.com%2F&f_pdf=1&f_qt=0&f_realp=0&f_wma=0&f_dir=0&f_fla=1&f_java=1&f_gears=0&f_ag=1&res=2560x1440&cd=32&cookie=1&url=http%3A%2F%2Fwww.psychicbazaar.com%2F2-tarot-cards"
   )
   val expected = List(
     List(
@@ -289,6 +291,270 @@ object IabEnrichmentCfLineSpec {
       "jsonschema",
       "1-0-0",
       "6a2f7bbf7669eb1808c79cfe3e1f8e57"
+    ),
+    List(
+      "pbzsite",
+      "web",
+      etlTimestamp,
+      "2012-05-27 11:35:53.000",
+      "2013-03-25 17:02:49.450",
+      "unstruct",
+      null, // We can't predict the event_id
+      "598951",
+      null, // No tracker namespace
+      "js-0.11.1",
+      "cloudfront",
+      etlVersion,
+      null, // No user_id set
+      "4ed25a4b97b34bf0dbbe2ba0899e868743b0c5d2",
+      "2876815413",
+      "9795bd0203804cd1",
+      "43",
+      null, // No network_userid set
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null, // Using the MaxMind domain lookup service
+      null,
+      "http://www.psychicbazaar.com/2-tarot-cards",
+      null, // No page title for events
+      "http://www.psychicbazaar.com/",
+      "http",
+      "www.psychicbazaar.com",
+      "80",
+      "/2-tarot-cards",
+      null,
+      null,
+      "http",
+      "www.psychicbazaar.com",
+      "80",
+      "/",
+      null,
+      null,
+      "internal", // Internal referer
+      null,
+      null,
+      null, // No marketing campaign info
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // No custom contexts
+      null, // Structured event fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      """{"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0","data":{"schema":"iglu:com.snowplowanalytics.snowplow.input-adapters/segment_webhook_config/jsonschema/1-0-0","data":{"vendor":"Χαριτίνη NEW Unicode test","name":"alex+test@snowplowanalytics.com","parameters":{"mappings":{"eventsPerMonth":"< 1 million","serviceType":"unsure"}}}}}""", // Unstructured event field set
+      null, // Transaction fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // Transaction item fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // Page ping fields are empty
+      null, //
+      null, //
+      null, //
+      "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36",
+      "Robot/Spider",
+      "Robot/Spider",
+      null,
+      "Robot",
+      "OTHER",
+      "en-GB",
+      "1",
+      "1",
+      "1",
+      "0",
+      "0",
+      "0",
+      "0",
+      "0",
+      "1",
+      "1",
+      "32",
+      "2560",
+      "934",
+      "Unknown",
+      "Unknown",
+      "Other",
+      "Europe/London",
+      "Unknown",
+      "0",
+      "2560",
+      "1440",
+      "UTF-8",
+      "2543",
+      "1420",
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      """{"schema":"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-1","data":[{"schema":"iglu:com.snowplowanalytics.snowplow/ua_parser_context/jsonschema/1-0-0","data":{"useragentFamily":"Googlebot","useragentMajor":"2","useragentMinor":"1","useragentPatch":null,"useragentVersion":"Googlebot 2.1","osFamily":"Other","osMajor":null,"osMinor":null,"osPatch":null,"osPatchMinor":null,"osVersion":"Other","deviceFamily":"Spider"}},{"schema":"iglu:com.iab.snowplow/spiders_and_robots/jsonschema/1-0-0","data":{"spiderOrRobot":true,"category":"SPIDER_OR_ROBOT","reason":"FAILED_UA_INCLUDE","primaryImpact":"UNKNOWN"}}]}""",
+      null,
+      "2012-05-27 11:35:53.000",
+      "com.snowplowanalytics.snowplow.input-adapters",
+      "segment_webhook_config",
+      "jsonschema",
+      "1-0-0",
+      "6a2f7bbf7669eb1808c79cfe3e1f8e57"
+    ),
+    List(
+      "pbzsite",
+      "web",
+      etlTimestamp,
+      "2012-05-27 11:35:53.000",
+      "2013-03-25 17:02:49.450",
+      "unstruct",
+      null, // We can't predict the event_id
+      "598951",
+      null, // No tracker namespace
+      "js-0.11.1",
+      "cloudfront",
+      etlVersion,
+      null, // No user_id set
+      "64083e4f337efb8e8ca3fb81a360a67836ebafe6",
+      "2876815413",
+      "9795bd0203804cd1",
+      "43",
+      null, // No network_userid set
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null, // Using the MaxMind domain lookup service
+      null,
+      "http://www.psychicbazaar.com/2-tarot-cards",
+      null, // No page title for events
+      "http://www.psychicbazaar.com/",
+      "http",
+      "www.psychicbazaar.com",
+      "80",
+      "/2-tarot-cards",
+      null,
+      null,
+      "http",
+      "www.psychicbazaar.com",
+      "80",
+      "/",
+      null,
+      null,
+      "internal", // Internal referer
+      null,
+      null,
+      null, // No marketing campaign info
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // No custom contexts
+      null, // Structured event fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      """{"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0","data":{"schema":"iglu:com.snowplowanalytics.snowplow.input-adapters/segment_webhook_config/jsonschema/1-0-0","data":{"vendor":"Χαριτίνη NEW Unicode test","name":"alex+test@snowplowanalytics.com","parameters":{"mappings":{"eventsPerMonth":"< 1 million","serviceType":"unsure"}}}}}""", // Unstructured event field set
+      null, // Transaction fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // Transaction item fields empty
+      null, //
+      null, //
+      null, //
+      null, //
+      null, //
+      null, // Page ping fields are empty
+      null, //
+      null, //
+      null, //
+      "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36",
+      "Robot/Spider",
+      "Robot/Spider",
+      null,
+      "Robot",
+      "OTHER",
+      "en-GB",
+      "1",
+      "1",
+      "1",
+      "0",
+      "0",
+      "0",
+      "0",
+      "0",
+      "1",
+      "1",
+      "32",
+      "2560",
+      "934",
+      "Unknown",
+      "Unknown",
+      "Other",
+      "Europe/London",
+      "Unknown",
+      "0",
+      "2560",
+      "1440",
+      "UTF-8",
+      "2543",
+      "1420",
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      """{"schema":"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-1","data":[{"schema":"iglu:com.snowplowanalytics.snowplow/ua_parser_context/jsonschema/1-0-0","data":{"useragentFamily":"Googlebot","useragentMajor":"2","useragentMinor":"1","useragentPatch":null,"useragentVersion":"Googlebot 2.1","osFamily":"Other","osMajor":null,"osMinor":null,"osPatch":null,"osPatchMinor":null,"osVersion":"Other","deviceFamily":"Spider"}}]}""",
+      null,
+      "2012-05-27 11:35:53.000",
+      "com.snowplowanalytics.snowplow.input-adapters",
+      "segment_webhook_config",
+      "jsonschema",
+      "1-0-0",
+      "6a2f7bbf7669eb1808c79cfe3e1f8e57"
     )
   )
 }
@@ -315,7 +581,7 @@ class IabEnrichmentCfLineSpec extends Specification with EnrichJobSpec {
 
     "correctly attach a derived context fetched from the IAB enrichment" in {
       val Some(goods) = readPartFile(dirs.output)
-      goods.size must_== 2
+      goods.size must_== 4
       val actual = goods.map(evt => evt.split("\t").map(s => if (s.isEmpty()) null else s))
       for {
         evtIdx <- IabEnrichmentCfLineSpec.expected.indices
