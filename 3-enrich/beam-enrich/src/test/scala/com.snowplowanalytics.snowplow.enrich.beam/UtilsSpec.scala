f28cdf46fdc474c8f56de1cf65edc888cb1b442e
Ben Fradet <benjamin.fradet@gmail.com>
2019-06-12 11:23:01 +0200

    Beam Enrich: bump scala-common-enrich to 1.0.0 (closes #4165)

--- 3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/UtilsSpec.scala
+++ 3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/UtilsSpec.scala
@@ -12,22 +12,25 @@
  * See the Apache License Version 2.0 for the specific language governing permissions and
  * limitations there under.
  */
-package com.snowplowanalytics
-package snowplow.enrich
-package beam
+package com.snowplowanalytics.snowplow.enrich.beam
 
-import java.net.URI
 import java.nio.file.{Files, Paths}
+import java.time.Instant
+
+import com.snowplowanalytics.snowplow.badrows._
+import com.snowplowanalytics.snowplow.enrich.common.outputs.EnrichedEvent
+import cats.implicits._
+import io.circe.parser
+import com.snowplowanalytics.iglu.core.SelfDescribingData
+import com.snowplowanalytics.iglu.core.circe.instances._
 
 import org.scalatest._
-import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
-import scalaz._
 
-import common.outputs.{EnrichedEvent, BadRow}
 import utils._
 
 class UtilsSpec extends FreeSpec with Matchers {
+  val processor = Processor("be", "1.0.0")
+
   "the utils object should" - {
     "make a timeMs function available" - {
       "which report the time spent doing an operation" in {
@@ -56,15 +59,6 @@ class UtilsSpec extends FreeSpec with Matchers {
         f.delete
       }
     }
-    "make a getFilesToCache function available" - {
-      "which sends back the files that need caching" in {
-        val res = parse(SpecHelpers.resolverConfig)
-        val reg = (("schema" -> "iglu:com.snowplowanalytics.snowplow/enrichments/jsonschema/1-0-0") ~
-          ("data" -> List(parse(SpecHelpers.ipLookupsEnrichmentConfig))))
-        getFilesToCache(res, reg) should contain only (
-          (new URI("http://acme.com/GeoLite2-City.mmdb"), "./ip_geo"))
-      }
-    }
     "make a getEnrichedEventMetrics function available" - {
       "which sends back vendor and tracker metrics" in {
         val event = {
@@ -78,31 +72,60 @@ class UtilsSpec extends FreeSpec with Matchers {
     }
     "make a getStringSize function available" - {
       "which sends back the size of a string in bytes" in {
-        getStringSize("a" * 10) shouldEqual 10
+        getSize("a" * 10) shouldEqual 10
       }
     }
     "make a resizeBadRow function available" - {
       "which leaves the bad row as is if it doesn't exceed the max size" in {
-        val badRow = BadRow("abc", NonEmptyList("error"))
-        resizeBadRow(badRow, 10) shouldEqual badRow
+        val badRow = BadRow
+          .CPFormatViolation(
+            processor,
+            Failure.CPFormatViolation(
+              Instant.ofEpochSecond(12),
+              "tsv",
+              FailureDetails.CPFormatViolationMessage.Fallback("ah")
+            ),
+            Payload.RawPayload("ah")
+          )
+          .compact
+        resizeBadRow(badRow, 500, processor) shouldEqual badRow
       }
+
       "which truncates the event in the bad row as is if it exceeds the max size" in {
-        val badRow = BadRow("a" * 100, NonEmptyList("error"))
-        val resizedBadRow = resizeBadRow(badRow, 40)
-        resizedBadRow.line shouldEqual "a"
-        resizedBadRow.errors.map(_.getMessage) shouldEqual NonEmptyList(
-          "Size of bad row (100) is greater than allowed maximum size (40)",
-          "error"
-        )
+        val original = BadRow
+          .CPFormatViolation(
+            processor,
+            Failure.CPFormatViolation(
+              Instant.ofEpochSecond(12),
+              "tsv",
+              FailureDetails.CPFormatViolationMessage.Fallback("ah")
+            ),
+            Payload.RawPayload("ah")
+          )
+          .compact
+
+        val res = resizeBadRow(original, 150, processor)
+        val resSdd = parseBadRow(res).right.get
+        resSdd shouldBe a[BadRow.SizeViolation]
+        val badRowSizeViolation = resSdd.asInstanceOf[BadRow.SizeViolation]
+        badRowSizeViolation.failure.maximumAllowedSizeBytes shouldEqual 150
+        badRowSizeViolation.failure.actualSizeBytes shouldEqual 267
+        badRowSizeViolation.failure.expectation shouldEqual "bad row exceeded the maximum size"
+        badRowSizeViolation.payload.line shouldEqual "{\"schema\":\"iglu"
+        badRowSizeViolation.processor shouldEqual processor
       }
     }
     "make a resizeEnrichedEvent function available" - {
       "which truncates a formatted enriched event and wrap it in a bad row" in {
-        val badRow = resizeEnrichedEvent("a" * 100, 100, 400)
-        badRow.line shouldEqual "a" * 10
-        badRow.errors.map(_.getMessage) shouldEqual NonEmptyList(
-          "Size of enriched event (100) is greater than allowed maximum (400)"
-        )
+        val res = resizeEnrichedEvent("a" * 100, 100, 400, processor)
+        val resSdd = parseBadRow(res).right.get
+        resSdd shouldBe a[BadRow.SizeViolation]
+        val badRowSizeViolation = resSdd.asInstanceOf[BadRow.SizeViolation]
+        badRowSizeViolation.failure.maximumAllowedSizeBytes shouldEqual 400
+        badRowSizeViolation.failure.actualSizeBytes shouldEqual 100
+        badRowSizeViolation.failure.expectation shouldEqual "event passed enrichment but exceeded the maximum allowed size as a result"
+        badRowSizeViolation.payload.line shouldEqual ("a" * 40)
+        badRowSizeViolation.processor shouldEqual processor
       }
     }
     "make a tabSeparatedEnrichedEvent function available" - {
@@ -153,4 +176,11 @@ class UtilsSpec extends FreeSpec with Matchers {
       }
     }
   }
+
+  def parseBadRow(jsonStr: String): Either[String, BadRow] =
+    for {
+      json <- parser.parse(jsonStr).leftMap(_.getMessage)
+      sdj <- SelfDescribingData.parse(json).leftMap(_.code)
+      res <- sdj.data.as[BadRow].leftMap(_.getMessage)
+    } yield res
 }
