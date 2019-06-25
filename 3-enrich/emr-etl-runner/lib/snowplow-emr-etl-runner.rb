90e71054e866b88bbeeda7708da01bce77ff4648
Anton Parkhomenko <mailbox@chuwy.me>
2019-06-25 16:27:03 +0700

    EmrEtlRunner: bump to 0.35.0 (close #4112)

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner.rb
@@ -37,6 +37,6 @@ require_relative 'snowplow-emr-etl-runner/linter'
 module Snowplow
   module EmrEtlRunner
     NAME    = "snowplow-emr-etl-runner"
-    VERSION = "0.34.3"
+    VERSION = "0.35.0"
   end
 end
