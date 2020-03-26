5a542e5de7a77605270e0b72cd63c06ca5bea5bf
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-03-26 21:46:29 +0300

    Stream Enrich: allow to download data from private S3 or GCS (close #4269)

--- 3-enrich/stream-enrich/core/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/model.scala
+++ 3-enrich/stream-enrich/core/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/model.scala
@@ -27,12 +27,20 @@ object model {
   sealed trait Credentials
   case object NoCredentials extends Credentials
   final case class AWSCredentials(accessKey: String, secretKey: String) extends Credentials
+  final case class GCPCredentials(creds: String) extends Credentials
+
+  /**
+   * Pair of optional AWS Credentials and optional GCP Credentials
+   * @param aws optional AWS Credentials
+   * @param gcp optional GCP Credentials
+   */
+  final case class DualCloudCredentialsPair(aws: Credentials, gcp: Credentials)
 
   // Case classes necessary to the decoding of the configuration
   final case class StreamsConfig(
     in: InConfig,
     out: OutConfig,
-    sourceSink: SourceSinkConfig,
+    sourceSink: TargetPlatformConfig,
     buffer: BufferConfig,
     appName: String
   )
@@ -44,16 +52,51 @@ object model {
     partitionKey: String
   )
   final case class KinesisBackoffPolicyConfig(minBackoff: Long, maxBackoff: Long)
-  sealed trait SourceSinkConfig
+
+  /** Represents configurations of all target platforms */
+  sealed trait TargetPlatformConfig
+
+  /** Represents configurations of all Stream Enrich targets that is AWS native
+   *  Credentials of other cloud providers might be provided in case an enrichment
+   *  requires a private data hosted on that platform
+   */
+  sealed trait AWSNativePlatformConfig extends TargetPlatformConfig {
+    def aws: AWSCredentials
+    def gcp: Option[GCPCredentials]
+
+    /** Represents AWS region
+     *
+     *  Note: Stream Enrich has been configured for AWS only until 1.1.0 and
+     *  backward compatibility of the configuration requires not to change
+     *  field name.
+     */
+    def region: String
+  }
+
+  /** Represents configurations of all Stream Enrich targets that is cloud agnostic
+   *  Credentials of any cloud provider might be provided in case an enrichment
+   *  requires a private data hosted on that platform
+   */
+  sealed trait CloudAgnosticPlatformConfig extends TargetPlatformConfig {
+    def aws: Option[AWSCredentials]
+    def gcp: Option[GCPCredentials]
+
+    /** Represents optional AWS region of the S3 bucket which stores private
+     *  data required for any enrichment
+     */
+    def region: Option[String]
+  }
+
   final case class Kinesis(
-    region: String,
     aws: AWSCredentials,
+    gcp: Option[GCPCredentials],
+    region: String,
     maxRecords: Int,
     initialPosition: String,
     initialTimestamp: Option[String],
     backoffPolicy: KinesisBackoffPolicyConfig,
     customEndpoint: Option[String]
-  ) extends SourceSinkConfig {
+  ) extends AWSNativePlatformConfig {
     val timestamp = initialTimestamp
       .toRight("An initial timestamp needs to be provided when choosing AT_TIMESTAMP")
       .right
@@ -69,19 +112,30 @@ object model {
     })
   }
   final case class Kafka(
+    aws: Option[AWSCredentials],
+    gcp: Option[GCPCredentials],
+    region: Option[String],
     brokers: String,
     retries: Int,
     consumerConf: Option[Map[String, String]],
     producerConf: Option[Map[String, String]]
-  ) extends SourceSinkConfig
+  ) extends CloudAgnosticPlatformConfig
   final case class Nsq(
+    aws: Option[AWSCredentials],
+    gcp: Option[GCPCredentials],
+    region: Option[String],
     rawChannel: String,
     host: String,
     port: Int,
     lookupHost: String,
     lookupPort: Int
-  ) extends SourceSinkConfig
-  case object Stdin extends SourceSinkConfig
+  ) extends CloudAgnosticPlatformConfig
+  final case class Stdin(
+    aws: Option[AWSCredentials],
+    gcp: Option[GCPCredentials],
+    region: Option[String]
+  ) extends CloudAgnosticPlatformConfig
+
   final case class BufferConfig(
     byteLimit: Long,
     recordLimit: Long,
